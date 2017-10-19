29f698c181e2640ef80ff23a9b6c3faa98f380d9
yangyir <31230104+yangyir@users.noreply.github.com>
2017-10-19 23:06:06 +0800

    add getintradaybuckets2 and fix bugs

--- trading/utilities/getintradaybuckets2.m
+++ trading/utilities/getintradaybuckets2.m
@@ -1,4 +1,7 @@
-function buckets = getintradaybuckets(varargin)
+function buckets = getintradaybuckets2(varargin)
+    %note:get intraday buckets from the one whole trading day
+    %start on the trading hours, i.e. 09:00 am
+    %ends on the trading hours, i.e. 01:00 am on the next day
     p = inputParser;
     p.CaseSensitive = false;
     p.KeepUnmatched = true;
@@ -6,8 +9,6 @@ function buckets = getintradaybuckets(varargin)
         @(x) validateattributes(x,{'char','numeric'},{},'','Date'));
     p.addParameter('Frequency',{},...
         @(x) validateattributes(x,{'char'},{},'','Frequency'));
-    p.addParameter('OutputFormat',{},...
-        @(x) validateattributes(x,{'char'},{},'','OutputFormat'));
     p.addParameter('TradingHours',{},...
         @(x) validateattributes(x,{'char','cell'},{},'','TradingHours'));
     p.addParameter('TradingBreak',{},...
@@ -15,27 +16,14 @@ function buckets = getintradaybuckets(varargin)
     p.parse(varargin{:});
     day = p.Results.Date;
     freq = p.Results.Frequency;
-    outputFormat = p.Results.OutputFormat;
     tradingHours = p.Results.TradingHours;
     tradingBreak = p.Results.TradingBreak;
-    if isempty(day)
-        day = today;
-    end
-    if isempty(freq)
-        freq = '1m';    %default interval as of 1m
-    end
-    if isempty(outputFormat)
-        outputFormat = 'number';
-    end
+    if isempty(day), day = today; end
+    if isempty(freq), freq = '1m';end
     
-
-    %sanity checking
-    if ~(strcmpi(outputFormat,'number') || strcmpi(outputFormat,'string'))
-        error('getintradaybuckets:invalid format input,must be number or string');
-    end
-    %
+    %sanity checks
     if isempty(tradingHours) && ~isempty(tradingBreak)
-        error('getintradaybuckets:missing trading hours inputs once trading break is given');
+        error('getintradaybuckets2:missing trading hours inputs once trading break is given');
     end
     
     if strcmpi(freq(end),'m')
@@ -43,18 +31,33 @@ function buckets = getintradaybuckets(varargin)
     elseif strcmpi(freq(end),'h')
         bucket = str2double(freq(1:end-1))*60;
     else
-        error('getintradaybuckets:invalid interval inputs');
+        error('getintradaybuckets2:invalid interval inputs');
     end
     
     minutes_per_day = 1440;
     if mod(minutes_per_day,bucket) > 0
-        error('getintradaybuckets:intraday buckets cannot be equally distrubuted');
+        error('getintradaybuckets2:intraday buckets cannot be equally distrubuted');
+    end
+    
+    if ~isempty(tradingHours)
+        th = regexp(tradingHours,';','split');
+        startstr = regexp(th{1},'-','split');
+        bucket_start = 60*str2double(startstr{1}(1:2))+str2double(startstr{1}(end-1:end));
+        endstr = regexp(th{end},'-','split');
+        bucket_end = 60*str2double(endstr{2}(1:2))+str2double(endstr{2}(end-1:end));
+        
+        if bucket_end < bucket_start
+            bucket_end = bucket_end + minutes_per_day;
+        end
+    else
+        bucket_start = 0;
+        bucket_end = minutes_per_day;
     end
     
     buckets = NaN(minutes_per_day,1);
-    m = 0;
+    m = bucket_start;
     count = 1;
-    while m < minutes_per_day
+    while m < bucket_end
         buckets(count,1) = m;
         m = m + bucket;
         count = count + 1;
@@ -68,8 +71,7 @@ function buckets = getintradaybuckets(varargin)
         use = zeros(length(buckets),1);
         th = regexp(tradingHours,';','split');
         m = regexp(th{1},'-','split');
-        a = regexp(th{2},'-','split');
-        e = regexp(th{3},'-','split');
+        a = regexp(th{2},'-','split');   
         m_open = 60*str2double(m{1}(1:2))+str2double(m{1}(end-1:end));
         m_close = 60*str2double(m{2}(1:2))+str2double(m{2}(end-1:end));
         a_open = 60*str2double(a{1}(1:2))+str2double(a{1}(end-1:end));
@@ -80,7 +82,8 @@ function buckets = getintradaybuckets(varargin)
                 use(i) = 1;
             end
         end    
-        if length(e) == 2
+        if size(th,2) == 3
+            e = regexp(th{3},'-','split');
             e_open = 60*str2double(e{1}(1:2))+str2double(e{1}(end-1:end));
             e_close = 60*str2double(e{2}(1:2))+str2double(e{2}(end-1:end));
             for i = 1:length(buckets)
@@ -90,8 +93,7 @@ function buckets = getintradaybuckets(varargin)
                     end
                 else
                     % overnight trades
-                    if (buckets(i) >= e_open && buckets(i) < minutes_per_day) || ...
-                       (buckets(i) >= 0 && buckets(i) < e_close)
+                    if (buckets(i) >= e_open && buckets(i) < minutes_per_day+e_close)
                         use(i) = 1;
                     end
                 end
@@ -103,7 +105,7 @@ function buckets = getintradaybuckets(varargin)
                 tb_start = 60*str2double(tb{1}(1:2))+str2double(tb{1}(end-1:end));
                 tb_end = 60*str2double(tb{2}(1:2))+str2double(tb{2}(end-1:end));
                 for i = 1:length(buckets)
-                    if buckets(i) > tb_start && buckets(i) < tb_end
+                    if buckets(i) >= tb_start && buckets(i) < tb_end
                         use(i) = 0;
                     end
                 end
@@ -114,9 +116,6 @@ function buckets = getintradaybuckets(varargin)
     end
     
     buckets = datenum(day) + buckets./minutes_per_day;
-    %    
-    if strcmpi(outputFormat,'string')
-        buckets = datestr(buckets);
-    end
+    %
         
 end
