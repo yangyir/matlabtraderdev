19d07cb299b3cacaa6118de167bbc42b3766527c
yangyir <yiran.yang@outlook.com>
2017-11-17 19:06:33 +0800

    add options for may contracts

--- trading_loadoptions.m
+++ trading_loadoptions.m
@@ -40,6 +40,44 @@ for i = 1:size(strikes_soymeal,1)
 end
 
 %%
+fut_m1805 = cFutures('m1805');
+fut_m1805.loadinfo([fut_dir_,'m1805_info.txt']);
+opt_c_m1805 = cell(size(strikes_soymeal));
+opt_p_m1805 = cell(size(strikes_soymeal));
+for i = 1:size(strikes_soymeal,1)
+    c_code_i = ['m1805-C-',num2str(strikes_soymeal(i))];
+    opt_c_m1805{i} = cOption(c_code_i);
+    fn = [opt_dir_,c_code_i,'_info.txt'];
+    fid = fopen(fn);
+    if fid > 0
+        opt_c_m1805{i}.loadinfo(fn);
+        fclose(fid);
+    else
+        if ~(exist('conn','var') && isa(conn,'cBloomberg'))
+            conn = cBloomberg;
+        end
+        opt_c_m1805{i}.init(conn);
+        opt_c_m1805{i}.saveinfo(fn);
+    end
+    %
+    p_code_i = ['m1805-P-',num2str(strikes_soymeal(i))];
+    opt_p_m1805{i} = cOption(p_code_i);
+    fn = [opt_dir_,p_code_i,'_info.txt'];
+    fid = fopen(fn);
+    if fid > 0
+        opt_p_m1805{i}.loadinfo(fn);
+        fclose(fid);
+    else
+        if ~(exist('conn','var') && isa(conn,'cBloomberg'))
+            conn = cBloomberg;
+        end
+        opt_p_m1805{i}.init(conn);
+        opt_p_m1805{i}.saveinfo(fn);
+    end
+    
+end
+
+%%
 strikes_sugar = [5900;6000;6100;6200;6300;6400];
 fut_SR801 = cFutures('SR801');
 fut_SR801.loadinfo([fut_dir_,'SR801_info.txt']);
@@ -78,6 +116,42 @@ for i = 1:size(strikes_sugar,1)
 end
 
 %%
+fut_SR805 = cFutures('SR805');
+fut_SR805.loadinfo([fut_dir_,'SR805_info.txt']);
+opt_c_SR805 = cell(size(strikes_sugar));
+opt_p_SR805 = cell(size(strikes_sugar));
+for i = 1:size(strikes_sugar,1)
+    c_code_i = ['SR805C',num2str(strikes_sugar(i))];
+    opt_c_SR805{i} = cOption(c_code_i);
+    fn = [opt_dir_,c_code_i,'_info.txt'];
+    fid = fopen(fn);
+    if fid > 0
+        opt_c_SR805{i}.loadinfo(fn);
+        fclose(fid);
+    else
+        if ~(exist('conn','var') && isa(conn,'cBloomberg'))
+            conn = cBloomberg;
+        end
+        opt_c_SR805{i}.init(conn);
+        opt_c_SR805{i}.saveinfo(fn);
+    end
+    %
+    p_code_i = ['SR805P',num2str(strikes_sugar(i))];
+    opt_p_SR805{i} = cOption(p_code_i);
+    fn = [opt_dir_,p_code_i,'_info.txt'];
+    fid = fopen(fn);
+    if fid > 0 
+        opt_p_SR805{i}.loadinfo(fn);
+        fclose(fid);
+    else
+        if ~(exist('conn','var') && isa(conn,'cBloomberg'))
+            conn = cBloomberg;
+        end
+        opt_p_SR805{i}.init(conn);
+        opt_p_SR805{i}.saveinfo(fn);
+    end
+end
+%%
 fprintf('listed option loaded......\n');
 fprintf('\tcalls m1801\n');
 disp(opt_c_m1801);
@@ -87,4 +161,13 @@ fprintf('\tcalls SR801\n');
 disp(opt_p_SR801);
 fprintf('\tputs SR801\n');
 disp(opt_p_SR801);
+%
+fprintf('\tcalls m1805\n');
+disp(opt_c_m1805);
+fprintf('\tputs m1805\n');
+disp(opt_c_m1805);
+fprintf('\tcalls SR805\n');
+disp(opt_p_SR805);
+fprintf('\tputs SR805\n');
+disp(opt_p_SR805);
 
