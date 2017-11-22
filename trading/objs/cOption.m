25b803f37eed435237698f728a7f9c092a42c78c
yangyir <yiran.yang@outlook.com>
2017-11-22 13:09:44 +0800

    add destructor in cInstrument, cFutures and cOptions

--- trading/objs/cOption.m
+++ trading/objs/cOption.m
@@ -8,19 +8,19 @@ classdef cOption < cInstrument
         code_bbg_underlier@char
         
         opt_american@double
-        opt_type
-        opt_strike
-        opt_expiry_date1
+        opt_type@char
+        opt_strike@double
+        opt_expiry_date1@double
         opt_expiry_date2@char
         
-        contract_size
-        tick_size
-        tick_value
-        asset_name
-        exchange
-        first_trade_date1
+        contract_size@double
+        tick_size@double
+        tick_value@double
+        asset_name@char
+        exchange@char
+        first_trade_date1@double
         first_trade_date2@char
-        last_trade_date1
+        last_trade_date1@double
         last_trade_date2@char
         
         trading_hours@char
@@ -32,6 +32,33 @@ classdef cOption < cInstrument
     end
     
     methods
+        function [] = delete(obj)
+            obj.code_ctp = '';
+            obj.code_wind = '';
+            obj.code_bbg = '';
+            obj.code_ctp_underlier = '';
+            obj.code_wind_underlier = '';
+            obj.code_bbg_underlier = '';
+            obj.opt_american = [];
+            obj.opt_type = '';
+            obj.opt_strike =[];
+            obj.opt_expiry_date1 =[];
+            obj.opt_expiry_date2 = '';
+            obj.contract_size =[];
+            obj.tick_size = [];
+            obj.tick_value = [];
+            obj.asset_name = '';
+            obj.exchange = '';
+            obj.first_trade_date1 = [];
+            obj.first_trade_date2 = '';
+            obj.last_trade_date1 = [];
+            obj.last_trade_date2 = '';
+            obj.trading_hours = '';
+            obj.trading_break = '';
+            
+            delete@cInstrument(obj);
+        end
+        
         function obj = cOption(codestr)
             if nargin < 1
                 return
