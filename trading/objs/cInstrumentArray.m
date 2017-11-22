760bafbcefc67ac90d221c8cb51f31ea157c555b
yangyir <yiran.yang@outlook.com>
2017-11-22 17:31:25 +0800

    oops, forgot to commit

--- trading/objs/cInstrumentArray.m
+++ trading/objs/cInstrumentArray.m
@@ -19,14 +19,18 @@ classdef cInstrumentArray < handle
                 return
             end
             
-            if ~isa(instrument,'cInstrument')
-                error('cInstrumentArray:hasinstrument:cInstrument type of input expected')
+            if ischar(instrument)
+                code_str = instrument;
+            elseif isa(instrument,'cInstrument')
+                code_str = instrument.code_ctp;
+            else
+                error('cInstrumentArray:hasinstrument:cInstrument or char type of input expected')
             end
             n = obj.count;
             bool = false;
             idx = 0;
             for i = 1:n
-                if strcmpi(instrument.code_ctp,obj.list_{i}.code_ctp)
+                if strcmpi(code_str,obj.list_{i}.code_ctp)
                     bool = true;
                     idx = i;
                     break;
