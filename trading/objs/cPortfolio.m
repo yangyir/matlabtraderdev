88745af8d1c4eeb5e1e2a08cad7b3ed3b20cf729
yangyir <yiran.yang@outlook.com>
2017-11-24 14:23:45 +0800

    add more logic for volume today in cPortfolio

--- trading/objs/cPortfolio.m
+++ trading/objs/cPortfolio.m
@@ -40,11 +40,12 @@ classdef cPortfolio < handle
         %end of count
     
         
-        function obj = addinstrument(obj,instrument,px,volume,dtnum)
+        function obj = addinstrument(obj,instrument,px,volume,dtnum,closetoday)
             if nargin < 3
                 px = 0;
                 volume = 0;
                 dtnum = now;
+                closetoday = 0;
             end
             
             if nargin == 3
@@ -53,10 +54,18 @@ classdef cPortfolio < handle
             
             if nargin == 4
                 dtnum = now;
+                closetoday = 0;
+            end
+            
+            if nargin == 5
+                closetoday = 0;
             end
             
             [bool,idx] = obj.hasinstrument(instrument);
             if ~bool
+                if closetoday ~= 0
+                    error('cPortfolio:addinstrument:position not found to close in the portfolio')
+                end
                 n = obj.count;
                 list_ = cell(n+1,1);
                 c_ = zeros(n+1,1);
@@ -85,7 +94,62 @@ classdef cPortfolio < handle
                 volume_today_ = obj.instrument_volume_today(idx,1);
                 obj.instrument_volume(idx,1) = volume_+volume;
                 if dtnum > getlastbusinessdate
-                    obj.instrument_volume_today(idx,1) = volume_today_ + volume;
+                    if volume_today_ > 0
+                        if volume > 0
+                            %same direction-long positions
+                            obj.instrument_volume_today(idx,1) = volume_today_ + volume;
+                        else
+                            %unwind positions
+                            if closetoday
+                                if abs(volume_today_) < abs(volume)
+                                    error('cPortfolio:addinstrument:closetoday volume exceeds existing volume as of today')
+                                end
+                                obj.instrument_volume_today(idx,1) = volume_today_ + volume;
+                            else
+                                volume_yesterday_ = volume_ - volume_today_;
+                                if abs(volume_yesterday_) > abs(volume)
+                                    obj.instrument_volume_today(idx,1) = volume_today_;
+                                else
+                                    %all the volume yesterday has been
+                                    %unwinded
+                                    obj.instrument_volume_today(idx,1) = volume_ + volume;
+                                end
+                            end
+                        end
+                    elseif volume_today_ < 0
+                        if volume < 0
+                            %same direction-short positions
+                            obj.instrument_volume_today(idx,1) = volume_today_ + volume;
+                        else
+                            %unwind positions
+                            if closetoday
+                                if abs(volume_today_) < abs(volume)
+                                    error('cPortfolio:addinstrument:closetoday volume exceeds existing volume as of today');
+                                end
+                                obj.instrument_volume_today(idx,1) = volume_today_ + volume;
+                            else
+                                volume_yesterday_ = volume_ - volume_today_;
+                                if abs(volume_yesterday_) > abs(volume)
+                                    obj.instrument_volume_today(idx,1) = volume_today_;
+                                else
+                                    %all the volume yesterday has been
+                                    %unwinded
+                                    obj.instrument_volume_today(idx,1) = volume_ + volume;
+                                end
+                            end
+                        end
+                    elseif volume_today_ == 0
+                        if closetoday
+                            error('cPortfolio:addinstrument:position not found to close in the portfolio')
+                        end
+                        if sign(volume_) == sign(volume) || sign(volume_) == 0
+                            obj.instrument_volume_today(idx,1) = volume_today_ + volume;
+                        else
+                            %unwind trades
+                            obj.instrument_volume_today(idx,1) = 0;
+                        end
+                            
+                    end
                 end
                 
                 if obj.instrument_volume(idx,1) == 0
