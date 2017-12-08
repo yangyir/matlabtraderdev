170601d1f9bcac781a0c7e234c65f1a0c9eaf8a0
yangyir <31230104+yangyir@users.noreply.github.com>
2017-12-08 12:26:38 +0800

    add trading_opt_yy

---
+++ trading_opt_yy.m
@@ -0,0 +1,33 @@
+%%
+login_counter_opt1;
+
+%%
+fprintf('register option strategy with options......\n');
+try
+    stratopt_yy = cStratOpt;
+    stratopt_yy.registercounter(c_opt1);
+    stratopt_yy.registeroptions('m1805',9);
+    stratopt_yy.timer_interval_ = 60;
+    stratopt_yy.loadportfoliofromcounter;
+    stratopt_yy.portfolio_.print;
+catch e
+    error(e.message);
+end
+
+%%
+stratopt_yy.start;
+
+%%
+%real-time pnl and risk
+[pnltbl,risktbl] = stratopt_yy.pnlriskrealtime;
+printpnltbl(pnltbl);
+printrisktbl(risktbl);
+fprintf('\n');
+
+%%
+stratopt_yy.stop;
+%%
+stratopt_yy.saveportfoliotofile('c:\temp\check1.txt');
+
+
+
