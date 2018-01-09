8e700bbcda93ea85e869f240f8c760c0736940d2
yangyir <31230104+yangyir@users.noreply.github.com>
2018-01-09 09:24:50 +0800

    trading script change

--- trading_opt_ly.m
+++ trading_opt_ly.m
@@ -8,6 +8,8 @@ try
     stratopt_ly.registercounter(c_opt2);
     stratopt_ly.registeroptions('m1805',9);
     stratopt_ly.timer_interval_ = 60;
+    stratopt_ly.loadportfoliofromcounter;
+    stratopt_ly.portfolio_.print;
 catch e
     error(e.message);
 end
@@ -19,15 +21,17 @@ stratopt_ly.start;
 stratopt_ly.stop;
 
 %%
-stratopt_ly.loadportfoliofromcounter;
-stratopt_ly.portfolio_.print;
-
-%%
 %real-time pnl and risk
 [pnltbl,risktbl] = stratopt_ly.pnlriskrealtime;
 printpnltbl(pnltbl);printrisktbl(risktbl);fprintf('\n');
 
 %%
+code = 'm1805-C-3050';
+lots = 5;
+stratopt_ly.shortopensingleinstrument(code,lots);
+%%
+pnltbleod = cHelper.pnlrisk1(stratopt_ly.portfolio_,getlastbusinessdate);
+printpnltbl(pnltbleod);
 
 
 
