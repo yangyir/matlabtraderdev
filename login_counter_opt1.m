8d499d9a1fff079c9a9326ee2a71647861b9c071
yangyir <yiran.yang@outlook.com>
2018-01-09 09:27:53 +0800

    bug

--- login_counter_opt1.m
+++ login_counter_opt1.m
@@ -4,5 +4,5 @@ cd(pathhome);
 %
 if ~exist('c_opt1','var') || ~isa(c_opt1,'CounterCTP'), c_opt1 = CounterCTP.ccb_liyang_fut;end
 if ~c_opt1.is_Counter_Login, c_opt1.login;end
-if ~exist('m_opt1','var') || ~isa(m_opt1,'cCTP'), m_opt1 = cCTP.ccb_liyang_fut;end
-if ~m_opt1.isconnect, m_opt1.login;end
+% if ~exist('m_opt1','var') || ~isa(m_opt1,'cCTP'), m_opt1 = cCTP.ccb_liyang_fut;end
+% if ~m_opt1.isconnect, m_opt1.login;end
