a97f2256a7e68d36d397e9932dd55613cc2b3762
yangyir <31230104+yangyir@users.noreply.github.com>
2018-03-20 00:43:23 +0800

    wr backtest logic

--- Untitled2.m
+++ Untitled2.m
@@ -1,68 +1,38 @@
 %%
-d = candle_5m{1};
-nperiod = 144;
+d = candle_15m{1};
+nperiods = 144;
 nobs = size(d,1);
+openp = d(:,2);
 highp = d(:,3);
 lowp = d(:,4);
 closep = d(:,5);
-nrecord = nobs-nperiod;
-%%
-record_upper = zeros(nrecord,1);
-record_lower = zeros(nrecord,1);
-nh = 0;
-nl = 0;
-for i = 1:nrecord
-    highestp = max(highp(i:i+nperiod-1));
-    lowestp = min(lowp(i:i+nperiod-1));
-    p = closep(i+nperiod);
-    if p >= highestp
-        %find out how many buckets of time period later the highest price
-        %is updated
-        nh = nh+1;
-        for j = i+nperiod+1:nobs
-            if closep(j) > p
-                record_upper(nh,1) = j-i-nperiod;
-                break
-            end
-        end
-    elseif p <= lowestp
-        %find out how many buckets of time period later the lowest price is
-        %updated
-        nl = nl + 1;
-        for j = i+nperiod+1:nobs
-            if closep(j) < p
-                record_lower(nl,1) = j-i-nperiod;
-                break
-            end
-        end
-    else
-        
-    end
-end
-record_upper = record_upper(1:nh,:);
-record_lower = record_lower(1:nl,:);
-subplot(211);
-hist(record_upper,40);title('upper');
-subplot(212);
-hist(record_lower,40);title('lower');
+nrecords = nobs-nperiods;
+
 %%
 %risk control
 maxposition = inf;
 maxdrawdown = inf;
+%level down and out
+leveldo = -50;
+%level up and out
+leveluo = -50;
 %
-trades_long = zeros(nrecord,1);
-trades_sell = zeros(nrecord,1);
-positions = zeros(nrecord,1);
-costs = zeros(nrecord,1);
-pnl = zeros(nrecord,1);
-wrindicators = zeros(nrecord,1);
+trades_long = zeros(nrecords,1);
+trades_sell = zeros(nrecords,1);
+positions = zeros(nrecords,1);
+costs = zeros(nrecords,1);
+pnl = zeros(nrecords,1);
+wrindicators = zeros(nrecords,1);
 %
-for i = 1:nrecord
-    highestp = max(highp(i:i+nperiod-1));
-    lowestp = min(lowp(i:i+nperiod-1));
-    p = closep(i+nperiod);
+upperhit = false;
+lowerhit = false;
+for i = 1:nrecords
+    highestp = max(highp(i:i+nperiods-1));
+    lowestp = min(lowp(i:i+nperiods-1));
+    p = closep(i+nperiods);
+    wr = willpctr(highp(i:i+nperiods),lowp(i:i+nperiods),closep(i:i+nperiods),nperiods);
+    wrindicators(i) = wr(end);
     if p >= highestp
-        wrindicators(i) = 0;
         %in case the close price is higher than the highest of the previous
         %selected period, it is taken as a over-bought signal
         if i == 1, positions(i) = -1;end
@@ -75,15 +45,11 @@ for i = 1:nrecord
         %last for a couple of time period and we shall unwind the existing
         %long position once the wr indicator breaches -50 from the top
         if i > 1 && positions(i-1) > 0
-%             if wrindicators(i) > wrindicators(i-1)
-%                 positions(i) = positions(i-1);
-%             else
-                positions(i) = -1;
-%             end
+            upperhit = true;
+            positions(i) = positions(i-1);
         end
         %
     elseif p <= lowestp
-        wrindicators(i) = -100;
         %in case the close price is lower than the lowest of the previous
         %selected period, it is taken as a over-sold signal
         if i == 1, positions(i) = 1; end
@@ -93,15 +59,20 @@ for i = 1:nrecord
             positions(i) = min(maxposition,positions(i-1)+1);
         end
         if i > 1 && positions(i-1) < 0
-            if wrindicators(i) < wrindicators(i-1)
-%                 positions(i) = positions(i-1);
-%             else
-                positions(i) = 1;
-            end
+            lowerhit = true;
+            positions(i) = positions(i-1);
         end
     else
-        wrindicators(i) = (p-lowestp)/(highestp-lowestp)*-100;
-        if i > 1, positions(i) = positions(i-1);end
+        if i > 1 
+            if upperhit && wrindicators(i) < leveldo
+                positions(i) = 0;
+                upperhit = false;
+            elseif lowerhit && wrindicators(i) > leveluo
+                positions(i) = 0;
+            else
+                positions(i) = positions(i-1);
+            end
+        end
     end
     %
     if i == 1 && positions(i) == 0, costs(i) = 0; end
@@ -114,7 +85,7 @@ for i = 1:nrecord
     end
 end
 subplot(221)
-plot(closep(nperiod+1:end),'b');grid on;title('close');
+plot(closep(nperiods+1:end),'b');grid on;title('close');
 subplot(222)
 bar(positions);grid on;title('position');
 subplot(223)
