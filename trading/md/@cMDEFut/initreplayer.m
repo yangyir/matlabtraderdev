75180fcc9adf957bc9065ec20f5ade9ed32d6dcc
yangyir <31230104+yangyir@users.noreply.github.com>
2018-05-06 01:32:38 +0800

    refresh timevec of candles once its mode is set to replay with replay date specified

--- trading/md/@cMDEFut/initreplayer.m
+++ trading/md/@cMDEFut/initreplayer.m
@@ -24,4 +24,24 @@ function [] = initreplayer(obj,varargin)
     obj.replay_datetimevec_ = r.tickdata_{idx}(:,1);
     obj.replay_count_ = 1;
     
+    % candles_ and candles4save_'s timevec needs to be inline with replay
+    % date
+    [f2,idx2] = obj.qms_.instruments_.hasinstrument(codestr);
+    if ~f2, error('cMDEFut:initreplayer:code not registered!');end
+    instruments = obj.qms_.instruments_.getinstrument;
+    
+    buckets = getintradaybuckets2('date',obj.replay_date1_,...
+        'frequency',[num2str(obj.candle_freq_(idx2)),'m'],...
+        'tradinghours',instruments{idx2}.trading_hours,...
+        'tradingbreak',instruments{idx2}.trading_break);
+    candle_ = [buckets,zeros(size(buckets,1),4)];
+    obj.candles_{idx2} = candle_;
+    
+    buckets = getintradaybuckets2('date',obj.replay_date1_,...
+        'frequency','1m',...
+        'tradinghours',instruments{idx2}.trading_hours,...
+        'tradingbreak',instruments{idx2}.trading_break);
+    candle_ = [buckets,zeros(size(buckets,1),4)];
+    obj.candles4save_{idx2} = candle_;
+    
 end
