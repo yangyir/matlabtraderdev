877f7c750a862d3f5c7468d54e1627bf23b35f62
yangyir <yiran.yang@outlook.com>
2018-05-17 17:44:00 +0800

    dev_temp

--- dev_temp.m
+++ dev_temp.m
@@ -2,6 +2,8 @@ mde = cMDEFut;
 code = 'rb1810';
 inst = code2instrument(code);
 mde.registerinstrument(inst);
+mde.candle_freq_ = 15;
+mde.display_ = 1;
 %%
 fns = {'rb1810_20180423_tick';...
     'rb1810_20180424_tick';...
@@ -15,4 +17,23 @@ fns = {'rb1810_20180423_tick';...
     'rb1810_20180508_tick';...
     'rb1810_20180509_tick';...
     'rb1810_20180510_tick';...
-    'rb1810_20180511_tick'};
+    'rb1810_20180511_tick';...
+    'rb1810_20180514_tick';...
+    'rb1810_20180515_tick';......
+    'rb1810_20180516_tick'};
+mde.initreplayer('code',code,'filenames',fns);
+%%
+mde.initcandles;
+%%
+replay_speed = 60000;
+mde.timer_interval_ = 60/ replay_speed;
+%%
+clc;
+mde.replay_count_ = 1;
+mde.start
+%%
+ticks = mde.getlasttick(inst);
+fprintf('time:%s price:%s\n',datestr(ticks(1),'yyyy-mm-dd HH:MM:SS'),num2str(ticks(2)));
+%%
+mde.stop;
+delete(timerfindall)
