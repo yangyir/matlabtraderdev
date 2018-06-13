f946b4eaed3991fc375a6a6f9b8f9313fe7c62ea
qs618 <31229970+qs618@users.noreply.github.com>
2018-06-13 19:12:28 +0800

    updated candle_manual
    2018/6/13 19:11, there are still some problems unsolved in this script,...

--- backtest/backtest_script_check_candles.m
+++ backtest/backtest_script_check_candles.m
@@ -1,5 +1,11 @@
-futs = code2instrument('rb1810');
+% 数据说明：Bloomberg , tick data 转化成 candle_data, 左开右闭，
+% 例如： 时间 09：00：00 的一分钟K线数据来源的区间： （09:00:00， 09:01:00】
+% 但是有特殊情况是： 收盘时间 15：00：00， 这分钟没有K线数据的， 但是存在tick数据，导致：
+% 14：59：00 K线图对应tick数据区间： （14：59：00 , 15：00：00】
+% 21：00：00 K线图对应tick数据区间： 【21：00：00， 21：01：00】
+% 解决方法： K线图buckets vector虚拟增加时间轴 15：00：00 ，最后算完再4删掉？？？我想下，现在脑子 in a mess
 
+futs = code2instrument('rb1810');
 fn_tick = 'rb1810_20180423_tick.mat';
 fn_candles = 'rb1810_20180423_1m.txt';
 d = load(fn_tick);
@@ -12,9 +18,9 @@ buckets = getintradaybuckets2('date',floor(ticks(1,1)),...
 candles_manual = zeros(size(buckets,1),5);
 candles_manual(:,1) = buckets;
 %%
-t = ticks(1,1);
-pxtrade = ticks(1,2);
-idx = buckets(1:end-1)<=t & buckets(2:end)>t;
+t = ticks(2,1);
+pxtrade = ticks(2,2);
+idx = buckets(1:end-1)<t & buckets(2:end)>=t;
 this_bucket = buckets(idx);
 %%
 if ~isempty(this_bucket)
@@ -36,7 +42,15 @@ nticks = size(ticks,1);
 for i = 2:nticks
     t = ticks(i,1);
     pxtrade = ticks(i,2);
-    idx = buckets(1:end-1)<=t & buckets(2:end)>t;
+    % equalorNot 用来解决str相同，但是double不同导致最终比较结果错误的问题
+    equalorNot = (round(buckets(2:end) *10e+07) == round(t*10e+07));
+    if sum(sum(equalorNot))==0
+       idx = buckets(1:end-1)<t & buckets(2:end)>t;
+    else
+        idx = buckets(1:end-1)<t & equalorNot;
+    end
+    
+%     idx = buckets(1:end-1)<t & ((buckets(2:end)>=t )) ; 
     this_bucket = buckets(idx);
     %
     if ~isempty(this_bucket)
