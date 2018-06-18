04e6bd89d51ab1cfa168c5eb4964ed820bbabb86
qs618 <qiongsun618@126.com>
2018-06-18 21:21:27 +0800

    not finished yet...

--- trading/md/@cMDEFut/updatecandleinmem - sunq.m
+++ trading/md/@cMDEFut/updatecandleinmem - sunq.m
@@ -32,27 +32,100 @@ function [] = updatecandleinmem(mdefut, instrument)
     elseif datenum(instrument.break_interval{end,end}) == datenum('01:00:00')
         kind =3;
     end
-
     if isempty(mdefut.ticks_), return; end
     ns = size(mdefut.ticks_,1);
     count = mdefut.ticks_count_;
-    
-    
-    
-    for i = 1:ns
-        buckets = mdefut.candles_{i}(:,1);
-        buckets4save = mdefut.candles4save_{i}(:,1);
-        t = mdefut.ticks_{i}(count(i),1);
+    for i =1:ns
+        buckets = mutfut.candles_{i}(:,1);
+        buckets4save = mdefut.candles4save_{i}(:,i);
+        t = mdefut.ticks_{i}(cout(i),4);
         px_trade = mdefut.ticks_{i}(count(i),4);
-        idx = buckets(1:end-1)<=t & buckets(2:end)>t;
-        idx4save = buckets4save(1:end-1)<=t & buckets4save(2:end)>t;
+        datestring1 = datestr(buckets(1));
+        datestring2 = datestring1(1:end-8);
+        datestring3 = datestr(buckets(end));
+        datestring4 = datestring3(1:end-8);
+        num15_00_00 = datenum([datestring2,'15:00:00']);
+        num20_59_00 = datenum([datestring2,'20:59:00']);
+        num21_00_00 = datenum([datestring2, '21:00:00']);
+        num21_00_0_5 = datenum([datestring2, '21:00:0.5']);
+        num11_30_00 = datenum([datestring2, '11:30:00']);
+        num13_30_00 = datenum([datestring2, '13:30:00']);
+        num13_00_00 = datenum([datestring2, '13:00:00']);
+        num10_15_00 = datenum([datestring2, '10:15:00']);
+        num10_30_00 = datenum([datestring2, '10:30:00']);
+        num23_00_00 = datenum([datestring2, '23:00:00']);
+        num09_00_00 = datenum([datestring2, '09:00:00']);
+        num22_59_59_5= datenum([datestring2, '22:59:59.5']); 
+        num01_00_00 = datenum([datestring4,'01:00:00']);
+        num00_00_00 = datenum([datestring4,'00:00:00']);
+        num00_00_0_5 = datenum([datestring4,'00:00:0.5']);
+        if kind == 1
+              if t == num20_59_00
+                  continue
+              elseif t == num21_00_00
+                  t = num21_00_0_5;
+              elseif t == num23_00_00
+                  t = num22_59_59_5;
+              elseif t == num10_30_00
+                  continue
+              elseif t == num13_30_00
+                  continue
+              elseif t == num10_15_00
+                  t = num10_30_00;
+              elseif t== num11_30_00
+                  t = num13_30_00;
+              elseif t == num15_00_00
+                  t = num21_00_00;
+              elseif t == num23_00_00
+                  t = num09_00_00;
+              end
+            elseif kind == 2
+                if t == num11_30_00
+                    continue
+                elseif t == num13_00_00
+                    continue
+                end  
+            elseif kind == 3
+              if t == num20_59_00
+                  continue
+              elseif t == num21_00_00
+                  t = num21_00_0_5;
+              elseif t == num23_00_00
+                  t = num22_59_59_5;
+              elseif t == num10_30_00
+                  continue
+              elseif t == num13_30_00
+                  continue
+              elseif t == num10_15_00
+                  t = num10_30_00;
+              elseif t== num11_30_00
+                  t = num13_30_00;
+              elseif t == num15_00_00
+                  t = num21_00_00;
+              elseif t == num23_00_00
+                  t = num09_00_00;
+              end
+        end
+        % equalorNot 用来解决str相同，但是double不同导致最终比较结果错误的问题
+        equalorNot = (round(buckets(2:end) *10e+07) == round(t*10e+07));
+        equalorNot4save = (round(buckets4save(2:end) *10e+07) == round(t*10e+07));
+        if sum(sum(equalorNot))==0
+           idx = buckets(1:end-1)<t & buckets(2:end)>t;
+        else
+            idx = buckets(1:end-1)<t & equalorNot;
+        end
         this_bucket = buckets(idx);
+        if sum(sum(equalorNot4save))==0
+           idx4save = buckets4save(1:end-1)<t & buckets4save(2:end)>t;
+        else
+           idx4save = buckets4save(1:end-1)<t & equalorNot4save;
+        end
         this_bucket_save = buckets4save(idx4save);
         %
         if ~isempty(this_bucket)
             this_count = find(buckets == this_bucket);
         else
-            if t >= buckets(end) && t < buckets(end)+buckets(end)-buckets(end-1)
+            if t >= buckets(end) 
                 this_count = size(buckets,1);
             else
                 this_count = [];
@@ -82,7 +155,7 @@ function [] = updatecandleinmem(mdefut, instrument)
         if ~isempty(this_bucket_save)
             this_count_save = find(buckets4save == this_bucket_save);
         else
-            if t >= buckets4save(end) && t < buckets4save(end)+buckets4save(end)-buckets4save(end-1)
+            if t >= buckets4save(end)
                 this_count_save = size(buckets4save,1);
             else
                 this_count_save = [];
