bfd2e6468813873abc77fe95965754e0d6896ae1
Yiran <yiran.yang@outlook.com>
2018-07-25 17:05:55 +0800

    bug fix in cStrat::refresh
    we might end up with empty entries of tick or candle as the MDEFut is not speedy up. add try/catch block to make sure that the strat is still running

--- trading/strat/@cStrat/refresh.m
+++ trading/strat/@cStrat/refresh.m
@@ -6,12 +6,17 @@ function [] = refresh(strategy)
     end
     
     if strcmpi(strategy.mode_,'replay') && strcmpi(strategy.status_,'working')
-        instrument = strategy.instruments_.getinstrument{1};
-        tick = strategy.mde_fut_.getlasttick(instrument);
-        candle = strategy.mde_fut_.getlastcandle(instrument);
-        fprintf('runtime:%s; candlebuckettime:%s; price:%s\n',...
-            datestr(tick(1),'yyyy-mm-dd HH:MM:SS'),...
-            datestr(candle{1}(1),'HH:MM'),num2str(candle{1}(5)));
+        try
+            instrument = strategy.instruments_.getinstrument{1};
+            tick = strategy.mde_fut_.getlasttick(instrument);
+            candle = strategy.mde_fut_.getlastcandle(instrument);
+            fprintf('runtime:%s; candlebuckettime:%s; price:%s\n',...
+                datestr(tick(1),'yyyy-mm-dd HH:MM:SS'),...
+                datestr(candle{1}(1),'HH:MM'),num2str(candle{1}(5)));
+        catch e
+            msg = ['error:cStrat:display replay info:',e.message,'\n'];
+            fprintf(msg);
+        end
     end
 
     try
