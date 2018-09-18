d24f4baee2181523ce1b4e70124c45ae422701eb
yangyir <31230104+yangyir@users.noreply.github.com>
2018-09-18 18:46:36 +0800

    login/logoff time control

--- trading/md/@cMDEFut/loadtrades.m
+++ trading/md/@cMDEFut/loadtrades.m
@@ -1,4 +1,22 @@
 function [] = loadtrades(obj,varargin)
 %cMDEFut doesn't load trades
-    variablenotused(obj)
+%     variablenotused(obj)
+    %as the trade array is supposed to be loaded via cOps between 1)
+    %08:50am and 09:00am or 2)between 20:50pm and 21:00pm, we tried to
+    %reconnect the MD then
+    if ~strcmpi(obj.status_,'realtime'), return; end
+    
+    if obj.qms_.isconnect, return; end
+    
+    if isempty(obj.qms_.watcher_.ds)
+        
+    else
+        if isa(obj.qms_.watcher_.ds,'cCTP')
+            countername = obj.qms_.watcher_.ds.char;
+            obj.login('Connection','CTP','CounterName',countername);
+        else
+            error('cMDEFut:data source not supported');
+        end
+    end
+        
 end
--- trading/md/@cMDEFut/savetrades.m
+++ trading/md/@cMDEFut/savetrades.m
@@ -1,4 +1,10 @@
 function [] = savetrades(obj,varargin)
 %note:cMDEFut doesn't save trades
-    variablenotused(obj);
+%     variablenotused(obj);
+% the trades are saved between 15:15pm and 15:25pm, when we can disconnect
+% the MDE
+    if obj.qms_.isconnect
+        obj.logoff;
+    end
+    
 end
