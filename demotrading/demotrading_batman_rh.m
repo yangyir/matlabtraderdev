d7c9969d3a768227e60ae54dd961360c3ec17a60
yangyir <31230104+yangyir@users.noreply.github.com>
2018-09-21 17:18:42 +0800

    rename demotrading_batman_rh

--- demotrading/demotrading_batman_rh.m
+++ demotrading/demotrading_batman_rh.m
@@ -1,5 +1,5 @@
 clear all;clc;delete(timerfindall);cd('C:\yangyiran');
-%%
+%
 countername = 'rh_demo';
 bookname = 'book-demo-rh-single';
 markettype = 'futures';
@@ -9,7 +9,7 @@ combos = rtt_setup('CounterName',countername,'BookName',bookname,'StrategyName',
 fprintf('\ncombos successfully created...\n');
 %%
 code = instruments{1};
-samplefreq = 15;
+samplefreq = 5;
 combos.strategy.setsamplefreq(code,samplefreq);
 combos.strategy.setmaxunits(code,3);
 combos.strategy.setmaxexecutionperbucket(code,1);
@@ -27,8 +27,8 @@ combos.ops.start;
 %%
 combos.strategy.start;
 %%
-price = 4175;
-target = 4150;
+price = 4170;
+target = 4140;
 stoploss = 4200;
 volume = 4;
 combos.strategy.placeentrust(code,'buysell','s','price',price,'volume',volume,'target',target,'stoploss',stoploss);
@@ -37,18 +37,20 @@ combos.strategy.placeentrust(code,'buysell','s','price',price,'volume',volume,'t
 %withdraw
 combos.strategy.withdrawentrusts(code);
 %% 新注册合约到策略
-code2register = 'zn1811';
+code2register = 'T1812';
 combos.strategy.registerinstrument(code2register);
 combos.strategy.setsamplefreq(code2register,samplefreq);
 %%
-price = 21825;
-target = 21750;
-stoploss = 21905;
-volume = 5;
-combos.strategy.placeentrust(code2register,'buysell','s','price',price,'volume',volume,'target',target,'stoploss',stoploss);
+code2trade = 'T1812';
+price = 94.135;
+target = 94.205;
+stoploss = 94.100;
+volume = 1;
+buysell = 'b';
+combos.strategy.placeentrust(code2trade,'buysell',buysell,'price',price,'volume',volume,'target',target,'stoploss',stoploss);
 
 %%
-combos.strategy.longclose(code2register,3,1,'overrideprice',21700);
+combos.strategy.longclose('cu1811',2,1,'overrideprice',49750);
 %%
 combos.mdefut.stop
 %%
