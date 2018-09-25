f49ff6434f74b0f7f8114fbe608f9f3b7fd989f4
yangyir <31230104+yangyir@users.noreply.github.com>
2018-09-26 07:26:13 +0800

    mdefut quotes gui

--- gui/mygui_callback_getquote.m
+++ gui/mygui_callback_getquote.m
@@ -2,9 +2,27 @@ function mygui_callback_getquote( hObject , eventdata , handles )
     variablenotused(hObject);
     variablenotused(eventdata);
     
-    global mdefut;
+    global MDEFUT_INSTANCE;
     
     ctpcode_list = get(handles.instruments.table,'RowName');
     
-
+    n = size(ctpcode_list,1);
+    
+    quote_data = cell(n,7);
+    
+    while ~strcmpi(MDEFUT_INSTANCE.status_,'sleep')
+        for i = 1:n
+            quote_i = MDEFUT_INSTANCE.qms_.getquote(ctpcode_list{i});
+            quote_data{i,1} = num2str(quote_i.bid_size1);
+            quote_data{i,2} = num2str(quote_i.bid1);
+            quote_data{i,3} = num2str(quote_i.ask1);
+            quote_data{i,4} = num2str(quote_i.ask_size1);
+            quote_data{i,5} = datestr(quote_i.update_time1,'HH:MM:SS');
+        end
+        
+        set(handles.instruments.table,'Data',quote_data);
+        
+        pause(1);
+    end
+    
 end
--- gui/mygui_demo_mdefut.m
+++ gui/mygui_demo_mdefut.m
@@ -4,6 +4,16 @@ function handles = mygui_demo_mdefut(mdefut)
     
     handles = mygui_framework_mdefut;
     
+    idx = get(handles.counter.popup_counter,'value');
+    strings = get(handles.counter.popup_counter,'string');
+    countername = strings{idx};
+    
+    MDEFUT_INSTANCE.login('Connection','CTP','CounterName',countername);
+    
+    MDEFUT_INSTANCE.fileioflag_ = false;
+    
+    MDEFUT_INSTANCE.printflag_ = false;
+        
     table_fut = handles.instruments.table;
     
     codectp_list = get(table_fut,'RowName');
@@ -12,5 +22,12 @@ function handles = mygui_demo_mdefut(mdefut)
         MDEFUT_INSTANCE.registerinstrument(codectp_list{i});
     end
     
-    set(handles.instruments.table
+    try
+        delete(timerfindall);
+    catch
+    end
+    
+    MDEFUT_INSTANCE.start;
+    
+    set(handles.operation.button_futquote,'CallBack',{@mygui_callback_getquote, handles});
 end
