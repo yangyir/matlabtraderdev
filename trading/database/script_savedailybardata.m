a9372c06fa6f73baa2e38b0678a8af591c2d4c3c
yangyir <31230104+yangyir@users.noreply.github.com>
2018-10-12 06:58:04 +0800

    latest save data script, and remove hard coded instruments

--- trading/database/script_savedailybardata.m
+++ trading/database/script_savedailybardata.m
@@ -5,255 +5,216 @@ override = true;
 if ~(exist('conn','var') && isa(conn,'cBloomberg'))
     conn = cBloomberg;
 end
+lastbd = getlastbusinessdate;
 
 %%
-saveactivefuturesfrombloomberg(conn);
-lastbd = getlastbusinessdate;
-activefuturesdir = [getenv('DATAPATH'),'activefutures\'];
-filename = ['activefutures_',datestr(lastbd,'yyyymmdd'),'.txt'];
-activefutures = cDataFileIO.loadDataFromTxtFile([activefuturesdir,filename]);
-assetlist = getassetmaptable;
-nasset = length(assetlist);
+% saveactivefuturesfrombloomberg(conn);
+% activefuturesdir = [getenv('DATAPATH'),'activefutures\'];
+% filename = ['activefutures_',datestr(lastbd,'yyyymmdd'),'.txt'];
+% activefutures = cDataFileIO.loadDataFromTxtFile([activefuturesdir,filename]);
+% assetlist = getassetmaptable;
+% nasset = length(assetlist);
+
 %%
 %base metals
-bm = {'copper';'aluminum';'zinc';'lead';'nickel'};
-for i = 1:size(bm,1)
-    for iasset = 1:nasset
-        if strcmpi(assetlist{iasset},bm{i})
-            activefut_i = activefutures{iasset};
-            break
-        end
+list = {'copper';'aluminum';'zinc';'lead';'nickel'};
+for i = 1:size(list,1)
+    futlist_i = listcontracts(list{i},'connection','bloomberg');
+    check_i = getdata(conn.ds_,futlist_i,'last_tradeable_dt');
+    ltd = check_i.last_tradeable_dt;
+    idx = ltd >= lastbd;
+    livefutlist_i = futlist_i(idx);
+    for j = 1:size(livefutlist_i,1)
+        code_j = bbg2ctp(livefutlist_i{j});
+        savedailybarfrombloomberg(conn,code_j,override);
     end
-    savedailybarfrombloomberg(conn,activefut_i,override);
-    %
-    excode = activefut_i(1:2);
-    yystr = activefut_i(3:4);
-    yynum = str2double(yystr);
-    mmstr = activefut_i(5:6);
-    mmnum = str2double(mmstr);
-    %
-    if ~strcmpi(bm{i},'nickel')
-        %SIX consective futures for base metals except for nickel
-        nfut = 5;
-        mmdiff = 1;
-    else
-        %TWO futures for nickel expired every other month
-        nfut = 1;
-        mmdiff = 2;
-    end
-    %
-    for ifut = 1:nfut
-        mmnum = mmnum + mmdiff;
-        if mmnum > 12
-            mmnum = mmnum-12;
-            yynum = yynum+1;
-        end
-        if mmnum < 10
-            mmstr = ['0',num2str(mmnum)];
-        else
-            mmstr = num2str(mmnum);
-        end
-        code = [excode,num2str(yynum),mmstr];
-        savedailybarfrombloomberg(conn,code,override);
-    end
-
 end
-fprintf('done for saving daily bar data for base metal futures......\n');
+fprintf('done for saving daily bar data for base metal futures......\n\n');
 
 %%
 % govtbond futures
-govtbond = {'govtbond_5y';'govtbond_10y'};
-for i = 1:size(govtbond,1)
-    for iasset = 1:nasset
-        if strcmpi(assetlist{iasset},govtbond{i})
-            activefut_i = activefutures{iasset};
-            break
-        end
-    end
-    savedailybarfrombloomberg(conn,activefut_i,override);
-    %
-    for ichar = 1:length(activefut_i)
-        if isnumchar(activefut_i(ichar))
-            break
-        end
-    end
-        
-    excode = activefut_i(1:ichar-1);
-    yystr = activefut_i(ichar:ichar+1);
-    yynum = str2double(yystr);
-    mmstr = activefut_i(ichar+2:ichar+3);
-    mmnum = str2double(mmstr);
-    %
-    %quartetly expired futures
-    nfut = 1;
-    mmdiff = 3;
-    %
-    for ifut = 1:nfut
-        mmnum = mmnum + mmdiff;
-        if mmnum > 12
-            mmnum = mmnum-12;
-            yynum = yynum+1;
-        end
-        if mmnum < 10
-            mmstr = ['0',num2str(mmnum)];
-        else
-            mmstr = num2str(mmnum);
-        end
-        code = [excode,num2str(yynum),mmstr];
-        savedailybarfrombloomberg(conn,code,override);
+list = {'govtbond_5y';'govtbond_10y'};
+for i = 1:size(list,1)
+    futlist_i = listcontracts(list{i},'connection','bloomberg');
+    check_i = getdata(conn.ds_,futlist_i,'last_tradeable_dt');
+    ltd = check_i.last_tradeable_dt;
+    idx = ltd >= lastbd;
+    livefutlist_i = futlist_i(idx);
+    for j = 1:size(livefutlist_i,1)
+        code_j = bbg2ctp(livefutlist_i{j});
+        savedailybarfrombloomberg(conn,code_j,override);
     end
 end
-
-fprintf('done for saving daily bar data for govt bond futures......\n');
+fprintf('done for saving daily bar data for govtbond futures......\n\n');
 
 %%
 % equity index
-eqindex = {'eqindex_300';'eqindex_50';'eqindex_500'};
-for i = 1:size(eqindex,1)
-    for iasset = 1:nasset
-        if strcmpi(assetlist{iasset},eqindex{i})
-            activefut_i = activefutures{iasset};
-            break
-        end
-    end
-    savedailybarfrombloomberg(conn,activefut_i,override);
-    %
-    for ichar = 1:length(activefut_i)
-        if isnumchar(activefut_i(ichar))
-            break
-        end
-    end
-        
-    excode = activefut_i(1:ichar-1);
-    yystr = activefut_i(ichar:ichar+1);
-    yynum = str2double(yystr);
-    mmstr = activefut_i(ichar+2:ichar+3);
-    mmnum = str2double(mmstr);
-    %
-    % next futures contract only ignore the quarter contract for the
-    % time bing
-    nfut = 1;
-    mmdiff = 1;
-    %
-    for ifut = 1:nfut
-        mmnum = mmnum + mmdiff;
-        if mmnum > 12
-            mmnum = mmnum-12;
-            yynum = yynum+1;
-        end
-        if mmnum < 10
-            mmstr = ['0',num2str(mmnum)];
-        else
-            mmstr = num2str(mmnum);
-        end
-        code = [excode,num2str(yynum),mmstr];
-        savedailybarfrombloomberg(conn,code,override);
+list = {'eqindex_300';'eqindex_50';'eqindex_500'};
+for i = 1:size(list,1)
+    futlist_i = listcontracts(list{i},'connection','bloomberg');
+    check_i = getdata(conn.ds_,futlist_i,'last_tradeable_dt');
+    ltd = check_i.last_tradeable_dt;
+    idx = ltd >= lastbd;
+    livefutlist_i = futlist_i(idx);
+    for j = 1:size(livefutlist_i,1)
+        code_j = bbg2ctp(livefutlist_i{j});
+        savedailybarfrombloomberg(conn,code_j,override);
     end
 end
+fprintf('done for saving daily bar data for equity index futures......\n\n');
 
-fprintf('done for saving daily bar data for equity index futures......\n');
 %%
 % precious metals
-pm = {'gold';'silver'};
-for i = 1:size(pm,1)
-    for iasset = 1:nasset
-        if strcmpi(assetlist{iasset},pm{i})
-            activefut_i = activefutures{iasset};
-            break
-        end
-    end
-    savedailybarfrombloomberg(conn,activefut_i,override);
-    %
-    for ichar = 1:length(activefut_i)
-        if isnumchar(activefut_i(ichar))
-            break
-        end
-    end
-        
-    excode = activefut_i(1:ichar-1);
-    yystr = activefut_i(ichar:ichar+1);
-    yynum = str2double(yystr);
-    mmstr = activefut_i(ichar+2:ichar+3);
-    mmnum = str2double(mmstr);
-    %
-    % next futures contract semi-annual
-    nfut = 1;
-    mmdiff = 6;
-    %
-    for ifut = 1:nfut
-        mmnum = mmnum + mmdiff;
-        if mmnum > 12
-            mmnum = mmnum-12;
-            yynum = yynum+1;
-        end
-        if mmnum < 10
-            mmstr = ['0',num2str(mmnum)];
-        else
-            mmstr = num2str(mmnum);
-        end
-        code = [excode,num2str(yynum),mmstr];
-        savedailybarfrombloomberg(conn,code,override);
+list = {'gold';'silver'};
+for i = 1:size(list,1)
+    futlist_i = listcontracts(list{i},'connection','bloomberg');
+    check_i = getdata(conn.ds_,futlist_i,'last_tradeable_dt');
+    ltd = check_i.last_tradeable_dt;
+    idx = ltd >= lastbd;
+    livefutlist_i = futlist_i(idx);
+    for j = 1:size(livefutlist_i,1)
+        code_j = bbg2ctp(livefutlist_i{j});
+        savedailybarfrombloomberg(conn,code_j,override);
     end
 end
-fprintf('done for saving daily bar data for precious metal futures\n');
+fprintf('done for saving daily bar data for precious metal futures\n\n');
 
 %%
-% agriculture for options
-ag_codes_ctp = {'m1809';'m1901';'m1905';'SR809';'SR901';'m1905'};
-for i = 1:size(ag_codes_ctp,1)
-    savedailybarfrombloomberg(conn,ag_codes_ctp{i},override);
+% energy
+list = {'pta';'lldpe';'pp';'methanol';'thermal coal';'crude oil'};
+for i = 1:size(list,1)
+    futlist_i = listcontracts(list{i},'connection','bloomberg');
+    check_i = getdata(conn.ds_,futlist_i,'last_tradeable_dt');
+    ltd = check_i.last_tradeable_dt;
+    idx = ltd >= lastbd;
+    livefutlist_i = futlist_i(idx);
+    for j = 1:size(livefutlist_i,1)
+        code_j = bbg2ctp(livefutlist_i{j});
+        savedailybarfrombloomberg(conn,code_j,override);
+    end
 end
-fprintf('done for saving daily bar data for agriculture futures\n');
+fprintf('done for saving daily bar data for energy futures\n\n');
 
 %%
-
-
-%%
-
-
-
-% agriculture for options
-black_codes_ctp = {'rb1810';'rb1901';'i1809';'i1901'};
-for i = 1:size(black_codes_ctp,1)
-    savedailybarfrombloomberg(conn,black_codes_ctp{i},override);
+list = {'sugar';'cotton';'corn';'egg';...
+            'soybean';'soymeal';'soybean oil';'palm oil';...
+            'rapeseed oil';'rapeseed meal';...
+            'apple';...
+            'rubber'};
+for i = 1:size(list,1)
+    futlist_i = listcontracts(list{i},'connection','bloomberg');
+    check_i = getdata(conn.ds_,futlist_i,'last_tradeable_dt');
+    ltd = check_i.last_tradeable_dt;
+    idx = ltd >= lastbd;
+    livefutlist_i = futlist_i(idx);
+    for j = 1:size(livefutlist_i,1)
+        code_j = bbg2ctp(livefutlist_i{j});
+        savedailybarfrombloomberg(conn,code_j,override);
+    end
 end
-fprintf('done for saving daily bar data for black futures\n');
+fprintf('done for saving daily bar data for agriculture futures\n\n');
 
 %%
-%soymeal
-futures_code_ctp_soymeal = {'m1809';'m1901'};
-strikes_soymeal = (2500:50:3500)';
-
-c_code_ctp_soymeal = cell(size(futures_code_ctp_soymeal,1),size(strikes_soymeal,1));
-p_code_ctp_soymeal = cell(size(futures_code_ctp_soymeal,1),size(strikes_soymeal,1));
-
-for i = 1:size(futures_code_ctp_soymeal,1)
-    for j = 1:size(strikes_soymeal,1)
-        c_code_ = [futures_code_ctp_soymeal{i},'-C-',num2str(strikes_soymeal(j))];
-        savedailybarfrombloomberg(conn,c_code_,override);
-        %
-        p_code_ = [futures_code_ctp_soymeal{i},'-P-',num2str(strikes_soymeal(j))];
-        savedailybarfrombloomberg(conn,p_code_,override);
-    end  
+list = {'coke';'coking coal';'deformed bar';'iron ore';'glass'};
+for i = 1:size(list,1)
+    futlist_i = listcontracts(list{i},'connection','bloomberg');
+    check_i = getdata(conn.ds_,futlist_i,'last_tradeable_dt');
+    ltd = check_i.last_tradeable_dt;
+    idx = ltd >= lastbd;
+    livefutlist_i = futlist_i(idx);
+    for j = 1:size(livefutlist_i,1)
+        code_j = bbg2ctp(livefutlist_i{j});
+        savedailybarfrombloomberg(conn,code_j,override);
+    end
 end
-fprintf('done for soymeal options......\n');
+fprintf('done for saving daily bar data for industry futures\n\n');
 
 %%
-%white sugar
-futures_code_ctp_sugar = {'SR809';'SR901'};
-strikes_sugar = (5000:100:6500)';
-c_code_ctp_sugar = cell(size(futures_code_ctp_sugar,1),size(strikes_sugar,1));
-p_code_ctp_sugar = cell(size(futures_code_ctp_sugar,1),size(strikes_sugar,1));
+% soymeal option
+futlist_i = listcontracts('soymeal','connection','bloomberg');
+check_i = getdata(conn.ds_,futlist_i,'last_tradeable_dt');
+ltd = check_i.last_tradeable_dt;
+idx = ltd >= lastbd;
+livefutlist_i = futlist_i(idx);
+check_i = getdata(conn.ds_,livefutlist_i,'open_int');
+open_int = check_i.open_int;
+open_int_sorted = sort(open_int,'descend');
 
-for i = 1:size(futures_code_ctp_sugar,1)
-    for j = 1:size(strikes_sugar,1)
-        c_code_ = [futures_code_ctp_sugar{i},'C',num2str(strikes_sugar(j))];
-        savedailybarfrombloomberg(conn,c_code_,override);
-        %
-        p_code_ = [futures_code_ctp_sugar{i},'P',num2str(strikes_sugar(j))];
-        savedailybarfrombloomberg(conn,p_code_,override);
-    end  
+idx1 = find(open_int == open_int_sorted(1));
+idx2 = find(open_int == open_int_sorted(2));
+
+check_i = history(conn.ds_,livefutlist_i{idx1},'px_last',lastbd,lastbd);
+px1 = check_i(2);
+futcode1 = bbg2ctp(livefutlist_i{idx1});
+%
+check_i = history(conn.ds_,livefutlist_i{idx2},'px_last',lastbd,lastbd);
+px2 = check_i(2);
+futcode2 = bbg2ctp(livefutlist_i{idx2});
+%
+bucketsize = 50;
+nopt = 10;
+strikes1_soymeal = floor(px1/bucketsize)*bucketsize-(nopt)/2*bucketsize:bucketsize:ceil(px1/bucketsize)*bucketsize+(nopt)/2*bucketsize;
+strikes2_soymeal = floor(px2/bucketsize)*bucketsize-(nopt)/2*bucketsize:bucketsize:ceil(px2/bucketsize)*bucketsize+(nopt)/2*bucketsize;
+
+for j = 1:length(strikes1_soymeal)
+    c_code_ = [futcode1,'-C-',num2str(strikes1_soymeal(j))];
+    savedailybarfrombloomberg(conn,c_code_,override);
+    %
+    p_code_ = [futcode1,'-P-',num2str(strikes1_soymeal(j))];
+    savedailybarfrombloomberg(conn,p_code_,override);
 end
-fprintf('done for white sugar options......\n');
+
+for j = 1:length(strikes2_soymeal)
+    c_code_ = [futcode2,'-C-',num2str(strikes2_soymeal(j))];
+    savedailybarfrombloomberg(conn,c_code_,override);
+    %
+    p_code_ = [futcode2,'-P-',num2str(strikes2_soymeal(j))];
+    savedailybarfrombloomberg(conn,p_code_,override);
+end
+fprintf('done for soymeal options......\n\n');
+
+%%
+% sugar
+futlist_i = listcontracts('sugar','connection','bloomberg');
+check_i = getdata(conn.ds_,futlist_i,'last_tradeable_dt');
+ltd = check_i.last_tradeable_dt;
+idx = ltd >= lastbd;
+livefutlist_i = futlist_i(idx);
+check_i = getdata(conn.ds_,livefutlist_i,'open_int');
+open_int = check_i.open_int;
+open_int_sorted = sort(open_int,'descend');
+
+idx1 = find(open_int == open_int_sorted(1));
+idx2 = find(open_int == open_int_sorted(2));
+
+check_i = history(conn.ds_,livefutlist_i{idx1},'px_last',lastbd,lastbd);
+px1 = check_i(2);
+futcode1 = bbg2ctp(livefutlist_i{idx1});
+%
+check_i = history(conn.ds_,livefutlist_i{idx2},'px_last',lastbd,lastbd);
+px2 = check_i(2);
+futcode2 = bbg2ctp(livefutlist_i{idx2});
+%
+bucketsize = 100;
+nopt = 10;
+strikes1_soymeal = floor(px1/bucketsize)*bucketsize-(nopt)/2*bucketsize:bucketsize:ceil(px1/bucketsize)*bucketsize+(nopt)/2*bucketsize;
+strikes2_soymeal = floor(px2/bucketsize)*bucketsize-(nopt)/2*bucketsize:bucketsize:ceil(px2/bucketsize)*bucketsize+(nopt)/2*bucketsize;
+
+for j = 1:length(strikes1_soymeal)
+    c_code_ = [futcode1,'C',num2str(strikes1_soymeal(j))];
+    savedailybarfrombloomberg(conn,c_code_,override);
+    %
+    p_code_ = [futcode1,'P',num2str(strikes1_soymeal(j))];
+    savedailybarfrombloomberg(conn,p_code_,override);
+end
+
+for j = 1:length(strikes2_soymeal)
+    c_code_ = [futcode2,'C',num2str(strikes2_soymeal(j))];
+    savedailybarfrombloomberg(conn,c_code_,override);
+    %
+    p_code_ = [futcode2,'P',num2str(strikes2_soymeal(j))];
+    savedailybarfrombloomberg(conn,p_code_,override);
+end
+fprintf('done for white sugar options......\n\n');
 
 
--- trading/database/script_saveintradaybardata.m
+++ trading/database/script_saveintradaybardata.m
@@ -7,68 +7,130 @@ if ~(exist('conn','var') && isa(conn,'cBloomberg'))
 end
 
 %%
-%base metals
-bm_codes_ctp = {'cu1810';'cu1811';'cu1812';'cu1901';'cu1902';'cu1903';...
-    'al1810';'al1811';'al1812';'al1901';'al1902';'al1903';...
-    'zn1810';'zn1811';'zn1812';'zn1901';'zn1902';'zn1903';...
-    'pb1810';'pb1811';'pb1812';'pb1901';'pb1902';'pb1903';...
-    'ni1811';'ni1901'};
+saveactivefuturesfrombloomberg(conn);
+activefuturesdir = [getenv('DATAPATH'),'activefutures\'];
+filename = ['activefutures_',datestr(lastbd,'yyyymmdd'),'.txt'];
+activefutures = cDataFileIO.loadDataFromTxtFile([activefuturesdir,filename]);
+assetlist = getassetmaptable;
+nasset = length(assetlist);
 
-for i = 1:size(bm_codes_ctp,1)
-    saveintradaybarfrombloomberg(conn,bm_codes_ctp{i},override);
+%%
+%%
+%base metals
+list = {'copper';'aluminum';'zinc';'lead';'nickel'};
+for i = 1:size(list,1)
+    futlist_i = listcontracts(list{i},'connection','bloomberg');
+    check_i = getdata(conn.ds_,futlist_i,'last_tradeable_dt');
+    ltd = check_i.last_tradeable_dt;
+    idx = ltd >= lastbd;
+    livefutlist_i = futlist_i(idx);
+    for j = 1:size(livefutlist_i,1)
+        code_j = bbg2ctp(livefutlist_i{j});
+        saveintradaybarfrombloomberg(conn,code_j,override);
+    end
 end
-fprintf('done for saving intraday bar data for base metal futures......\n');
+fprintf('done for saving intraday bar data for base metal futures......\n\n');
 
 %%
 % govtbond futures
-govtbond_codes_ctp = {'TF1812';'TF1903';...
-    'T1812';'T1903'};
-
-for i = 1:size(govtbond_codes_ctp,1)
-    saveintradaybarfrombloomberg(conn,govtbond_codes_ctp{i},override);
+list = {'govtbond_5y';'govtbond_10y'};
+for i = 1:size(list,1)
+    futlist_i = listcontracts(list{i},'connection','bloomberg');
+    check_i = getdata(conn.ds_,futlist_i,'last_tradeable_dt');
+    ltd = check_i.last_tradeable_dt;
+    idx = ltd >= lastbd;
+    livefutlist_i = futlist_i(idx);
+    for j = 1:size(livefutlist_i,1)
+        code_j = bbg2ctp(livefutlist_i{j});
+        saveintradaybarfrombloomberg(conn,code_j,override);
+    end
 end
-fprintf('done for saving intraday bar data for govt bond futures......\n');
+fprintf('done for saving intraday bar data for govt bond futures......\n\n');
 
 %%
 % equity index futures
-eqindex_codes_ctp = {'IF1809';'IF1810';'IF1811';'IF1812';...
-    'IH1809';'IH1810';'IH1811';'IH1812';...
-    'IC1809';'IC1810';'IC1811';'IC1812'};
-
-for i = 1:size(eqindex_codes_ctp,1)
-    saveintradaybarfrombloomberg(conn,eqindex_codes_ctp{i},override);
+list = {'eqindex_300';'eqindex_50';'eqindex_500'};
+for i = 1:size(list,1)
+    futlist_i = listcontracts(list{i},'connection','bloomberg');
+    check_i = getdata(conn.ds_,futlist_i,'last_tradeable_dt');
+    ltd = check_i.last_tradeable_dt;
+    idx = ltd >= lastbd;
+    livefutlist_i = futlist_i(idx);
+    for j = 1:size(livefutlist_i,1)
+        code_j = bbg2ctp(livefutlist_i{j});
+        saveintradaybarfrombloomberg(conn,code_j,override);
+    end
 end
-fprintf('done for saving intraday bar data for equity index futures......\n');
+fprintf('done for saving intraday bar data for equity index futures......\n\n');
 
 %%
-%precious metals
-pm_codes_ctp = {'au1812';'ag1812'};
-
-for i = 1:size(pm_codes_ctp,1)
-    saveintradaybarfrombloomberg(conn,pm_codes_ctp{i},override);
+% precious metals
+list = {'gold';'silver'};
+for i = 1:size(list,1)
+    futlist_i = listcontracts(list{i},'connection','bloomberg');
+    check_i = getdata(conn.ds_,futlist_i,'last_tradeable_dt');
+    ltd = check_i.last_tradeable_dt;
+    idx = ltd >= lastbd;
+    livefutlist_i = futlist_i(idx);
+    for j = 1:size(livefutlist_i,1)
+        code_j = bbg2ctp(livefutlist_i{j});
+        saveintradaybarfrombloomberg(conn,code_j,override);
+    end
 end
-fprintf('done for saving intraday bar data for precious metal futures\n');
+fprintf('done for saving intraday bar data for precious metal futures\n\n');
 
 %%
-%rebal
-rb_codes_ctp = {'rb1810';'rb1901'};
-
-for i = 1:size(rb_codes_ctp,1)
-    saveintradaybarfrombloomberg(conn,rb_codes_ctp{i},override);
+% energy
+list = {'pta';'lldpe';'pp';'methanol';'thermal coal';'crude oil'};
+for i = 1:size(list,1)
+    futlist_i = listcontracts(list{i},'connection','bloomberg');
+    check_i = getdata(conn.ds_,futlist_i,'last_tradeable_dt');
+    ltd = check_i.last_tradeable_dt;
+    idx = ltd >= lastbd;
+    livefutlist_i = futlist_i(idx);
+    for j = 1:size(livefutlist_i,1)
+        code_j = bbg2ctp(livefutlist_i{j});
+        saveintradaybarfrombloomberg(conn,code_j,override);
+    end
 end
-fprintf('done for saving intraday bar data for rb futures\n');
+fprintf('done for saving intraday bar data for energy futures\n\n');
 
 %%
-%iron ore
-ironore_codes_ctp = {'i1809';'i1901';'i1905'};
-
-for i = 1:size(ironore_codes_ctp,1)
-    saveintradaybarfrombloomberg(conn,ironore_codes_ctp{i},override);
+list = {'sugar';'cotton';'corn';'egg';...
+            'soybean';'soymeal';'soybean oil';'palm oil';...
+            'rapeseed oil';'rapeseed meal';...
+            'apple';...
+            'rubber'};
+for i = 1:size(list,1)
+    futlist_i = listcontracts(list{i},'connection','bloomberg');
+    check_i = getdata(conn.ds_,futlist_i,'last_tradeable_dt');
+    ltd = check_i.last_tradeable_dt;
+    idx = ltd >= lastbd;
+    livefutlist_i = futlist_i(idx);
+    for j = 1:size(livefutlist_i,1)
+        code_j = bbg2ctp(livefutlist_i{j});
+        saveintradaybarfrombloomberg(conn,code_j,override);
+    end
 end
-fprintf('done for saving intraday bar data for iron ore futures\n');
+fprintf('done for saving intraday bar data for agriculture futures\n\n');
+
+%%
+list = {'coke';'coking coal';'deformed bar';'iron ore';'glass'};
+for i = 1:size(list,1)
+    futlist_i = listcontracts(list{i},'connection','bloomberg');
+    check_i = getdata(conn.ds_,futlist_i,'last_tradeable_dt');
+    ltd = check_i.last_tradeable_dt;
+    idx = ltd >= lastbd;
+    livefutlist_i = futlist_i(idx);
+    for j = 1:size(livefutlist_i,1)
+        code_j = bbg2ctp(livefutlist_i{j});
+        saveintradaybarfrombloomberg(conn,code_j,override);
+    end
+end
+fprintf('done for saving intraday bar data for industry futures\n\n');
 
 %%
 %clear variables
 clear i
-clear override conn bm_codes_ctp govtbond_codes_ctp pm_codes_ctp
+clear override conn list futlist_i check_i ltd idx livefutlist_i
 
