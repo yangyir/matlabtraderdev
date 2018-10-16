9c78a3e81c5b08c49d59a604e65921e4ff35167a
yangyir <31230104+yangyir@users.noreply.github.com>
2018-10-16 12:14:06 +0800

    update file by removing redundant comments

--- trading/strat/@cStrat/getavailablefund.m
+++ trading/strat/@cStrat/getavailablefund.m
@@ -1,10 +1,14 @@
 function val = getavailablefund(obj)
+%cStrat
     currentmargin = obj.getcurrentmargin;
     frozenmargin = obj.getfrozenmargin;
-    if isempty(obj.totalequity_)
+    if isempty(obj.preequity_)
         val = 0;
         fprintf('cStrat:getavailablefund:initial level of fund not set,pls call setavailablefund func\n');
     else
-        val = obj.totalequity_ - currentmargin - frozenmargin;  
+        [runningpnl,closedpnl] = obj.helper_.calcpnl('mdefut',obj.mde_fut_);
+        totalpnl = sum(sum(runningpnl+closedpnl));
+        obj.currentequity_ = obj.preequity_ + totalpnl;
+        val = obj.currentequity_ - currentmargin - frozenmargin;
     end
 end
