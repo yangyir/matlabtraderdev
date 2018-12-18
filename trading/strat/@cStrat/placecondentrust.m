1da191c3e40ea18a49b74cf402e53619e4cc77c8
yangyir <31230104+yangyir@users.noreply.github.com>
2018-12-19 00:55:55 +0800

    Update placecondentrust.m

--- trading/strat/@cStrat/placecondentrust.m
+++ trading/strat/@cStrat/placecondentrust.m
@@ -1,6 +1,6 @@
 function [ret,e,msg] = placecondentrust(obj,instrument,varargin)
 %cStrat
-if ~strcmpi(obj.status_,'working')
+    if ~strcmpi(obj.status_,'working')
         ret = 0;
         e = [];
         msg = sprintf('%s:placecondentrust:it is not allowed when the strategy is not working....',class(obj));
@@ -60,5 +60,62 @@ if ~strcmpi(obj.status_,'working')
     pxtarget = p.Results.Limit;
     pxstoploss = p.Results.Stop;
     riskmanagername = p.Results.RiskManagerName;
+    
+    %sanity check to make sure that price/target/stoploss are correctly 
+    if directionnum == 1
+        %pxstoploss < price < pxtarget
+        if ~(pxstoploss < price && price < pxtarget)
+            ret = 0;
+            e = [];
+            msg = sprintf('%s:invalid target/stoploss input for long open trade...',class(obj));
+            fprintf('%s\n',msg);
+            return
+        end
+    elseif directionnum == -1
+        %pxstoploss > price > pxtarget
+        if ~(pxstoploss > price && price > pxtarget)
+            ret = 0;
+            e = [];
+            msg = sprintf('%s:invalid target/stoploss input for short open trade...',class(obj));
+            fprintf('%s\n',msg);
+            return
+        end
+    end
+    
+    try
+        samplefreqstr = obj.riskcontrols_.getconfigvalue('code',instrument.code_ctp,'propname','samplefreq');
+    catch err
+        %note:the code is executed here because
+        %1)either riskcontrols are not initiated at all
+        %2)or riskcontrols for such instrument is not set
+        ret = 0;
+        e = [];
+        msg = sprintf('%s',['Error:',err.message]);
+        fprintf('%s\n',msg);
+        return
+    end
+    
+    signalinfo = struct('name','manual',...
+        'riskmanagername',riskmanagername,...
+        'instrument',instrument,...
+        'frequency',samplefreqstr,...
+        'pxtarget',pxtarget,...
+        'pxstoploss',pxstoploss);
+     
+    if directionnum == 1
+        if strcmpi(offsetstr,'open')
+            [ret,e,msg] = obj.condlongopen(instrument.code_ctp,price,abs(lots),...
+               'signalinfo',signalinfo);
+        else
+            error('condlongclose not implemented')
+        end
+    elseif directionnum == -1
+        if strcmpi(offsetstr,'open')
+            [ret,e,msg] = obj.shortopen(instrument.code_ctp,price,abs(lots),...
+                'signalinfo',signalinfo);
+        else
+            error('condshortclose not implemented')
+        end 
+    end
     
 end
