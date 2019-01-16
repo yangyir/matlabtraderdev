3db0d0c6fb79208f2bfa4eab127412f57cddaec0
yangyir <31230104+yangyir@users.noreply.github.com>
2019-01-16 18:07:00 +0800

    bug fix

--- trading/database/@cBloomberg/history.m
+++ trading/database/@cBloomberg/history.m
@@ -1,7 +1,10 @@
 function data = history(obj,instrument,fields,fromdate,todate)
     if ~iscell(fields) && ischar(fields), fields = {fields}; end
-
-    data = obj.ds_.history(instrument.code_bbg,fields,fromdate,todate);
+    if isa(instrument,'cInstrument')
+        data = obj.ds_.history(instrument.code_bbg,fields,fromdate,todate);
+    else
+        data = obj.ds_.history(code_bbg,fields,fromdate,todate);
+    end
 end
 %end of history
 
