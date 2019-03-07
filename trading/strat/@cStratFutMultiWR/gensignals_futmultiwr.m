6b6e32863d245789e1e2ca46b1c0447c89ec69ae
yangyir <31230104+yangyir@users.noreply.github.com>
2019-03-07 15:28:55 +0800

    Update gensignals_futmultiwr.m

--- trading/strat/@cStratFutMultiWR/gensignals_futmultiwr.m
+++ trading/strat/@cStratFutMultiWR/gensignals_futmultiwr.m
@@ -37,8 +37,8 @@ function signals = gensignals_futmultiwr(strategy)
                 error('ERROR:%s:gensignals_futmultiwr:last candle shall be excluded with follow mode',class(obj))
             end
             
+            ti = strategy.mde_fut_.calc_technical_indicators(instruments{i},'includeextraresults',true);
             if strcmpi(wrmode,'classic')
-                ti = strategy.mde_fut_.calc_technical_indicators(instruments{i});
                 maxpx_last = ti{1}(2);
                 minpx_last = ti{1}(3);
                 if strategy.printflag_ && strcmpi(strategy.mode_,'replay')
@@ -48,22 +48,18 @@ function signals = gensignals_futmultiwr(strategy)
                         datestr(tick(1),'yyyy-mm-dd HH:MM:SS'),instruments{i}.code_ctp,num2str(tick(4)),strategy.wr_(i),...
                         num2str(maxpx_last),num2str(minpx_last),num2str(ti{1}(4)));
                 end
-            elseif strcmpi(wrmode,'flash')
-                [maxpx_last,maxpx_before,~,maxcandle] = strategy.getmaxnperiods(instruments{i},'IncludeLastCandle',includelastcandle);
-                [minpx_last,minpx_before,~,mincandle] = strategy.getminnperiods(instruments{i},'IncludeLastCandle',includelastcandle);
-            elseif strcmpi(wrmode,'flashma')
-                ti = strategy.mde_fut_.calc_technical_indicators(instruments{i},'includeextraresults',true);
+            elseif strcmpi(wrmode,'flash') || strcmpi(wrmode,'flashma')
                 maxpx_last = ti{1}(2);
                 minpx_last = ti{1}(3);
                 maxpx_before = ti{1}(end-1);
                 minpx_before = ti{1}(end);
                 maxcandle = ti{3};
                 mincandle = ti{4};
-                wrseries = ti{2};
             elseif (strcmpi(wrmode,'reverse') || strcmpi(wrmode,'follow'))
-                [maxpx_last,~,~,maxcandle] = strategy.getmaxnperiods(instruments{i},'IncludeLastCandle',includelastcandle);
-                [minpx_last,~,~,mincandle] = strategy.getminnperiods(instruments{i},'IncludeLastCandle',includelastcandle);
-                %
+                maxpx_last = ti{1}(2);
+                minpx_last = ti{1}(3);
+                maxcandle = ti{3};
+                mincandle = ti{4};
                 %refresh max and min prices
                 maxpx_before = strategy.maxnperiods_(i);
                 if maxpx_last > maxpx_before || maxpx_before <= 0, strategy.maxnperiods_(i) = maxpx_last;end
@@ -76,7 +72,6 @@ function signals = gensignals_futmultiwr(strategy)
             else
                 error('ERROR:%s:gensignals_futmultiwr:unknown wr mode!!!',class(strategy))
             end
-            
             %
             if strcmpi(wrmode,'classic')
                 %note:in mode 'classic', we generate signal based on WR, i.e.
@@ -116,99 +111,63 @@ function signals = gensignals_futmultiwr(strategy)
                 end
                 signals{i,1} = {};
                 %
-            elseif strcmpi(wrmode,'reverse') || strcmpi(wrmode,'flash') || strcmpi(wrmode,'follow') || strcmpi(wrmode,'flashma')
-                if ( strcmpi(wrmode,'reverse') || strcmpi(wrmode,'follow') )
-                    %note:first time set entrusts
-                    %IMPORTANT:shall be open entrust
-                    n = strategy.helper_.numberofentrusts('Offset','Open','Code',instruments{i}.code_ctp);
-                    if n == 0 && (strcmpi(wrmode,'reverse') ||strcmpi(wrmode,'follow'))
-                        signals{i,1} = struct('name','williamsr',...
-                            'instrument',instruments{i},...
-                            'frequency',samplefreqstr,...
-                            'lengthofperiod',lengthofperiod,...
-                            'checkflag',1,...
-                            'highesthigh',maxpx_last,...
-                            'lowestlow',minpx_last,...
-                            'highestcandle',maxcandle,...
-                            'lowestcandle',mincandle,...
-                            'wrmode',wrmode);
-                        continue;
-                    end
-                elseif strcmpi(wrmode,'flash')
-                    candlesticks = strategy.mde_fut_.getcandles(instruments{i});
-                    if ~isempty(candlesticks)
-                        candlesticks = candlesticks{1};
-                        latesthigh = candlesticks(end,3);
-                        latestlow = candlesticks(end,4);
-                        %double check whether the latest high or low has
-                        %breached the old ones
-                        if latesthigh <= maxpx_last && latestlow >= minpx_last
-                            isgensignal = true;
-                        else
-                            isgensignal = false;
-                        end
-                    else
-                        %the code reaches here is because candles not
-                        %poped up yet. the signal can be generated purely
-                        %based on historical data
+            elseif strcmpi(wrmode,'reverse') || strcmpi(wrmode,'follow')
+                %note:first time set entrusts
+                %IMPORTANT:shall be open entrust
+                n = strategy.helper_.numberofentrusts('Offset','Open','Code',instruments{i}.code_ctp);
+                if n == 0
+                    signals{i,1} = struct('name','williamsr',...
+                        'instrument',instruments{i},...
+                        'frequency',samplefreqstr,...
+                        'lengthofperiod',lengthofperiod,...
+                        'checkflag',1,...
+                        'highesthigh',maxpx_last,...
+                        'lowestlow',minpx_last,...
+                        'highestcandle',maxcandle,...
+                        'lowestcandle',mincandle,...
+                        'wrmode',wrmode);
+                    continue;
+                end
+                signals{i,1} = {};
+                %
+            elseif strcmpi(wrmode,'flash') || strcmpi(wrmode,'flashma')
+                candlesticks = strategy.mde_fut_.getcandles(instruments{i});
+                if ~isempty(candlesticks)
+                    candlesticks = candlesticks{1};
+                    latesthigh = candlesticks(end,3);
+                    latestlow = candlesticks(end,4);
+                    %double check whether the latest high or low has breached the old ones
+                    %if it has breached, we need to check again once the
+                    %candle is fully poped up at a later stage
+                    if latesthigh <= maxpx_last && latestlow >= minpx_last
                         isgensignal = true;
+                    else
+                        isgensignal = false;
                     end
-
-                    if isgensignal && (maxpx_last > maxpx_before || minpx_last < minpx_before ...
+                else
+                    %the code reaches here is because candles not
+                    %poped up yet. the signal can be generated purely
+                    %based on historical data
+                    isgensignal = true;
+                end
+                if isgensignal && (maxpx_last > maxpx_before || minpx_last < minpx_before ...
                         || maxpx_before <= 0 || minpx_before <= 0)
-                        if maxpx_last > maxpx_before || maxpx_before <= 0
-                            checkflag = 1;
-                        elseif minpx_last < minpx_before || minpx_before <= 0
-                            checkflag = -1;
-                        end
-                        signals{i,1} = struct('name','williamsr',...
-                            'instrument',instruments{i},...
-                            'frequency',samplefreqstr,...
-                            'lengthofperiod',lengthofperiod,...
-                            'checkflag',checkflag,...
-                            'highesthigh',maxpx_last,...
-                            'lowestlow',minpx_last,...
-                            'highestcandle',maxcandle,...
-                            'lowestcandle',mincandle,...
-                            'wrmode',wrmode);
-                        continue;
+                    if maxpx_last > maxpx_before || maxpx_before <= 0
+                        checkflag = 1;
+                    elseif minpx_last < minpx_before || minpx_before <= 0
+                        checkflag = -1;
                     end
-                elseif strcmpi(wrmode,'flashma')
-                    candlesticks = strategy.mde_fut_.getcandles(instruments{i});
-                    if ~isempty(candlesticks)
-                        candlesticks = candlesticks{1};
-                        latesthigh = candlesticks(end,3);
-                        latestlow = candlesticks(end,4);
-                        %double check whether the latest high or low has
-                        %breached the old ones
-                        if latesthigh <= maxpx_last && latestlow >= minpx_last
-                            isgensignal = true;
-                        else
-                            isgensignal = false;
-                        end
-                    else
-                        isgensignal = true;
-                    end
-
-                    if isgensignal && (maxpx_last > maxpx_before || minpx_last < minpx_before)
-                        if maxpx_last > maxpx_before
-                            checkflag = 1;
-                        elseif minpx_last < minpx_before
-                            checkflag = -1;
-                        end
-                        signals{i,1} = struct('name','williamsr',...
-                            'instrument',instruments{i},...
-                            'frequency',samplefreqstr,...
-                            'lengthofperiod',lengthofperiod,...
-                            'checkflag',checkflag,...
-                            'highesthigh',maxpx_last,...
-                            'lowestlow',minpx_last,...
-                            'highestcandle',maxcandle,...
-                            'lowestcandle',mincandle,...
-                            'wrmode',wrmode,...
-                            'wrseries',wrseries);
-                        continue;
-                    end                    
+                    signals{i,1} = struct('name','williamsr',...
+                        'instrument',instruments{i},...
+                        'frequency',samplefreqstr,...
+                        'lengthofperiod',lengthofperiod,...
+                        'checkflag',checkflag,...
+                        'highesthigh',maxpx_last,...
+                        'lowestlow',minpx_last,...
+                        'highestcandle',maxcandle,...
+                        'lowestcandle',mincandle,...
+                        'wrmode',wrmode);
+                    continue;
                 end
                 signals{i,1} = {};
                 %
