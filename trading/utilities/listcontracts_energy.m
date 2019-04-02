01fb36a920eafe78d11c3494465e2ffcf2c94143
yangyir <31230104+yangyir@users.noreply.github.com>
2019-04-03 01:44:24 +0800

    Update listcontracts_energy.m
    for crude

--- trading/utilities/listcontracts_energy.m
+++ trading/utilities/listcontracts_energy.m
@@ -59,7 +59,7 @@ else
 %             (curr_yy-1-init_yy)*12+...  %contracts between the 2nd and last year
 %             curr_mm+...                 %contracts for this year
 %             3;                          %now listed and activly traded
-        n = 5;
+        n = 6;
     else
         n = (curr_mm-init_mm+1)+11;
     end
@@ -82,6 +82,8 @@ while i<=n
             mm = 15;
         elseif i == 5
             mm = 16;
+        else
+            mm = n+11;
         end
     end
     if mod(mm,12)==0
