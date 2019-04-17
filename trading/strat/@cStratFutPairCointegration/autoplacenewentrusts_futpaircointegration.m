20ef4934f4f1c0d164544ec1951ec85d4e2adecf
yangyir <31230104+yangyir@users.noreply.github.com>
2019-04-17 15:06:54 +0800

    update methods for cointegration

--- trading/strat/@cStratFutPairCointegration/autoplacenewentrusts_futpaircointegration.m
+++ trading/strat/@cStratFutPairCointegration/autoplacenewentrusts_futpaircointegration.m
@@ -1,4 +1,42 @@
-function [] = autoplacenewentrusts_futpaircointegration(obj,signals)
+function [] = autoplacenewentrusts_futpaircointegration(strategy,signals)
 %cStratFutPairCointegration
+    if isempty(strategy.helper_), error('%s::autoplacenewentrusts::missing helper!!!',class(strategy));end
+    
+    if isempty(signals), return; end
+    
+    volume_exist = zeros(2,1);
+    for i = 1:2
+        signal = signals{1};
+        instrument = signal.instrument;
+        autotrade = strategy.riskcontrols_.getconfigvalue('code',instrument.code_ctp,'propname','autotrade');
+        if ~autotrade, return; end
+        try
+            [flag,idx] = strategy.helper_.book_.hasposition(instrument);
+        catch
+            flag = false;
+            idx = 0;
+        end
+        if ~flag
+            volume_exist(i) = 0;
+        else
+            pos = strategy.helper_.book_.positions_{idx};
+            volume_exist(i) = pos.position_total_;
+        end
+    end
+    
+    
+    if volume_exist(1) == 0 && volume_exist(2) == 0
+        for i = 1:2
+            
+        end
+        
+        
+    elseif volume_exist(1) ~= 0 && volume_exist(2) ~= 0
+    else
+        error('%s::autoplacenewentrusts::internal error!!!',class(strategy));
+    end
+    
+    
+
     return
 end
--- trading/strat/@cStratFutPairCointegration/gensignals_futpaircointegration.m
+++ trading/strat/@cStratFutPairCointegration/gensignals_futpaircointegration.m
@@ -34,7 +34,7 @@ function [signals] = gensignals_futpaircointegration(strategy)
     count = size(strategy.data_,1);
     doRebalance = mod(count-M,N) == 0;
     if doRebalance
-        fprintf('rebalancing...\n');
+%         fprintf('rebalancing...\n');
         [h,~,~,~,reg1] = egcitest(strategy.data_(end-M+1:end,2:3));
         if h ~= 0
             strategy.cointegrationparams_ = reg1;
@@ -50,6 +50,7 @@ function [signals] = gensignals_futpaircointegration(strategy)
     
     lasttick1 = strategy.mde_fut_.getlasttick(instruments{1});
     lasttick2 = strategy.mde_fut_.getlasttick(instruments{2});
+    
     if isempty(lasttick1) || isempty(lasttick2)
         signals = {};
         return
@@ -59,15 +60,41 @@ function [signals] = gensignals_futpaircointegration(strategy)
     check = lasttick1(4) - (params.coeff(1) + params.coeff(2) * lasttick2(4));
     check = check / params.RMSE;
     
+    samplefreqstr = strategy.riskcontrols_.getconfigvalue('code',instruments{1}.code_ctp,'propname','samplefreq');
+    
     if check > strategy.upperbound_
+        signals = cell(2,1);
         fprintf('5y overbought:sell 5y at %s and buy 10y at %s\n',num2str(lasttick1(4)),num2str(lasttick2(4)))
+        signals{1,1} = struct('name','paircointegration',...
+                        'instrument',instruments{1},...
+                        'frequency',samplefreqstr,...
+                        'direction',-1,...
+                        'volume',params.coeff(2));
+        signals{2,1} = struct('name','paircointegration',...
+                        'instrument',instruments{2},...
+                        'frequency',samplefreqstr,...
+                        'direction',1,...
+                        'volume',1);
+        return
     elseif check < strategy.lowerbound_
+        signals = cell(2,1);
         fprintf('5y oversold:buy 5y at %s and sell 10y at %s\n',num2str(lasttick1(4)),num2str(lasttick2(4)))
+        signals{1,1} = struct('name','paircointegration',...
+                        'instrument',instruments{1},...
+                        'frequency',samplefreqstr,...
+                        'direction',1,...
+                        'volume',params.coeff(2));
+        signals{2,1} = struct('name','paircointegration',...
+                        'instrument',instruments{2},...
+                        'frequency',samplefreqstr,...
+                        'direction',-1,...
+                        'volume',1);
+        return
     else
         fprintf('%4.1f\n',check);
+        signals = {};
+        return
     end
-    
-    signals = {};
         
 end
 
