428560c7c2e258c0751521b6a717e338e7faacdc
yangyir <31230104+yangyir@users.noreply.github.com>
2019-05-07 08:41:59 +0800

    Update loadmktdata.m

--- trading/strat/@cStratFutPairCointegration/loadmktdata.m
+++ trading/strat/@cStratFutPairCointegration/loadmktdata.m
@@ -1,5 +1,6 @@
 function [] = loadmktdata(obj,varargin)
-    fprintf('haha,we are here!\n');
+    fprintf('%s:loadmktdata:%s\n',class(obj),datestr(now,'yyyymmdd HH:MM:SS'));
+%     fprintf('haha,we are here!\n');
 %     if isempty(obj.data_)
 %         combos.strategy.initdata;
 %     else
