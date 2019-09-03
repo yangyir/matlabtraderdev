af7ab7f86330a7ea32a65a4f894ea37bdbc46972
yangyir <31230104+yangyir@users.noreply.github.com>
2019-09-03 08:32:02 +0800

    Create regressiontest_tdsq_imperfect.m

---
+++ regressiontest/cstrat/tdsq/regressiontest_tdsq_imperfect.m
@@ -0,0 +1,31 @@
+%%
+try
+    clc;
+    delete(timerfindall);
+catch
+end
+combo = rtt_setup('countername','ccb_ly_fut',...
+    'bookname','booktdsq-reg',...
+    'strategyname','tdsq',...
+    'riskconfigfilename','config_tdsq_imperfectonly_regressiontest.txt',...
+    'initialfundlevel',1e6,...
+    'mode','replay',...
+    'replayfromdate','2019-09-02','replaytodate','2019-09-02');
+%%
+combo.mdefut.printflag_ = false;
+combo.ops.printflag_ = true;
+combo.ops.print_timeinterval_ = 15*60;
+combo.strategy.printflag_ = false;
+%%
+combo.mdefut.start;
+combo.ops.start;
+combo.strategy.start;
+%%
+combo.mdefut.stop;
+%%
+timers = timerfindall;
+fprintf('\n');
+for i = 1:length(timers)
+    fprintf('%25s:%3s\n',timers(i).name,timers(i).running);
+end
+    
