b801f2b77f8bfd11e33aac96b93c962bde7b2e3c
yangyir <31230104+yangyir@users.noreply.github.com>
2019-09-12 13:36:27 +0800

    Create bks_tdsq_ni1911.m

---
+++ backtest/strategy-tdsq/script/bks_tdsq_ni1911.m
@@ -0,0 +1,28 @@
+%% perfect
+tradesperfect = bkf_gentrades_tdsqperfect('ni1911',p,bs,ss,lvlup,lvldn,bc,sc,sns,macdvec,sigvec,'riskmode','macd');
+fprintf('perfect trades:\n');
+bkf_printtrades_tdsq(tradesperfect);
+%% imperfect
+tradesimperfect = bkf_gentrades_tdsqimperfect('ni1911',p,bs,ss,lvlup,lvldn,bc,sc,sns,macdvec,sigvec,'riskmode','macd','openapproach','new');
+fprintf('imperfect trades:\n');
+bkf_printtrades_tdsq(tradesimperfect);
+%% single lvlup
+tradessinglelvlup = bkf_gentrades_tdsqsinglelvlup('ni1911',p,bs,ss,lvlup,lvldn,bc,sc,sns,macdvec,sigvec,'riskmode','macd-setup');
+fprintf('single lvlup trades:\n');
+bkf_printtrades_tdsq(tradessinglelvlup);
+%% single lvldn
+tradessinglelvldn = bkf_gentrades_tdsqsinglelvldn('ni1911',p,bs,ss,lvlup,lvldn,bc,sc,sns,macdvec,sigvec,'riskmode','macd-setup');
+fprintf('single lvldn trades:\n');
+bkf_printtrades_tdsq(tradessinglelvldn);
+%% double range
+tradesdoublerange = bkf_gentrades_tdsqdoublerange('ni1911',p,bs,ss,lvlup,lvldn,bc,sc,sns,macdvec,sigvec,'riskmode','macd');
+fprintf('double range trades:\n');
+bkf_printtrades_tdsq(tradesdoublerange);
+%%
+tradesdoublebullish = bkf_gentrades_tdsqdoublebullish('ni1911',p,bs,ss,lvlup,lvldn,bc,sc,sns,macdvec,sigvec,'riskmode','macd-setup');
+fprintf('double bullish trades:\n');
+bkf_printtrades_tdsq(tradesdoublebullish);
+%%
+tradesdoublebearish = bkf_gentrades_tdsqdoublebearish('ni1911',p,bs,ss,lvlup,lvldn,bc,sc,sns,macdvec,sigvec,'riskmode','macd-setup');
+fprintf('double bearish trades:\n');
+bkf_printtrades_tdsq(tradesdoublebearish);
