dd55fee14df58eb531d80a01a1813183c348f63c
yangyir <31230104+yangyir@users.noreply.github.com>
2019-10-09 00:10:53 +0800

    Update bks_straddle_50etf.m

--- backtest/strategy-syntheticstraddle/script/bks_straddle_50etf.m
+++ backtest/strategy-syntheticstraddle/script/bks_straddle_50etf.m
@@ -34,7 +34,7 @@ finalrets = zeros(n,2);
 limit = 1.5;
 stop = 0.9;
 dayscut = 42;
-criterial = 'delta';
+criterial = 'pv';
 for i = 1:n
     straddle_i = straddles.node_(i);
     unwindidx = straddle_i.unwindinfo('limit',limit,'stop',stop,'dayscut',dayscut,'criterial',criterial);
@@ -51,7 +51,7 @@ rate = 0.03;
 nbdaysperyear = 252;
 dailyfiret = rate/nbdaysperyear*ones(n,1);
 dailyoptret = dailyfiret.*finalrets(:,1);
-daysvec = [1:1:n];daysvec = daysvec';
+daysvec = 1:1:n;daysvec = daysvec';
 figure(2);
 subplot(221);plot(cumsum(dailyfiret),'b');hold on;
 plot(cumsum(dailyoptret),'r');hold off;legend('fixed income','straddle','location','northwest');
@@ -80,7 +80,7 @@ subplot(224);plot(annualret(:,1),'b');hold on;
 plot(annualret(:,2),'r');hold off;legend('fixed income','straddle','location','northwest');
 title('annual return over business days')
 %% with leverage
-leverage = 2;
+leverage = 1.2;
 dailyfiret2 = rate/nbdaysperyear*ones(n,1)*leverage;
 dailyoptret_leveraged = dailyfiret2.*finalrets(:,1);
 figure(3);
@@ -121,10 +121,121 @@ pvs = zeros(n,1);
 for i = 1:n
     pvs(i) = straddles.node_(i).pvs_(1);
 end
+%% with make sense leverage and running step by step
+fundavailable = 1-1/(1+rate);
+pr = 1/6;
+premium0 = fundavailable*pr/dayscut;
+cash = zeros(n,1);
+pvholding = zeros(n,1);
+fundused = zeros(n,1);
+proceeds = zeros(n,1);
+nlive = zeros(n,1);
+premium = zeros(n,1);
+dts = zeros(n,1);
+closedts = zeros(n,1);
+for i = 1:n, dts(i) = straddles.node_(i).opendt1_;end
+for i = 1:n, closedts(i) = straddles.node_(i).tradedts_(finalrets(i,2));end
 
+for i = 1:n
+    dt_i = dts(i);
+    if i == 1
+        premium(i) = premium0;
+    else
+        if proceeds(i-1) == 0
+            premium(i) = premium0;
+        else
+            %update premium if we have option unwinded
+            residual = fundavailable*pr - fundused(i-1) + proceeds(i-1);
+            premium0 = max(residual/(dayscut-nlive(i-1)),0);
+            premium(i) = premium0;
+        end
+    end
+    for j = 1:i
+        if dt_i >=  dts(j) && dt_i < closedts(j)
+            %live
+            fundused(i) = fundused(i) + premium(j);
+            idx_j = find(straddles.node_(j).tradedts_ == dt_i,1,'first');
+            pvholding(i) = pvholding(i) + premium(j)*straddles.node_(j).pvs_(idx_j)/straddles.node_(j).pvs_(1);
+            nlive(i) = nlive(i)+1;
+        elseif dt_i >= dts(j) && dt_i == closedts(j)
+            %live or closed
+            proceeds(i) = proceeds(i) + premium(j)*(finalrets(j,1)-1);
+        end
+    end
+    cash(i) = fundavailable - fundused(i) + proceeds(i);
+end
 
+dailyoptret_leveraged2 = premium.*finalrets(:,1);
+figure(4);
+subplot(221);plot(cumsum(dailyfiret),'b');hold on;
+plot(cumsum(dailyoptret_leveraged2),'r');hold off;legend('fixed income','straddle-leveraged','location','northwest');
+title('cumulative payoff over business days');
+subplot(222);
+plot(cumsum(dailyfiret)./daysvec*nbdaysperyear,'b');hold on;
+plot(cumsum(dailyoptret_leveraged2)./daysvec*nbdaysperyear,'r');hold off;legend('fixed income','straddle-leveraged','location','northeast');
+title('cumulative return over business days')
 
+cutoffperiodret_levraged2 = zeros(n-dayscut+1,2);
+for i = 1:n-dayscut+1
+    cutoffperiodret_levraged2(i,1) = sum(dailyfiret(i:i+dayscut-1))*nbdaysperyear/dayscut;
+    cutoffperiodret_levraged2(i,2) = sum(dailyoptret_leveraged2(i:i+dayscut-1))*nbdaysperyear/dayscut;
+end
 
+subplot(223);plot(cutoffperiodret_levraged2(:,1),'b');hold on;
+plot(cutoffperiodret_levraged2(:,2),'r');hold off;legend('fixed income','straddle-leveraged','location','northwest');
+title('cutoff period return over business days')
+subplot(224)
+annualret_leveraged2 = zeros(n-nbdaysperyear+1,1);
+for i = 1:n-nbdaysperyear+1
+    annualret_leveraged2(i,1) = sum(dailyfiret(i:i+nbdaysperyear-1));
+    annualret_leveraged2(i,2) = sum(dailyoptret_leveraged2(i:i+nbdaysperyear-1));
+end
+subplot(224);plot(annualret_leveraged2(:,1),'b');hold on;
+plot(annualret_leveraged2(:,2),'r');hold off;legend('fixed income','straddle-leveraged');
+title('annual return over business days')
+
+%% calculate running premium with a special start date
+notional = 1;
+rate = 0.03;
+fundavailable = notional-notional/(1+rate);
+pr = 1/3;
+premium0 = fundavailable*pr/dayscut;
+istart = 413;
+iend = istart+252-1;
+cash = zeros(252,1);
+pvholding = zeros(252,1);
+fundused = zeros(252,1);
+proceeds = zeros(252,1);
+nlive = zeros(252,1);
+%premium paid for each straddle
+premium = zeros(252,1);
+for i = istart:iend
+    dt_i = dts(i);
+    if i == istart
+        premium(i-istart+1) = premium0;
+    else
+        if proceeds(i-istart) == 0
+            premium(i-istart+1) = premium0;
+        else
+            residual = fundavailable*pr - fundused(i-istart) + proceeds(i-istart);
+            premium0 = max(residual/(dayscut-nlive(i-istart)),0);
+            premium(i-istart+1) = premium0;
+        end
+    end
+    for j = istart:i
+        if dt_i >=  dts(j) && dt_i < closedts(j)
+            %live
+            fundused(i-istart+1) = fundused(i-istart+1) + premium(j-istart+1);
+            idx_j = find(straddles.node_(j).tradedts_ == dt_i,1,'first');
+            pvholding(i-istart+1) = pvholding(i-istart+1) + premium(j-istart+1)*straddles.node_(j).pvs_(idx_j)/straddles.node_(j).pvs_(1);
+            nlive(i-istart+1) = nlive(i-istart+1)+1;
+        elseif dt_i >= dts(j) && dt_i == closedts(j)
+            %live or closed
+            proceeds(i-istart+1) = proceeds(i-istart+1) + premium(j-istart+1)*(finalrets(j,1)-1);
+        end
+    end
+    cash(i-istart+1) = fundavailable - fundused(i-istart+1) + proceeds(i-istart+1);
+end
 
 
 
