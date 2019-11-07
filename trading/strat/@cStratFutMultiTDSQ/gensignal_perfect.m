d465b7ecf1baf5cce9af3ecc4d629951e2e33b1d
yangyir <31230104+yangyir@users.noreply.github.com>
2019-11-07 17:49:07 +0800

    Update gensignal_perfect.m
    add more comments in Chinese
    and also add an additional control once the lvlup or lvldn had been breached

--- trading/strat/@cStratFutMultiTDSQ/gensignal_perfect.m
+++ trading/strat/@cStratFutMultiTDSQ/gensignal_perfect.m
@@ -16,17 +16,20 @@ function [signal] = gensignal_perfect(strategy,instrument,p,bs,ss,lvlup,lvldn,ma
         stoploss = truelow - truelowbarsize;
         
         np = size(p,1);
+        %如果perfectbs是发生在过去的时间点，我们需要检查perfectbs是否依然有效
         if np > ibs
+            %1.如果从perfectbs发生的时间点到现在时间点的任何收盘价低于了stoploss
+            %则perfectbs变得无效
             stillvalid = isempty(find(p(ibs:end,5)<stoploss,1,'first'));
-            %
+            %2.如果最新收盘价低于了lvldn在perfectbs时间点的值
             if stillvalid
                 if p(end,5) < lvldn(ibs), stillvalid = false;end
             end
-            %
+            %3.如果最近收盘价低于了truelow
             if stillvalid
                 if p(end,5) < truelow, stillvalid = false;end
             end
-            %
+            %4.如果要用bssetup的值的情况。。。
             if stillvalid && usesetups
                 if bs(end) >= 4 && bs(end) < 9, stillvalid = false;end
             end
@@ -34,6 +37,8 @@ function [signal] = gensignal_perfect(strategy,instrument,p,bs,ss,lvlup,lvldn,ma
             stillvalid = true;
         end
 
+        %然后需要检查价格是否有向上突破lvlup,这里取收盘价做为比较决定价格是否有突破
+        %如果有突破，我们接着检查从突破到现在是否MACD转负过
         haslvlupbreachedwithmacdbearishafterwards = false;
         if stillvalid
             ibreach = find(p(ibs:end,5) > lvlup(ibs),1,'first');
@@ -42,6 +47,16 @@ function [signal] = gensignal_perfect(strategy,instrument,p,bs,ss,lvlup,lvldn,ma
                 ibreach = ibreach + ibs-1;
                 diffvec = macdvec(ibreach:end)-sigvec(ibreach:end);
                 haslvlupbreachedwithmacdbearishafterwards = ~isempty(find(diffvec<0,1,'first'));
+                %用最高价判断是否完全跌回lvlup之下
+                haslvlupbreachedbutbouncedback = ~isempty(find(p(ibreach:end,3)<lvlup(ibs),1,'first'));
+                %如果价格回到了lvlup之下，我们认为perfectbs也就无效了
+                if haslvlupbreachedbutbouncedback
+                    stillvalid = false;
+                end
+                %如果此时间点收盘价在lvlup之下，我们认为该时间点无效
+                if ~haslvlupbreachedbutbouncedback && p(end,5) < lvlup(ibs)
+                    stillvalid = false;
+                end
             end
         end
 
@@ -49,6 +64,7 @@ function [signal] = gensignal_perfect(strategy,instrument,p,bs,ss,lvlup,lvldn,ma
             signal = {};
         else
             samplefreqstr = strategy.riskcontrols_.getconfigvalue('code',instrument.code_ctp,'propname','samplefreq');
+            %如果有突破lvlup且macd有转负需要重新计算risklvl
             if haslvlupbreachedwithmacdbearishafterwards
                 risklvl = p(end,5) - (p(ibs,5) - stoploss);
             else
@@ -73,16 +89,20 @@ function [signal] = gensignal_perfect(strategy,instrument,p,bs,ss,lvlup,lvldn,ma
         stoploss = truehigh + truehighbarsize;
         
         np = size(p,1);
+        %如果perfectss是发生在过去的时间点，我们需要检查perfectss是否依然有效
         if np > iss
+            %1.如果从perfectss发生的时间点到现在时间点的任何收盘价高于了stoploss
+            %则perfectss变得无效
             stillvalid = isempty(find(p(iss:end,5)>stoploss,1,'first'));
+            %2.如果最新收盘价高于了lvldn在perfectss时间点的值
             if stillvalid
                 if p(end,5) > lvlup(iss), stillvalid = false;end
             end
-            %
+            %3.如果最近收盘价高于了truehigh
             if stillvalid
                 if p(end,5) > truehigh, stillvalid = false;end
             end
-            %
+            %4.如果要用sssetup的值的情况。。。
             if stillvalid && usesetups
                 if ss(end) >= 4 && ss(end) < 9, stillvalid = false;end
             end
@@ -91,6 +111,8 @@ function [signal] = gensignal_perfect(strategy,instrument,p,bs,ss,lvlup,lvldn,ma
             stillvalid = true;
         end
         
+        %然后需要检查价格是否有向下突破lvldn,这里取收盘价做为比较决定价格是否有突破
+        %如果有突破，我们接着检查从突破到现在是否MACD转正过
         haslvldnbreachedwithmacdbullishafterwards = false;
         if stillvalid
             ibreach = find(p(iss:end,5) < lvldn(iss),1,'first');
@@ -99,12 +121,23 @@ function [signal] = gensignal_perfect(strategy,instrument,p,bs,ss,lvlup,lvldn,ma
                 ibreach = ibreach + iss-1;
                 diffvec = macdvec(ibreach:end)-sigvec(ibreach:end);
                 haslvldnbreachedwithmacdbullishafterwards = ~isempty(find(diffvec>0,1,'first'));
+                %用最低价判断是否完全反弹回lvldn之上
+                haslvldnbreachedbutbouncedback = ~isempty(find(p(ibreach:end,4)>lvldn(iss),1,'first'));
+                %如果价格回到了lvldn之上，我们认为perfectbs也就无效了
+                if haslvldnbreachedbutbouncedback
+                    stillvalid = false;
+                end
+                %如果此时间点收盘价在lvldn之上，我们认为该时间点无效
+                if ~haslvldnbreachedbutbouncedback && p(end,5) > lvldn(iss)
+                    stillvalid = false;
+                end
             end
         end
         
         if ~stillvalid
             signal = {};
         else
+            %如果有突破lvldn且macd有转正需要重新计算risklvl
             if haslvldnbreachedwithmacdbullishafterwards
                 risklvl = p(end,5) + (stoploss-p(iss,5));
             else
