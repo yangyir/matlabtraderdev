bc2f74bc26e1f7f6fbe67089f4a898fd875fbd7b
yangyir <31230104+yangyir@users.noreply.github.com>
2019-12-02 09:34:50 +0800

    Create opt_arbmonitor.m

---
+++ trading/options/opt_arbmonitor.m
@@ -0,0 +1,28 @@
+function [] = opt_arbmonitor(mdeopt,underlier,strikes)
+    qms = mdeopt.qms_;
+
+    for i = 1:length(strikes)
+        if strcmpi(underlier(1:2),'SR') || strcmpi(underlier(1:2),'CF') ...
+                || strcmpi(underlier(1:2),'cu') || strcmpi(underlier(1:2),'ru')
+            qc = qms.getquote([underlier,'C',num2str(strikes(i))]);
+            qp = qms.getquote([underlier,'P',num2str(strikes(i))]);
+        else
+            qc = qms.getquote([underlier,'-C-',num2str(strikes(i))]);
+            qp = qms.getquote([underlier,'-P-',num2str(strikes(i))]);
+        end
+
+        bid = -qp.ask1 + qc.bid1 + strikes(i);
+        ask = strikes(i) +qc.ask1 - qp.bid1;
+        bid_underlier = qc.bid_underlier;
+        ask_underlier = qc.ask_underlier;
+        if i == 1
+            fprintf('\n');
+            fprintf('%s\t%s\t%s\t%s\t%s\n','strike','fwdbid','fwdask','futbid','futask');
+        end
+
+        fprintf('%s\t%s\t%s\t%s\t%s\n',num2str(strikes(i)),num2str(bid),num2str(ask),...
+            num2str(bid_underlier),num2str(ask_underlier));
+    end
+
+end
+
