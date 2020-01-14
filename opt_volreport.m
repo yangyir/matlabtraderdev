bc9d6895733ae14404f07bad1dffba758ca76f63
yangyir <31230104+yangyir@users.noreply.github.com>
2020-01-15 01:04:24 +0800

    Create opt_volreport.m

---
+++ opt_volreport.m
@@ -0,0 +1,11 @@
+function [output] = opt_volreport(code_underlier,cobdate,varargin)
+    hd_underlier = cDataFileIO.loadDataFromTxtFile([code_underlier,'_daily.txt']);
+    if ischar(cobdate),cobdate = datenum(cobdate);end
+    try
+        spot = hd_underlier(hd_underlier(:,1) == cobdate,5);
+    catch
+        fprintf('%s price not saved on %s\n',code_underlier,datestr(cobdate,'yyyy-mm-dd'));
+        output = [];
+        return
+    end
+end
