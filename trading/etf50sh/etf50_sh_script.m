b21c893f2e4897c15f2484cc43fca66ddc140129
yangyir <31230104+yangyir@users.noreply.github.com>
2020-02-28 20:34:44 +0800

    Update etf50_sh_script.m

--- trading/etf50sh/etf50_sh_script.m
+++ trading/etf50sh/etf50_sh_script.m
@@ -9,7 +9,7 @@ bd_c_mar = cell(n_c_mar,1);
 tbl_c_mar = [k',zeros(length(k),3)];
 for i = 1:n_c_mar
     try
-        bd_c_mar{i} = pnlriskbreakdownbbg(opt50_c_mar{i},datenum('2020-02-27'));
+        bd_c_mar{i} = pnlriskbreakdownbbg(opt50_c_mar{i},datenum('2020-02-28'));
         tbl_c_mar(i,2) = bd_c_mar{i}.iv1;
         tbl_c_mar(i,3) = bd_c_mar{i}.iv2;
         tbl_c_mar(i,4) = bd_c_mar{i}.deltacarry/bd_c_mar{i}.spot2/10000;
@@ -24,7 +24,7 @@ bd_p_mar = cell(n_p_mar,1);
 tbl_p_mar = [k',zeros(length(k),3)];
 for i = 1:n_p_mar
     try
-        bd_p_mar{i} = pnlriskbreakdownbbg(opt50_p_mar{i},datenum('2020-02-27'));
+        bd_p_mar{i} = pnlriskbreakdownbbg(opt50_p_mar{i},datenum('2020-02-28'));
         tbl_p_mar(i,2) = bd_p_mar{i}.iv1;
         tbl_p_mar(i,3) = bd_p_mar{i}.iv2;
         tbl_p_mar(i,4) = bd_p_mar{i}.deltacarry/bd_p_mar{i}.spot2/10000;
@@ -37,31 +37,35 @@ end
 fwd1 = bd_c_mar{1}.fwd1;fwd2 = bd_c_mar{1}.fwd2;
 m1 = tbl_c_mar(:,1)/fwd1;
 m2 = tbl_c_mar(:,1)/fwd2;
+m = max(min(m1),min(m2)):0.01:min(max(m1),max(m2));
+volinterp1 = interp1(m1,tbl_c_mar(:,2),m);
+volinterp2 = interp1(m1,tbl_c_mar(:,3),m);
+
 subplot(211);
-plot(m1,tbl_c_mar(:,2),'-');hold on;
+% plot(m1,tbl_c_mar(:,2),'-');hold on;
+plot(m,volinterp1,'-');hold on;
 if bd_c_mar{1}.spot1 < bd_c_mar{1}.spot2
     color = 'r';
-    plot(m2,tbl_c_mar(:,3),'r-');
+    plot(m,volinterp2,'r-');
 else
     color = 'g';
-    plot(m2,tbl_c_mar(:,3),'g-');
+    plot(m,volinterp2,'g-');
 end
 hold off;
 legend(bd_c_mar{1}.date1,bd_c_mar{1}.date2);
 xlabel('moneyness');ylabel('vol');
 subplot(212);
-m = max(min(m1),min(m2)):0.01:min(max(m1),max(m2));
-volinterp1 = interp1(m1,tbl_c_mar(:,2),m);
-volinterp2 = interp1(m1,tbl_c_mar(:,3),m);
+
 bar(m,volinterp2-volinterp1,color);
 xlabel('moneyness');ylabel('spread');
 %%
-port1_mar = {'p2.8';'p2.9';'c2.9';'c3.0'};
+port1_mar = {'p2.8';'c2.9';'p2.9';'c3.0'};
 v1_mar = [60;-53;-48;62];
 deltapnl = zeros(length(port1_mar),1);
 gammapnl = zeros(length(port1_mar),1);
 thetapnl = zeros(length(port1_mar),1);
 vegapnl = zeros(length(port1_mar),1);
+unexplainedpnl = zeros(length(port1_mar),1);
 deltacarry = zeros(length(port1_mar),1);
 gammacarry = zeros(length(port1_mar),1);
 thetacarry = zeros(length(port1_mar),1);
@@ -82,6 +86,7 @@ for i = 1:length(port1_mar)
     gammapnl(i) = v1_mar(i)*bd_i.pnlgamma;
     thetapnl(i) = v1_mar(i)*bd_i.pnltheta;
     vegapnl(i) = v1_mar(i)*bd_i.pnlvega;
+    unexplainedpnl(i) = v1_mar(i)*bd_i.pnlunexplained;
     vegacarry(i) = v1_mar(i)*bd_i.vegacarry;
     deltacarry(i) = v1_mar(i)*bd_i.deltacarry;
     gammacarry(i) = v1_mar(i)*bd_i.gammacarry;
