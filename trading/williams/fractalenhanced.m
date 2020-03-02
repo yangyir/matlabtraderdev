071c76e8e1401fa27a6b2830ac470b37be2d4e26
yangyir <31230104+yangyir@users.noreply.github.com>
2020-03-02 15:47:10 +0800

    Update fractalenhanced.m

--- trading/williams/fractalenhanced.m
+++ trading/williams/fractalenhanced.m
@@ -1,4 +1,4 @@
-function [ idx,HH,LL,upperchannel,lowerchannel] = fractalenhanced( p,nperiod,varargin )
+function [ idx,HH,LL,upperchannel,lowerchannel,volsmooth] = fractalenhanced( p,nperiod,varargin )
 %FRACTALENHANCED Summary of this function goes here
 % the fractal enhanced indicator draws range boxes overlaid on the
 % price chart. the upper and lower side of the boxes are made from recent
@@ -8,10 +8,51 @@ function [ idx,HH,LL,upperchannel,lowerchannel] = fractalenhanced( p,nperiod,var
     ip = inputParser;
     ip.CaseSensitive = false;ip.KeepUnmatched = true;
     ip.addParameter('volatilityperiod',13,@isnumeric);
+    ip.addParameter('tolerance',0.003,@isnumeric);
     ip.parse(varargin{:});
     inpbandsperiod = ip.Results.volatilityperiod;
+    change = ip.Results.tolerance;
+    
+    if inpbandsperiod < 0
+        error('fractalenhanced:volatilityperiod shall be non-negative')
+    end
+    
+    if change < 0
+        error('fractalenhanced:tolerance input shall be non-negative')
+    end
+    
+    [idx,HH,LL] = fractal(p,nperiod);
+    
+    if inpbandsperiod == 0 && change == 0
+        upperchannel = HH;
+        lowerchannel = LL;
+        return;
+    end
     
     np = size(p,1);
+    if inpbandsperiod == 0 && change > 0
+        upperchannel = HH;
+        lowerchannel = LL;
+        for i = 2:np
+            if abs((HH(i)-upperchannel(i-1))/p(i,5))>change
+                upperchannel(i) = HH(i);
+            else
+                if ~isnan(upperchannel(i-1))
+                    upperchannel(i) = upperchannel(i-1);
+                end
+            end
+            %
+            if abs((LL(i)-lowerchannel(i-1))/p(i,5))>change
+                lowerchannel(i) = LL(i);
+            else
+                if ~isnan(lowerchannel(i-1))
+                    lowerchannel(i) = lowerchannel(i-1);
+                end
+            end
+        end
+        return
+    end
+    
     sigma = nan(np,1);
     for i = 1:np
         if i < inpbandsperiod, continue;end
@@ -31,12 +72,8 @@ function [ idx,HH,LL,upperchannel,lowerchannel] = fractalenhanced( p,nperiod,var
     [~,volsmooth] = movavg(volder,1,smooth);
     volsmooth = min(max(volsmooth,-1),0);
 
-    [idx,HH,LL] = fractal(p,nperiod);
     
-    ret = p(2:end,5)./p(1:end-1,5)-1;
-    change = 1.618*std(ret);
-    change = min(0.003,change);
-    
+        
     upperchannel = HH;
     lowerchannel = LL;
     
