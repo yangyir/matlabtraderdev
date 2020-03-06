268a7023fdd2aa77a2c19a736937bb61be1c4cb0
yangyir <31230104+yangyir@users.noreply.github.com>
2020-03-06 23:33:31 +0800

    Create williamsad.m
    williams accumulate / distrubute

---
+++ trading/williams/williamsad.m
@@ -0,0 +1,47 @@
+function [ wad,trh,trl ] = williamsad( p,usevolume )
+%WILLIAMSAD Summary of this function goes here
+%   Williams Accumulation Distribution is traded on divergences. When price
+%   makes a new high and the indicator fails to exceed its previous high, a
+%   distribution is taking place. When price makes a new low and the WAD
+%   fails to make a new low, accumulation is occuring. Go long when there
+%   is a bullish divergence between Williams Accumulation Distribution and
+%   price. Go short on a bearish divergence.
+
+    if nargin < 2
+        usevolume = false;
+    end
+    np = size(p,1);
+    %Williams' accumulation distribution
+    wad = zeros(np,1);
+    %true range high;the greater of high(today) and close price(yesterday)
+    trh = zeros(np,1);
+    %true range low:the lesser of low(today) and close yesterday
+    trl = zeros(np,1);
+    
+    pxhigh = p(:,3);
+    pxlow = p(:,4);
+    pxclose = p(:,5);
+    volume = p(:,6);
+    
+    trh(1) = pxhigh(1);
+    trl(1) = pxlow(1);
+    for i = 2:np
+        trh(i) = max(pxhigh(i),pxclose(i-1));
+        trl(i) = min(pxlow(i),pxclose(i-1));
+        if pxclose(i)>pxclose(i-1)
+            pxmove = pxclose(i)-trl(i);
+        elseif pxclose(i)<pxclose(i-1)
+            pxmove = pxclose(i)-trh(i);
+        elseif pxclose(i) == pxclose(i-1)
+            pxmove = 0;
+        end
+        if usevolume
+            wad(i) = wad(i-1)+pxmove*volume(i);
+        else
+            wad(i) = wad(i-1)+pxmove;
+        end
+    end
+
+
+end
+
