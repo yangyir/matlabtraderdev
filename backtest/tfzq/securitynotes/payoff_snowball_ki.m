03128be6a212e12b1152efa1a92709c5ab0ef533
yangyir <31230104+yangyir@users.noreply.github.com>
2020-03-11 22:32:00 +0800

    security note related backtest

--- backtest/tfzq/securitynotes/payoff_snowball_ki.m
+++ backtest/tfzq/securitynotes/payoff_snowball_ki.m
@@ -1,23 +1,20 @@
-function [ payoff ] = payoff_snowball(px,varargin)
-%PAYOFF_SHARKFIN Summary of this function goes here
-%   Detailed explanation goes here
+function [ payoff,iskoed,iskied ] = payoff_snowball_ki(px,varargin)
+
     p = inputParser;
     p.CaseSensitive = false;p.KeepUnmatched = true;
     p.addParameter('startdate',today,@isnumeric);
     p.addParameter('expirydate',today+90,@isnumeric);
-    p.addParameter('minimumpayoff',0,@isnumeric);
     p.addParameter('knockoutlevel',1,@isnumeric);
+    p.addParameter('knockinlevel',0.7,@isnumeric);
     p.addParameter('knockoutpayoff',0,@isnumeric);
-    p.addParameter('principalprotectionlevel',0.9,@isnumeric);
     p.addParameter('participateratio',1,@isnumeric);
     
     p.parse(varargin{:});
     startdt = p.Results.startdate;
     expirydt = p.Results.expirydate;
     kolvl = p.Results.knockoutlevel;
-    minpayoff = p.Results.minimumpayoff;
+    kilvl = p.Results.knockinlevel;
     kopayoff = p.Results.knockoutpayoff;
-    pplvl = p.Results.principalprotectionlevel;
     pr = p.Results.participateratio;
     
     startidx = find(px(:,1)>=startdt,1,'first');
@@ -28,16 +25,24 @@ function [ payoff ] = payoff_snowball(px,varargin)
         return
     end
     
+    iskied = ~isempty(find(px(startidx+1:expiryidx,5)./px(startidx,5)<kilvl,1,'first'));
+        
     S0 = px(startidx,5);
     S1 = px(expiryidx,5);
     
     if S1/S0>=kolvl
+        iskoed = true;
         payoff = kopayoff;
     else
-        if S1/S0 < pplvl
-            payoff = minpayoff;
+        iskoed = false;
+        if iskied
+            if S1/S0 > kilvl
+                payoff = kopayoff;
+            else
+                payoff = (S1/S0-1)*pr;
+            end
         else
-            payoff = (1-S1/S0)*pr+minpayoff;
+            payoff = kopayoff;
         end
     end
     
--- backtest/tfzq/securitynotes/securitynote_general.m
+++ backtest/tfzq/securitynotes/securitynote_general.m
@@ -12,7 +12,9 @@ for i = 1:lastidx
 %     [payoff(i,1),payoff(i,2)] = payoff_sharkfin_straddle(hd_eqindex500,'startdate',hd_eqindex500(i,1),'expirydate',hd_eqindex500(i+62,1),'minimumpayoff',0.02,'knockoutupperlevel',1.1,'knockoutlowerlevel',0.9,'knockoutupperpayoff',0.02,'knockoutlowerpayoff',0.02,'participateratioupper',1,'participateratiolower',1);
 %     [payoff(i,1),payoff(i,2)] = payoff_sharkfin_straddle(hd_eqindex500,'startdate',hd_eqindex500(i,1),'expirydate',hd_eqindex500(i+62,1),'minimumpayoff',0.02,'knockoutupperlevel',1.1,'knockoutlowerlevel',0.9,'knockoutupperpayoff',0.05,'knockoutlowerpayoff',0.02,'participateratioupper',7/8,'participateratiolower',1);
 %     [payoff(i,1)] = payoff_bullspread(hd_eqindex500,'startdate',hd_eqindex500(i,1),'expirydate',hd_eqindex500(i+62,1),'minimumpayoff',0.02,'upperstrikelevel',1.08,'participateratio',0.62);
-    [payoff(i,1)] = payoff_bearspread(hd_eqindex500,'startdate',hd_eqindex500(i,1),'expirydate',hd_eqindex500(i+62,1),'minimumpayoff',0.02,'lowerstrikelevel',0.96,'participateratio',9/4);
+%     [payoff(i,1)] = payoff_bearspread(hd_eqindex500,'startdate',hd_eqindex500(i,1),'expirydate',hd_eqindex500(i+62,1),'minimumpayoff',0.02,'lowerstrikelevel',0.96,'participateratio',9/4);
+%     [payoff(i,1)] = payoff_snowball(hd_eqindex500,'startdate',hd_eqindex500(i,1),'expirydate',hd_eqindex500(i+62,1),'minimumpayoff',0.001,'knockoutlevel',1,'knockoutpayoff',0.075,'participateratio',1,'principalprotectionlevel',0.926);
+    [payoff(i,1)] = payoff_snowball_ki(hd_eqindex500,'startdate',hd_eqindex500(i,1),'expirydate',hd_eqindex500(i+62,1),'knockoutlevel',1,'knockinlevel',0.7,'knockoutpayoff',0.073,'participateratio',1);
 end
 totalpayoff = sum(payoff(:,1));
 annualreturn = totalpayoff/notional/lastidx*252;
@@ -23,6 +25,8 @@ annualreturn = totalpayoff/notional/lastidx*252;
 %expected annual return of shark fin straddle with asymetric payoff is 5.39%
 %expected annual return of bullspread(100-108) with 0.62 pr is 3.98%
 %expected annual return of bearspread(96-100) with 2.25 pr is 6.04%
+%expected annual return of principal-proctected snowball is 4.94%
+%expected annual return of non-principal-proctected snowball with 70% KI is7.09%
 %%
 blkprice(1,1.05,0.03,0.25,0.25)
 
