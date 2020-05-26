02c44222a0f538348f1555488117d833e8e77761
yangyir <31230104+yangyir@users.noreply.github.com>
2020-05-26 09:36:56 +0800

    Update fractal_genindicators1.m

--- trading/williams/fractal_genindicators1.m
+++ trading/williams/fractal_genindicators1.m
@@ -1,14 +1,14 @@
 function [ idxfractalb1,idxfractals1 ] = fractal_genindicators1( px,HH,LL,jaw,teeth,lips,varargin )
 %FRACTAL_GENINDICATORS Summary of this function goes here
 %   indicator of fractal's breachup-B and breachdn-S
-    p = inputParser;p.CaseSensitive = false;p.KeepUnmatched = true;
-    p.addParameter('instrument',[],@(x)validateattributes(x,{'cInstrument'},{},'','instrument'));
-    p.parse(varargin{:});
-    instrument = p.Results.instrument;
+%     p = inputParser;p.CaseSensitive = false;p.KeepUnmatched = true;
+%     p.addParameter('instrument',[],@(x)validateattributes(x,{'cInstrument'},{},'','instrument'));
+%     p.parse(varargin{:});
+%     instrument = p.Results.instrument;
     
-    flagweakb1 = fractal_isbreachb(px,HH,LL,jaw,teeth,lips,'level','weak','instrument',instrument);
-    flagmediumb1 = fractal_isbreachb(px,HH,LL,jaw,teeth,lips,'level','medium','instrument',instrument);
-    flagstrongb1 = fractal_isbreachb(px,HH,LL,jaw,teeth,lips,'level','strong','instrument',instrument);
+    flagweakb1 = fractal_isbreachb(px,HH,LL,jaw,teeth,lips,'level','weak',varargin{:});
+    flagmediumb1 = fractal_isbreachb(px,HH,LL,jaw,teeth,lips,'level','medium',varargin{:});
+    flagstrongb1 = fractal_isbreachb(px,HH,LL,jaw,teeth,lips,'level','strong',varargin{:});
     flagb1 = flagweakb1 + flagmediumb1 + flagstrongb1;
     %1.weak;2.medium;3.strong
     idxfractalb1 = [find(flagb1==1),ones(length(find(flagb1==1)),1);...
@@ -17,9 +17,9 @@ function [ idxfractalb1,idxfractals1 ] = fractal_genindicators1( px,HH,LL,jaw,te
     idxfractalb1 = sortrows(idxfractalb1);
     %
     %
-    flagweaks1 = fractal_isbreachs(px,HH,LL,jaw,teeth,lips,'level','weak','instrument',instrument);
-    flagmediums1 = fractal_isbreachs(px,HH,LL,jaw,teeth,lips,'level','medium','instrument',instrument);
-    flagstrongs1 = fractal_isbreachs(px,HH,LL,jaw,teeth,lips,'level','strong','instrument',instrument);
+    flagweaks1 = fractal_isbreachs(px,HH,LL,jaw,teeth,lips,'level','weak',varargin{:});
+    flagmediums1 = fractal_isbreachs(px,HH,LL,jaw,teeth,lips,'level','medium',varargin{:});
+    flagstrongs1 = fractal_isbreachs(px,HH,LL,jaw,teeth,lips,'level','strong',varargin{:});
     flags1 = flagweaks1 + flagmediums1 + flagstrongs1;
     %1.weak;2.medium;3.strong
     idxfractals1 = [find(flags1==1),ones(length(find(flags1==1)),1);...
