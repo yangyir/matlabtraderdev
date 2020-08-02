32c6626e257327e8d346786e27504a85526de3cd
yangyir <31230104+yangyir@users.noreply.github.com>
2020-08-02 23:41:21 +0800

    add b1 and s1 status function

---
+++ trading/williams/fractal_b1_status.m
@@ -0,0 +1,137 @@
+function [res] = fractal_b1_status(nfractal,extrainfo)
+px = extrainfo.px;
+ss = extrainfo.ss;
+sc = extrainfo.sc;
+lvlup = extrainfo.lvlup;
+lvldn = extrainfo.lvldn;
+idxHH = extrainfo.idxhh;
+HH = extrainfo.hh;
+% LL = extrainfo.ll;
+lips = extrainfo.lips;
+teeth = extrainfo.teeth;
+jaw = extrainfo.jaw;
+% wad = extrainfo.wad;
+
+if HH(end-1) < teeth(end-1)
+    b1type = 1;
+elseif HH(end-1) > teeth(end-1)
+    if teeth(end-1) > jaw(end-1)
+        b1type = 3;
+    elseif teeth(end-1) < jaw(end-1)
+        b1type = 2;
+    end
+end
+
+%islvlupbreach
+islvlupbreach = 0;
+if px(end,5)>lvlup(end) && px(end-1,5)<lvlup(end)
+    islvlupbreach = 1;
+end
+if ~islvlupbreach && (px(end,5)>lvlup(end) && px(end,4)<lvlup(end))
+    islvlupbreach = 2;
+end
+if ~islvlupbreach && ss(end) <= 9
+    idx1 = find(px(end-ss(end):end,5)>lvlup(end),1,'first');
+    if ~isempty(idx1)
+        idx2 = find(px(end-ss(end)+idx1:end,5)<lvlup(end),1,'first');
+        if ~isempty(idx2), islvlupbreach = 3;end        
+    end    
+end
+%isclose2lvlup
+isclose2lvlup = ~isnan(lvlup(end)) && ~isnan(lvldn(end)) && ...
+    lvlup(end)>lvldn(end) && ...
+    px(end,5)<lvlup(end) && ...
+    (lvlup(end)-px(end,5))/(lvlup(end)-lvldn(end))<0.1;
+%
+[~,~,nkabovelips,nkaboveteeth,nkfromhh,isteethjawcrossed,isteethlipscrossed] = fractal_countb(px,idxHH,nfractal,lips,teeth,jaw);
+%isvolblowup
+barsizelast = px(end,3)-px(end,4);
+barsizerest = px(end-nkfromhh+1:end-1,3)-px(end-nkfromhh+1:end-1,4);
+isvolblowup = (barsizelast-mean(barsizerest))/std(barsizerest)>norminv(0.99);
+%isvolblowup2
+retlast = log(px(end,5)/px(end-1,5));
+retrest = log(px(end-nkfromhh+1:end-1,5)./px(end-nkfromhh:end-2,5));
+isvolblowup2 = abs(retlast-mean(retrest))/std(retrest)>norminv(0.99);
+%
+lastss9 = find(ss==9,1,'last');
+if isempty(lastss9)
+    nkfromss = NaN;
+else
+    lastss = lastss9;
+    for i = lastss9+1:size(ss,1)
+        if ss(i) ~= 0
+            lastss = i;
+        else
+            break
+        end
+    end
+    nkfromss = size(ss,1)-lastss;
+end
+
+%does it breach-up high of a previous sell sequential
+issshighbreach = 0;
+if size(ss,1)-lastss9+1<=nkfromhh
+    lastssval = ss(lastss);
+    issshighbreach = px(end,5) > max(px(lastss-lastssval+1:lastss,3));
+end
+%does it stand beyond a sell sequential with highest high and close price
+issshighvalue = ss(end)>= 9 && px(end,5)>=max(px(end-ss(end)+1:end,5)) && px(end,3)>=max(px(end-ss(end)+1:end,3));
+%
+%does it breach-up hh after sc13
+lastsc13 = find(sc(1:end-1)==13,1,'last');
+if isempty(lastsc13)
+    nkfromsc13 = NaN;
+    isschighbreach = 0;
+else
+    nkfromsc13 = size(px,1)-lastsc13;
+    isschighbreach = px(end,5)>max(px(lastsc13:end-1,3));
+end
+%
+%all the special case above passed
+%and to check whether there are 2*nfractal+1 candles stay above teeth
+%before it breach the fractal hh
+istrendconfirmed = nkaboveteeth>=2*nfractal+1;
+if ~istrendconfirmed
+    %1.in case all candles are above teeth since hh formed but there are
+    %less than 2*nfractal+1 candles since then, we include candles before
+    %the hh formed and double check
+    if nkaboveteeth == nkfromhh
+        istrendconfirmed = isempty(find(px(end-2*nfractal:end,5)<teeth(end-2*nfractal:end),1,'first'));
+    end
+end
+%
+%check alligator's lips,teeth,jaw relationship
+if jaw(end)<teeth(end) && teeth(end)<lips(end)
+    alligatorstatus = 'jaw<teeth<lips';
+elseif jaw(end)<lips(end) && lips(end)<teeth(end)
+    alligatorstatus = 'jaw<lips<teeth';
+elseif lips(end)<teeth(end) && teeth(end)<jaw(end)
+    alligatorstatus = 'lips<teeth<jaw';
+elseif lips(end)<jaw(end) && jaw(end)<teeth(end)
+    alligatorstatus = 'lips<jaw<teeth';
+elseif teeth(end)<jaw(end) && jaw(end)<lips(end)
+    alligatorstatus = 'teeth<jaw<lips';
+elseif teeth(end)<lips(end) && lips(end)<jaw(end)
+    alligatorstatus = 'teeth<lips<jaw';
+end
+%
+rsi = rsindex(px(:,5));
+%
+res = struct('b1type',b1type,...
+    'islvlupbreach',islvlupbreach,...
+    'isclose2lvlup',isclose2lvlup,...
+    'isvolblowup',isvolblowup,...
+    'isvolblowup2',isvolblowup2,...
+    'isteethjawcrossed',isteethjawcrossed,...
+    'isteethlipscrossed',isteethlipscrossed,...
+    'alligatorstatus',alligatorstatus,...
+    'ss',ss(end),...
+    'nkfromss',nkfromss,...
+    'issshighvalue',issshighvalue,...
+    'issshighbreach',issshighbreach,...
+    'sc',sc(end),...
+    'nkfromsc13',nkfromsc13,...
+    'isschighbreach',isschighbreach,...
+    'istrendconfirmed',istrendconfirmed,...
+    'rsi',rsi(end));
+disp(res);
---
+++ trading/williams/fractal_s1_status.m
@@ -0,0 +1,141 @@
+function [res] = fractal_s1_status(nfractal,extrainfo)
+px = extrainfo.px;
+bs = extrainfo.bs;
+bc = extrainfo.bc;
+lvlup = extrainfo.lvlup;
+lvldn = extrainfo.lvldn;
+idxLL = extrainfo.idxll;
+% HH = extrainfo.hh;
+LL = extrainfo.ll;
+lips = extrainfo.lips;
+teeth = extrainfo.teeth;
+jaw = extrainfo.jaw;
+% wad = extrainfo.wad;
+
+if LL(end-1) > teeth(end-1)
+    s1type = 1;
+elseif LL(end-1) < teeth(end-1)
+    if teeth(end-1) < jaw(end-1)
+        s1type = 3;
+    elseif teeth(end-1) > jaw(end-1)
+        s1type = 2;
+    end
+end
+
+
+%islvldnbreach
+islvldnbreach = 0;
+if px(end,5)<lvldn(end) && px(end-1,5)>lvldn(end)
+    islvldnbreach = 1;
+end
+if ~islvldnbreach && (px(end,5)<lvldn(end) && px(end,3)>lvldn(end))
+    islvldnbreach = 2;
+end
+if ~islvldnbreach && bs(end) <= 9
+    idx1 = find(px(end-bs(end):end,5)<lvldn(end),1,'first');
+    if ~isempty(idx1)
+        idx2 = find(px(end-bs(end)+idx1+1:end,5)>lvldn(end),1,'first');
+        if ~isempty(idx2),islvldnbreach = 3;end
+    end
+end
+%isclose2lvldn
+isclose2lvldn = ~isnan(lvlup(end)) && ~isnan(lvldn(end)) && ...
+    lvlup(end)>lvldn(end) && ...
+    px(end,5)>lvldn(end) && ...
+    (lvlup(end)-px(end,5))/(lvlup(end)-lvldn(end))>0.9;
+%
+[~,~,nkbelowlips,nkbelowteeth,nkfromll,isteethjawcrossed,isteethlipscrossed] = fractal_counts(px,idxLL,nfractal,lips,teeth,jaw);
+%isvolblowup
+barsizelast = px(end,3)-px(end,4);
+barsizerest = px(end-nkfromll+1:end-1,3)-px(end-nkfromll+1:end-1,4);
+isvolblowup = (barsizelast-mean(barsizerest))/std(barsizerest)>norminv(0.99);
+%isvolblowup2
+retlast = log(px(end,5)/px(end-1,5));
+retrest = log(px(end-nkfromll+1:end-1,5)./px(end-nkfromll:end-2,5));
+isvolblowup2 = abs(retlast-mean(retrest))/std(retrest)>norminv(0.99);
+%
+lastbs9 = find(bs==9,1,'last');
+if isempty(lastbs9)
+    nkfrombs = NaN;
+else
+    lastbs = lastbs9;
+    for i = lastbs9+1:size(bs,1)
+        if bs(i) ~= 0
+            lastbs = i;
+        else
+            break
+        end
+    end
+    nkfrombs = size(bs,1)-lastbs;
+end
+
+%does it breach-dn low of a previous buy sequential
+isbslowbreach = 0;
+if size(bs,1)-lastbs9+1<=nkfromll
+    lastbsval = bs(lastbs);
+    isbslowbreach = px(end,5) < min(px(lastbs-lastbsval+1:lastbs,4));
+end
+%
+%does it stand beyond a buy sequential with lowest low and close price
+isbslowvalue = bs(end)>=9 && px(end,5) <= min(px(end-bs(end)+1:end,5)) && px(end,4) <= min(px(end-bs(end)+1:end,4));
+%
+%does it breach-dn ll after bc13
+lastbc13 = find(bc(1:end-1)==13,1,'last');
+if isempty(lastbc13)
+    nkfrombc13 = NaN;
+    isbclowbreach = 0;
+else
+    nkfrombc13 = size(px,1)-lastbc13;
+    isbclowbreach = px(end,5)<min(px(lastbc13:end-1,4));
+end
+%all the special case above passed
+%and to check whether there are 2*nfractal+1 candles stay below teeth
+%before it breach the fractal ll
+istrendconfirmed = nkbelowteeth>=2*nfractal+1;
+if ~istrendconfirmed
+    %1.in case all candles are below teeth since ll formed but there are
+    %less than 2*nfractal+1 candles since then, we include candles before
+    %the ll formed and double check
+    if nkbelowteeth == nkfromll
+        istrendconfirmed = isempty(find(px(end-2*nfractal:end,5)>teeth(end-2*nfractal:end),1,'first'));
+    end
+end
+
+%check alligator's lips,teeth,jaw relationship
+if jaw(end)<teeth(end) && teeth(end)<lips(end)
+    alligatorstatus = 'jaw<teeth<lips';
+elseif jaw(end)<lips(end) && lips(end)<teeth(end)
+    alligatorstatus = 'jaw<lips<teeth';
+elseif lips(end)<teeth(end) && teeth(end)<jaw(end)
+    alligatorstatus = 'lips<teeth<jaw';
+elseif lips(end)<jaw(end) && jaw(end)<teeth(end)
+    alligatorstatus = 'lips<jaw<teeth';
+elseif teeth(end)<jaw(end) && jaw(end)<lips(end)
+    alligatorstatus = 'teeth<jaw<lips';
+elseif teeth(end)<lips(end) && lips(end)<jaw(end)
+    alligatorstatus = 'teeth<lips<jaw';
+end
+%
+rsi = rsindex(px(:,5));
+%
+res = struct('s1type',s1type,...
+    'islvldnbreach',islvldnbreach,...
+    'isclose2lvldn',isclose2lvldn,...
+    'isvolblowup',isvolblowup,...
+    'isvolblowup2',isvolblowup2,...
+    'isteethjawcrossed',isteethjawcrossed,...
+    'isteethlipscrossed',isteethlipscrossed,...
+    'alligatorstatus',alligatorstatus,...
+    'bs',bs(end),...
+    'nkfrombs',nkfrombs,...
+    'isbslowvalue',isbslowvalue,...
+    'isbslowbreach',isbslowbreach,...
+    'bc',bc(end),...
+    'nkfrombc13',nkfrombc13,...
+    'isbclowbreach',isbclowbreach,...
+    'istrendconfirmed',istrendconfirmed,...
+    'rsi',rsi(end));
+
+disp(res);
+
+end
