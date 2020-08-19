e7027e70bd0701f19420c934898e56e8d0a380fe
yangyir <31230104+yangyir@users.noreply.github.com>
2020-08-20 00:29:41 +0800

    Create fractal_b1_status2str.m

---
+++ trading/williams/fractal_b1_status2str.m
@@ -0,0 +1,34 @@
+function [outputstr] = fractal_b1_status2str(status)
+    
+    if status.b1type == 2
+        outputstr = 'mediumbreach';
+    elseif status.b1type == 3
+        outputstr = 'strongbreach';
+    else
+        outputstr = 'weakbreach';
+        return
+    end
+    
+    if status.isvolblowup
+        outputstr = [outputstr,'-volblowup1'];
+    end
+    if ~status.isvolblowup && status.isvolblowup2
+        outputstr = [outputstr,'-volblowup2'];
+    end
+    
+    if status.islvlupbreach
+        outputstr = [outputstr,'-breachup-lvlup'];
+    end
+    
+    if status.istrendconfirmed
+        outputstr = [outputstr,'-trendconfirmed'];
+    else
+        outputstr = [outputstr,'-trendbreak'];
+    end
+    
+    if status.isclose2lvlup
+        outputstr = 'closetolvlup';
+        return
+    end
+    
+end
