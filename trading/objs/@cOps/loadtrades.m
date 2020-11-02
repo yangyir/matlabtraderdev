91cd2522bb94640de5385f4f8bea3d60146d906c
yangyir <31230104+yangyir@users.noreply.github.com>
2020-11-02 16:37:43 +0800

    Update loadtrades.m
    updates with bug fix

--- trading/objs/@cOps/loadtrades.m
+++ trading/objs/@cOps/loadtrades.m
@@ -91,9 +91,11 @@ function [] = loadtrades(obj,varargin)
                         if trade_i.opendirection_ == 1
                             trade_i.riskmanager_.wadopen_ = wad(iopen);
                             trade_i.riskmanager_.wadhigh_ = max(wad(iopen:end));
+                            trade_i.riskmanager_.cphigh_ = max(px(iopen:end,5));
                         elseif trade_i.opendirection_ == -1
                             trade_i.riskmanager_.wadopen_ = wad(iopen);
-                            trade_i.riskmanager_.wadlow_ = mn(wad(iopen:end));
+                            trade_i.riskmanager_.wadlow_ = min(wad(iopen:end));
+                            trade_i.riskmanager_.cplow_ = min(px(iopen:end,5));
                         end
                     end
                 end
