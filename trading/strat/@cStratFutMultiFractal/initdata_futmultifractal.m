bf808e26e1b418cf7b9cc78ee9cc5608cc552d21
yangyir <31230104+yangyir@users.noreply.github.com>
2021-06-22 22:12:00 +0800

    Update initdata_futmultifractal.m
    change from 20 to 10 business days when sampling period is 30m

--- trading/strat/@cStratFutMultiFractal/initdata_futmultifractal.m
+++ trading/strat/@cStratFutMultiFractal/initdata_futmultifractal.m
@@ -20,7 +20,7 @@ function [] = initdata_futmultifractal(stratfractal)
         elseif samplefreqnum == 10 || samplefreqnum == 15
             nbdays = 10;
         elseif samplefreqnum == 30
-            nbdays = 20;
+            nbdays = 10;
         elseif samplefreqnum == 1440
             nbdays = 252;
         else
