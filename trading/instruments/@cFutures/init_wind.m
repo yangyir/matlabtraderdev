f45203259326398ef0a94f54f78e111da1938f56
yangyir <31230104+yangyir@users.noreply.github.com>
2021-08-31 20:32:11 +0800

    Update init_wind.m
    cope with results in CHINESE returned by WIND

--- trading/instruments/@cFutures/init_wind.m
+++ trading/instruments/@cFutures/init_wind.m
@@ -80,6 +80,11 @@ function [] = init_wind(obj,w)
             obj.trading_hours = [str1,';',str2,';',str3];
             obj.trading_break = '10:15-10:30';
             return
+        elseif strcmpi(obj.asset_name,'apple')
+            str1 = '09:00-11:30';
+            str2 = '13:30-15:00';
+            obj.trading_hours = [str1,';',str2];
+            obj.trading_break = '10:15-10:30';
         else
             error('cFutures:init_wind:%s',e.message)
         end
