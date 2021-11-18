09235dc9aa8bd44005b37d3acf78227261f4b657
yangyir <31230104+yangyir@users.noreply.github.com>
2021-11-18 21:35:09 +0800

    Update regressiontest_fractal10_i.m

--- regressiontest/cstrat/fractal/regressiontest_fractal10_i.m
+++ regressiontest/cstrat/fractal/regressiontest_fractal10_i.m
@@ -5,16 +5,20 @@ try
 catch
 end
 %
+<<<<<<< Updated upstream
 codes = {'i2201'};
+=======
+codes = {'i2105'};
+>>>>>>> Stashed changes
 path_ = [getenv('HOME'),'\regressiontest\cstrat\fractal\'];
 for i = 1:length(codes)
     addpath([getenv('DATAPATH'),'\intradaybar\',codes{i},'\']);
     addpath([getenv('DATAPATH'),'\ticks\',codes{i},'\']);
 end
 cd(path_);
-bookname = 'book12';
+bookname = 'ironore';
 strategyname = 'fractal';
-riskconfigfilename = 'config_book11.txt';
+riskconfigfilename = 'config_ironore.txt';
 genconfigfile(strategyname,[path_,riskconfigfilename],'instruments',codes);
 for i = 1:length(codes)
 modconfigfile([path_,riskconfigfilename],'code',codes{i},...
@@ -28,7 +32,11 @@ combo = rtt_setup('countername','ccb_ly_fut',...
     'riskconfigfilename',riskconfigfilename,...
     'initialfundlevel',1e6,...
     'mode','replay',...
+<<<<<<< Updated upstream
     'replayfromdate','2021-10-21','replaytodate','2021-10-21');
+=======
+    'replayfromdate','2021-03-22','replaytodate','2021-03-22');
+>>>>>>> Stashed changes
 combo.strategy.displaysignalonly_ = false;
 combo.mdefut.printflag_ = true;combo.mdefut.print_timeinterval_ = 30*60;
 combo.ops.printflag_ = true;
