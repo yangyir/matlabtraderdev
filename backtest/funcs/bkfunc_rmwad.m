2f3a608ebd14257edd68c88dfdfd9f1234ee533d
yangyir <31230104+yangyir@users.noreply.github.com>
2021-11-18 21:33:56 +0800

    Create bkfunc_rmwad.m

---
+++ backtest/funcs/bkfunc_rmwad.m
@@ -0,0 +1,11 @@
+function [ output_args ] = bkfunc_rmwad( direction,opent,p,wad,freq )
+    if size(p,1) ~= size(wad)
+        error('bkfunc_rmwad:invalid price and wad input')
+    end
+    
+    
+    
+
+
+end
+
