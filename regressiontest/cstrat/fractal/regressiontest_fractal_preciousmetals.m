a56d306811e25a637019e97092ebcc093044c374
yangyir <31230104+yangyir@users.noreply.github.com>
2021-11-18 21:34:30 +0800

    Create regressiontest_fractal_preciousmetals.m

---
+++ regressiontest/cstrat/fractal/regressiontest_fractal_preciousmetals.m
@@ -0,0 +1,52 @@
+%% create config file
+try
+    clear;clc;
+    delete(timerfindall);
+catch
+end
+%
+codes = {'au2106';'ag2106'};
+for i = 1:length(codes)
+    addpath([getenv('DATAPATH'),'ticks\',codes{i}]);
+    addpath([getenv('DATAPATH'),'intradaybar\',codes{i}]);
+end
+path_ = [getenv('HOME'),'\regressiontest\cstrat\fractal\'];
+cd(path_);
+bookname = 'preciousmetals';
+strategyname = 'fractal';
+riskconfigfilename = 'config_preciousmetals.txt';
+genconfigfile(strategyname,[path_,riskconfigfilename],'instruments',codes);
+for i = 1:length(codes)
+modconfigfile([path_,riskconfigfilename],'code',codes{i},...
+    'propnames',{'nfractals';'samplefreq';'baseunits';'maxunits';'riskmanagername';'autotrade'},...
+    'propvalues',{4;'30m';1;1;'spiderman';1});
+end
+%
+combo = rtt_setup('countername','ccb_ly_fut',...
+    'bookname',bookname,...
+    'strategyname',strategyname,...
+    'riskconfigfilename',riskconfigfilename,...
+    'initialfundlevel',1e6,...
+    'mode','replay',...
+    'replayfromdate','2021-03-22','replaytodate','2021-03-22');
+combo.strategy.displaysignalonly_ = false;
+combo.mdefut.printflag_ = true;combo.mdefut.print_timeinterval_ = 30*60;
+combo.ops.printflag_ = true;
+combo.ops.print_timeinterval_ = 30*60;
+combo.strategy.printflag_ = false;
+%%
+combo.mdefut.start;
+combo.ops.start;
+combo.strategy.start;
+%%
+combo.mdefut.stop
+%%
+timers = timerfindall;
+fprintf('\n');
+for i = 1:length(timers)
+    fprintf('%25s:%3s\n',timers(i).name,timers(i).running);
+end
+%%
+combo.ops.condentrustspending_.latest
+%%
+mde_fin_plot(combo.mdefut);
