07122086ace400ae076fb45eb971f3651abf8cb7
yangyir <31230104+yangyir@users.noreply.github.com>
2022-01-17 16:38:15 +0800

    incorporate daily backtest

---
+++ trading/williams/fractal_daily_checker.m
@@ -0,0 +1,57 @@
+function [tblb,tbls,trades,pnl,resstruct] = fractal_daily_checker(code,varargin)
+
+isequity = isinequitypool(code);
+if ~isequity
+    error('fractal_daily_checker:invalid code input:only equity is supported')
+end
+
+p = inputParser;
+p.CaseSensitive = false;p.KeepUnmatched = true;
+p.addParameter('type','',@ischar);
+p.addParameter('plot',false,@islogical);
+p.addParameter('direction',[],@isnumeric);
+p.parse(varargin{:});
+type = p.Results.type;
+doplot = p.Results.plot;
+direction = p.Results.direction;
+
+%load the data
+fn = [code,'_daily.txt'];
+cp = cDataFileIO.loadDataFromTxtFile(fn);
+
+[tblb,tbls,trades,~,resstruct] = fractal_filter({code},{cp},type,direction,doplot);
+
+%backtest trade performance
+n = trades.latest_;
+pnl = cell(n,2);
+for i = 1:n
+    trade = trades.node_(i);
+    j = trade.id_;
+    d = resstruct{1};
+    %
+    for k = j+1:size(d.px,1)
+        extrainfo = fractal_genextrainfo(d,k);
+        if k == size(d.px,1)
+            extrainfo.latestopen = d.px(k,5);
+        else
+            extrainfo.latestopen = d.px(k+1,2);
+        end
+        if strcmpi(trade.status_,'closed'),break;end
+        tradeout = trade.riskmanager_.riskmanagementwithcandle([],...
+            'usecandlelastonly',false,...
+            'debug',false,...
+            'updatepnlforclosedtrade',true,...
+            'extrainfo',extrainfo);
+        if ~isempty(tradeout)
+            pnl{i,1} = tradeout.closepnl_;
+            pnl{i,2} = tradeout.closestr_;
+            break
+        elseif isempty(tradeout) && k == size(d.px,1)
+            pnl{i,1} = trade.runningpnl_;
+            pnl{i,2} = 'timeseries limit reached';
+        end
+    end
+end
+
+
+end
