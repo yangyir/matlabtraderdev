296e944779e54144c43fcfa252795dabd69d7069
yangyir <31230104+yangyir@users.noreply.github.com>
2022-03-25 09:56:30 +0800

    Update replay_timer_fcn.m

--- trading/equityfund/@cETFWatcher/replay_timer_fcn.m
+++ trading/equityfund/@cETFWatcher/replay_timer_fcn.m
@@ -20,31 +20,46 @@ function [] = replay_timer_fcn(mytimerobj,~,event)
                 end
             end
         elseif flag == 2
+            hasbreach = false;
             n_index = size(mytimerobj.codes_index_,1);
             n_sector = size(mytimerobj.codes_sector_,1);
-            latest_index = mytimerobj.conn_.ds_.wsq(mytimerobj.codes_index_,'rt_latest');
-            latest_sector = mytimerobj.conn_.ds_.wsq(mytimerobj.codes_sector_,'rt_latest');
+            latest_index = mytimerobj.conn_.ds_.wsq(mytimerobj.codes_index_,'rt_date,rt_time,rt_latest');
+            latest_sector = mytimerobj.conn_.ds_.wsq(mytimerobj.codes_sector_,'rt_date,rt_time,rt_latest');
             for i = 1:n_index
                 if ~isnan(mytimerobj.intradaybarriers_conditional_index_(i,1)) && ...
-                        latest_index(i) >= mytimerobj.intradaybarriers_conditional_index_(i,1) + 0.001
-                    fprintf('%s:BreachUP\n',mytimerobj.codes_index_{i}(1:end-3));
+                        latest_index(i,3) >= mytimerobj.intradaybarriers_conditional_index_(i,1) + 0.001
+                    dtnum = datenum([num2str(latest_index(i,1)),' ',num2str(latest_index(i,2))],'yyyymmdd HHMMSS');
+                    dtstr = datestr(dtnum,'yyyy-mm-dd:HH:MM');
+                    fprintf('%s:BreachUP:%s:(%s)\n',dtstr,mytimerobj.codes_index_{i}(1:end-3),mytimerobj.names_index_{i});
+                    hasbreach = true;
                 end
                 if ~isnan(mytimerobj.intradaybarriers_conditional_index_(i,2)) && ...
-                        latest_index(i) <= mytimerobj.intradaybarriers_conditional_index_(i,2) - 0.001
-                    fprintf('%s:BreachDN\n',mytimerobj.codes_index_{i}(1:end-3));
+                        latest_index(i,3) <= mytimerobj.intradaybarriers_conditional_index_(i,2) - 0.001
+                    dtnum = datenum([num2str(latest_index(i,1)),' ',num2str(latest_index(i,2))],'yyyymmdd HHMMSS');
+                    dtstr = datestr(dtnum,'yyyy-mm-dd:HH:MM');
+                    fprintf('%s:BreachDN:%s:(%s)\n',dtstr,mytimerobj.codes_index_{i}(1:end-3),mytimerobj.names_index_{i});
+                    hasbreach = true;
                 end
             end
             %
             for i = 1:n_sector
+                if strcmpi(mytimerobj.codes_sector_{i}(1:end-3),'512800'),continue;end
                 if ~isnan(mytimerobj.intradaybarriers_conditional_sector_(i,1)) && ...
-                        latest_sector(i) >= mytimerobj.intradaybarriers_conditional_sector_(i,1) + 0.001
-                    fprintf('%s:BreachUP\n',mytimerobj.codes_sector_{i}(1:end-3));
+                        latest_sector(i,3) >= mytimerobj.intradaybarriers_conditional_sector_(i,1) + 0.001
+                    dtnum = datenum([num2str(latest_sector(i,1)),' ',num2str(latest_sector(i,2))],'yyyymmdd HHMMSS');
+                    dtstr = datestr(dtnum,'yyyy-mm-dd:HH:MM');
+                    fprintf('%s:BreachUP:%s:(%s)\n',dtstr,mytimerobj.codes_sector_{i}(1:end-3),mytimerobj.names_sector_{i});
+                    hasbreach = true;
                 end
                 if ~isnan(mytimerobj.intradaybarriers_conditional_sector_(i,2)) && ...
-                        latest_sector(i) <= mytimerobj.intradaybarriers_conditional_sector_(i,2) - 0.001
-                    fprintf('%s:BreachDN\n',mytimerobj.codes_sector_{i}(1:end-3));
+                        latest_sector(i,3) <= mytimerobj.intradaybarriers_conditional_sector_(i,2) - 0.001
+                    dtnum = datenum([num2str(latest_sector(i,1)),' ',num2str(latest_sector(i,2))],'yyyymmdd HHMMSS');
+                    dtstr = datestr(dtnum,'yyyy-mm-dd:HH:MM');
+                    fprintf('%s:BreachDN:%s:(%s)\n',dtstr,mytimerobj.codes_sector_{i}(1:end-3),mytimerobj.names_sector_{i});
+                    hasbreach = true;
                 end
             end
+            if hasbreach, fprintf('\n'); end
         end
         
     else
