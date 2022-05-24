2078cd967cbd00b72605be228dc93e912d5b101c
yangyir <31230104+yangyir@users.noreply.github.com>
2022-05-25 07:46:26 +0800

    Update history.m
    add price adjustment

--- trading/database/@cWind/history.m
+++ trading/database/@cWind/history.m
@@ -1,10 +1,10 @@
 function data = history(obj,instrument,fields,fromdate,todate)
     %cWind not implemented yet
     if isa(instrument,'cInstrument')
-        [wdata,~,~,wtime] = obj.ds_.wsd(instrument.code_wind,fields,fromdate,todate);
+        [wdata,~,~,wtime] = obj.ds_.wsd(instrument.code_wind,fields,fromdate,todate,'PriceAdj=F');
         data = [wtime,wdata];
     else
-        [wdata,~,~,wtime] = obj.ds_.wsd(instrument,fields,fromdate,todate);
+        [wdata,~,~,wtime] = obj.ds_.wsd(instrument,fields,fromdate,todate,'PriceAdj=F');
         data = [wtime,wdata];
     end
 end
