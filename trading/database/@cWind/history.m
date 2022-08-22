946201044b25be926e04cb0692033f35c419cffa
yangyir <31230104+yangyir@users.noreply.github.com>
2022-08-22 16:15:54 +0800

    updates with csi 1000

--- trading/database/@cWind/history.m
+++ trading/database/@cWind/history.m
@@ -1,10 +1,32 @@
 function data = history(obj,instrument,fields,fromdate,todate)
     %cWind not implemented yet
     if isa(instrument,'cInstrument')
-        [wdata,~,~,wtime] = obj.ds_.wsd(instrument.code_wind,fields,fromdate,todate,'PriceAdj=F');
+        if isa(instrument,'cFX')
+            [wdata,~,~,wtime] = obj.ds_.wsd(instrument.code_wind,fields,fromdate,todate,'TradingCalendar=AMEX');
+        else
+            [wdata,~,~,wtime] = obj.ds_.wsd(instrument.code_wind,fields,fromdate,todate,'PriceAdj=F');
+        end
         data = [wtime,wdata];
     else
-        [wdata,~,~,wtime] = obj.ds_.wsd(instrument,fields,fromdate,todate,'PriceAdj=F');
+        if strcmpi(instrument,'SPTAUUSDOZ.IDC')
+            %gold
+            [wdata,~,~,wtime] = obj.ds_.wsd(instrument,fields,fromdate,todate,'PriceAdj=F','TradingCalendar=LSE');
+        elseif strcmpi(instrument,'B00.IPE')
+            %brent crude
+            [wdata,~,~,wtime] = obj.ds_.wsd(instrument,fields,fromdate,todate,'PriceAdj=F','TradingCalendar=IPE');
+        elseif strcmpi(instrument,'CA03ME.LME')
+            [wdata,~,~,wtime] = obj.ds_.wsd(instrument,fields,fromdate,todate,'PriceAdj=F','TradingCalendar=LME');
+        elseif strcmpi(instrument,'10YRNOTE.GBM') || ...
+                strcmpi(instrument,'USDX.FX') || ...
+                strcmpi(instrument,'EURUSD.FX') || ...
+                strcmpi(instrument,'USDJPY.FX') || ...
+                strcmpi(instrument,'GBPUSD.FX') || ...
+                strcmpi(instrument,'AUDUSD.FX') || ...
+                strcmpi(instrument,'SPX.GI')
+            [wdata,~,~,wtime] = obj.ds_.wsd(instrument,fields,fromdate,todate,'PriceAdj=F','TradingCalendar=AMEX');    
+        else
+            [wdata,~,~,wtime] = obj.ds_.wsd(instrument,fields,fromdate,todate,'PriceAdj=F');
+        end
         data = [wtime,wdata];
     end
 end
