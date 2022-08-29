f67d327d8483d8a18fa82bf605c6b10ee23ed558
yangyir <31230104+yangyir@users.noreply.github.com>
2022-08-29 17:05:58 +0800

    Update history.m

--- trading/database/@cWind/history.m
+++ trading/database/@cWind/history.m
@@ -8,13 +8,25 @@ function data = history(obj,instrument,fields,fromdate,todate)
         end
         data = [wtime,wdata];
     else
-        if strcmpi(instrument,'SPTAUUSDOZ.IDC')
+        if strcmpi(instrument,'SPTAUUSDOZ.IDC') || strcmpi(instrument,'SPTAGUSDOZ.IDC') || ...
+                strcmpi(instrument,'FTSE.GI')
             %gold
             [wdata,~,~,wtime] = obj.ds_.wsd(instrument,fields,fromdate,todate,'PriceAdj=F','TradingCalendar=LSE');
-        elseif strcmpi(instrument,'B00.IPE')
+        elseif strcmpi(instrument,'SX5E.DF')
+            [wdata,~,~,wtime] = obj.ds_.wsd(instrument,fields,fromdate,todate,'PriceAdj=F','TradingCalendar=FSE');
+        elseif strcmpi(instrument,'B00.IPE') || strcmpi(instrument,'B.IPE')
             %brent crude
             [wdata,~,~,wtime] = obj.ds_.wsd(instrument,fields,fromdate,todate,'PriceAdj=F','TradingCalendar=IPE');
-        elseif strcmpi(instrument,'CA03ME.LME')
+        elseif strcmpi(instrument,'S.CBT') || strcmpi(instrument,'C.CBT') || strcmpi(instrument,'W.CBT') 
+            %brent crude
+            [wdata,~,~,wtime] = obj.ds_.wsd(instrument,fields,fromdate,todate,'PriceAdj=F','TradingCalendar=CME');    
+        elseif strcmpi(instrument,'CA03ME.LME') || ...
+                strcmpi(instrument,'CA.LME') || ...
+                strcmpi(instrument,'AH.LME') || ...
+                strcmpi(instrument,'PB.LME') || ...
+                strcmpi(instrument,'ZS.LME') || ...
+                strcmpi(instrument,'NI.LME') || ...
+                strcmpi(instrument,'SN.LME')            
             [wdata,~,~,wtime] = obj.ds_.wsd(instrument,fields,fromdate,todate,'PriceAdj=F','TradingCalendar=LME');
         elseif strcmpi(instrument,'10YRNOTE.GBM') || ...
                 strcmpi(instrument,'USDX.FX') || ...
@@ -22,12 +34,16 @@ function data = history(obj,instrument,fields,fromdate,todate)
                 strcmpi(instrument,'USDJPY.FX') || ...
                 strcmpi(instrument,'GBPUSD.FX') || ...
                 strcmpi(instrument,'AUDUSD.FX') || ...
-                strcmpi(instrument,'SPX.GI')
+                strcmpi(instrument,'AUDJPY.FX') || ...
+                strcmpi(instrument,'SPX.GI') || ...
+                strcmpi(instrument,'IXIC.GI')
             [wdata,~,~,wtime] = obj.ds_.wsd(instrument,fields,fromdate,todate,'PriceAdj=F','TradingCalendar=AMEX');    
         else
             [wdata,~,~,wtime] = obj.ds_.wsd(instrument,fields,fromdate,todate,'PriceAdj=F');
         end
         data = [wtime,wdata];
     end
+    idx = ~isnan(sum(data,2));
+    data = data(idx,:);
 end
 %end of history
