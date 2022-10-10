25d7a74746320d57dcd7d91251d49f4842fff3f0
yangyir <31230104+yangyir@users.noreply.github.com>
2022-10-10 16:28:55 +0800

    Update riskmanagementwithcandle.m

--- trading/riskmanagement/@cSpiderman/riskmanagementwithcandle.m
+++ trading/riskmanagement/@cSpiderman/riskmanagementwithcandle.m
@@ -56,6 +56,72 @@ function [unwindtrade] = riskmanagementwithcandle(obj,candlek,varargin)
     end
     if isempty(idxstart2check), return; end
     
+    %note:20200926:further check whether the trade is open with conditional
+    %entrust executed but in fact the close price did not breach the
+    %relevant barrier and etc
+    signalinfo = trade.opensignal_;
+    if isa(signalinfo,'cFractalInfo')
+        if extrainfo.p(end,1) <= trade.opendatetime1_
+            val = signalinfo.mode_;
+            if strcmpi(val,'conditional-uptrendconfirmed')
+                if extrainfo.p(end,5) < extrainfo.hh(end-1) && extrainfo.p(end,3) > extrainfo.hh(end-1) && extrainfo.p(end,5) < max(extrainfo.teeth(end),extrainfo.lips(end))         
+                    obj.trade_.closedatetime1_ = extrainfo.latestdt;
+                    obj.trade_.closeprice_ = extrainfo.latestopen;
+                    volume = trade.openvolume_;
+                    obj.status_ = 'closed';
+                    obj.trade_.status_ = 'closed';
+                    obj.trade_.runningpnl_ = 0;
+                    instrument = trade.instrument_;
+                    if isempty(instrument)
+                        obj.trade_.closepnl_ = direction*volume*(trade.closeprice_-trade.openprice_);
+                    else
+                        obj.trade_.closepnl_ = direction*volume*(trade.closeprice_-trade.openprice_)/instrument.tick_size * instrument.tick_value;
+                    end
+                    unwindtrade = obj.trade_;
+                    return
+                end
+            elseif strcmpi(val,'conditional-dntrendconfirmed')
+                if extrainfo.p(end,5) > extrainfo.ll(end-1) && extrainfo.p(end,4) < extrainfo.ll(end-1) && extrainfo.p(end,5) > min(extrainfo.teeth(end),extrainfo.lips(end))         
+                    obj.trade_.closedatetime1_ = extrainfo.latestdt;
+                    obj.trade_.closeprice_ = extrainfo.latestopen;
+                    volume = trade.openvolume_;
+                    obj.status_ = 'closed';
+                    obj.trade_.status_ = 'closed';
+                    obj.trade_.runningpnl_ = 0;
+                    instrument = trade.instrument_;
+                    if isempty(instrument)
+                        obj.trade_.closepnl_ = direction*volume*(trade.closeprice_-trade.openprice_);
+                    else
+                        obj.trade_.closepnl_ = direction*volume*(trade.closeprice_-trade.openprice_)/instrument.tick_size * instrument.tick_value;
+                    end
+                    unwindtrade = obj.trade_;
+                    return
+                end
+            elseif strcmpi(val,'conditional-breachuplvlup')
+                if extrainfo.p(end,5) < extrainfo.hh(end-1) && extrainfo.p(end,3) > extrainfo.hh(end-1)
+                    obj.trade_.closedatetime1_ = extrainfo.latestdt;
+                    obj.trade_.closeprice_ = extrainfo.latestopen;
+                    volume = trade.openvolume_;
+                    obj.status_ = 'closed';
+                    obj.trade_.status_ = 'closed';
+                    obj.trade_.runningpnl_ = 0;
+                    instrument = trade.instrument_;
+                    if isempty(instrument)
+                        obj.trade_.closepnl_ = direction*volume*(trade.closeprice_-trade.openprice_);
+                    else
+                        obj.trade_.closepnl_ = direction*volume*(trade.closeprice_-trade.openprice_)/instrument.tick_size * instrument.tick_value;
+                    end
+                    unwindtrade = obj.trade_;
+                    return
+                end
+            elseif strcmpi(val,'conditional-breachdnlvldn')
+            else
+                %do nothing for now
+            end
+        end
+            
+    end
+    
     [ unwindtrade ] = obj.riskmanagement_tdsq('extrainfo',extrainfo,...
         'updatepnlforclosedtrade',updatepnlforclosedtrade);
     if ~isempty(unwindtrade)
