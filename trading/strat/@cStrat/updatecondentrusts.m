9b6cc03c80406ced4bd5ad6c96f94c776cfa8317
yangyir <31230104+yangyir@users.noreply.github.com>
2023-06-07 16:58:35 +0800

    Update updatecondentrusts.m
    refactor the code to make it faster

--- trading/strat/@cStrat/updatecondentrusts.m
+++ trading/strat/@cStrat/updatecondentrusts.m
@@ -1,6 +1,8 @@
 function [] = updatecondentrusts(strategy)
 %cStrat
     if isempty(strategy.helper_), return; end
+    
+    isfractal = isa(strategy,'cStratFutMultiFractal');
 
     try
         ncondpending = strategy.helper_.condentrustspending_.latest;
@@ -27,60 +29,66 @@ function [] = updatecondentrusts(strategy)
                 ticktime = lasttick(1);
                 if ordertime - ticktime > 1/1440, continue;end
                 
-                direction = condentrust.direction;
-                
-                if ~isempty(signalinfo)
-                    try
-                        isflash = strcmpi(signalinfo.wrmode,'flash');
-                    catch
+                if ~isfractal
+                    if ~isempty(signalinfo)
+                        try
+                            isflash = strcmpi(signalinfo.wrmode,'flash');
+                        catch
+                            isflash = false;
+                        end    
+                    else
                         isflash = false;
-                    end    
-                else
-                    isflash = false;
-                end
-                
-                if isflash
-                    %need to make sure either the max or min prices are
-                    %updated or not. if it is updated, then the existing
-                    %conditional entrust shall be removed
-                    if direction == 1 && lasttick(4) < signalinfo.lowestlow
-                        condentrusts2remove.push(condentrust);
-                        continue;
+                    end
+
+                    if isflash
+                        %need to make sure either the max or min prices are
+                        %updated or not. if it is updated, then the existing
+                        %conditional entrust shall be removed
+                        if direction == 1 && lasttick(4) < signalinfo.lowestlow
+                            condentrusts2remove.push(condentrust);
+                            continue;
+                        end
+                        %
+                        if direction == -1 && lasttick(4) > signalinfo.highesthigh
+                            condentrusts2remove.push(condentrust);
+                            continue;
+                        end
                     end
                     %
-                    if direction == -1 && lasttick(4) > signalinfo.highesthigh
-                        condentrusts2remove.push(condentrust);
-                        continue;
-                    end
-                end
-                %
-                if ~isempty(signalinfo)
-                    try
-                        isflashma = strcmpi(signalinfo.wrmode,'flashma') && abs(condpx+9.99)<=1e-5;
-                    catch
+                    if ~isempty(signalinfo)
+                        try
+                            isflashma = strcmpi(signalinfo.wrmode,'flashma') && abs(condpx+9.99)<=1e-5;
+                        catch
+                            isflashma = false;
+                        end    
+                    else
                         isflashma = false;
-                    end    
-                else
-                    isflashma = false;
-                end
-                
-                if isflashma
-                    %we need to check the short ma of wr breaches
-                    %above(below) the long ma of wr
-                    [~,idx] = strategy.hasinstrument(codestr);
-                    short = strategy.wrmashort_(idx);
-                    long = strategy.wrmalong_(idx);
-                    if direction == 1 && short > long
-                        condpx = lasttick(3);
-                        condentrust.price = condpx;
-                    elseif direction == -1 && short < long
-                        condpx = lasttick(2);
-                        condentrust.price = condpx;
+                    end
+
+                    if isflashma
+                        %we need to check the short ma of wr breaches
+                        %above(below) the long ma of wr
+                        [~,idx] = strategy.hasinstrument(codestr);
+                        short = strategy.wrmashort_(idx);
+                        long = strategy.wrmalong_(idx);
+                        if direction == 1 && short > long
+                            condpx = lasttick(3);
+                            condentrust.price = condpx;
+                        elseif direction == -1 && short < long
+                            condpx = lasttick(2);
+                            condentrust.price = condpx;
+                        end
                     end
                 end
                 %
-                if direction == 1 && lasttick(3) >= condpx && abs(condpx+9.99) > 1e-5
-                    [ret,~,errmsg] = strategy.longopen(codestr,volume,'overrideprice',lasttick(3),...
+                lasttick = strategy.mde_fut_.getlasttick(codestr);
+                bid = lasttick(3);
+                ask = lasttick(4);
+                direction = condentrust.direction;
+                ticksize = instrument.tick_size;
+                
+                if direction == 1 && bid >= condpx && abs(condpx+9.99) > 1e-5
+                    [ret,~,errmsg] = strategy.longopen(codestr,volume,'overrideprice',bid+4*ticksize,...
                         'signalinfo',condentrust.signalinfo_);
                     if ret
                         condentrusts2remove.push(condentrust);
@@ -89,8 +97,8 @@ function [] = updatecondentrusts(strategy)
                             condentrusts2remove.push(condentrust);
                         end
                     end
-                elseif direction == -1 && lasttick(2) <= condpx && abs(condpx+9.99) > 1e-5                    
-                    [ret,~,errmsg] = strategy.shortopen(codestr,volume,'overrideprice',lasttick(2),...
+                elseif direction == -1 && ask <= condpx && abs(condpx+9.99) > 1e-5                    
+                    [ret,~,errmsg] = strategy.shortopen(codestr,volume,'overrideprice',ask-4*ticksize,...
                         'signalinfo',condentrust.signalinfo_);
                     if ret
                         condentrusts2remove.push(condentrust);
