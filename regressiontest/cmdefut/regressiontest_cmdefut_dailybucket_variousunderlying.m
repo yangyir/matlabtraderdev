17e34ab232b4d14e94bc7f012eab784d73f76ddd
yangyir <31230104+yangyir@users.noreply.github.com>
2023-07-11 21:03:42 +0800

    Update regressiontest_cmdefut_dailybucket_variousunderlying.m

--- regressiontest/cmdefut/regressiontest_cmdefut_dailybucket_variousunderlying.m
+++ regressiontest/cmdefut/regressiontest_cmdefut_dailybucket_variousunderlying.m
@@ -2,16 +2,16 @@ clc;clear;delete(timerfindall);
 mdefut = cMDEFut;
 mdefut.settimerinterval(0.005);
 % codes = {'IH2307';'T2309';'lh2309';'rb2310';'cu2308';'ag2308'};
-codes = {'T2309''};
+codes = {'l2309'};
 instr = cell(length(codes),1);
 for i = 1:length(codes)
     instr{i} = code2instrument(codes{i});
     mdefut.registerinstrument(instr{i});
     mdefut.setcandlefreq(1440,instr{i});
 end
-%%
+%
 replay_startdt = '2023-06-30';
-replay_enddt = '2023-07-04';
+replay_enddt = '2023-07-07';
 replay_dates = gendates('fromdate',replay_startdt,'todate',replay_enddt);
 ndates = size(replay_dates,1);
 replay_filenames = cell(length(codes),1);
@@ -25,10 +25,12 @@ for i = 1:length(codes)
     mdefut.initreplayer('code',codes{i},'filenames',replay_filenames{i,1});
 end
 
-%%
+%
 mdefut.initcandles;
+mde_fin_plot(mdefut);
 %%
 mdefut.printflag_ = true;
 mdefut.print_timeinterval_ = 30*60;
 mdefut.start;
 %%
+mde_fin_plot(mdefut);
