fc637b821563be764ab89d00b993d3237ad00788
yangyir <31230104+yangyir@users.noreply.github.com>
2023-08-10 22:01:11 +0800

    Create isfile2.m

---
+++ trading/utilities/isfile2.m
@@ -0,0 +1,26 @@
+function [flag,output] = isfile2(fileName,varargin)
+%isfile determine whether the input fileName exists
+%input fileName shall contain the full path
+%e.g.'C:\temp\file'
+p = inputParser;
+p.CaseSensitive = false;
+p.KeepUnmatched = false;
+p.addRequired('fileName',@ischar);
+p.addOptional('fileFormat','.mat',@ischar);
+p.parse(fileName,varargin{:});
+fileName = p.Results.fileName;
+fileFormat = p.Results.fileFormat;
+fileName = [fileName,fileFormat];
+
+try
+    % load data from existing .mat file
+    output = importdata(fileName);
+    flag = 1;
+catch err
+    flag = 0;
+    output = {};
+%     fprintf(['isfile:',err.message,'\n']);
+end
+    
+end
+
