53d3acbe5cf84a928a0d23dffe09f2d34c8ef09e
yangyir <31230104+yangyir@users.noreply.github.com>
2023-10-20 15:03:52 +0800

    Update script_govtbond_analysis.m

--- govtbond/script_govtbond_analysis.m
+++ govtbond/script_govtbond_analysis.m
@@ -4,12 +4,16 @@ fprintf('wind instance initiated...\n');
 dir_ = getenv('DATAPATH');
 dir_data_ = [dir_,'dailybar\'];
 coldefs = {'date','open','high','low','close'};
-%%
+%
 codes_govtbond = {'TB10Y.WI';...%国债10年活跃
 'TB30Y.WI';...%国债30年活跃
 'CDB10Y.WI';%国开10年活跃
 };
 fn_govtbond = {'gzhy_daily.txt';'gzhy_30y_daily.txt';'gkhy_daily.txt'};
+codes_govtbondfut = {'T2312';...%10年国债期货
+    'TL2312';...%30年国债期货
+    };
+%%
 for i = 1:length(codes_govtbond)
     [wdata,~,~,wtime] = w.ds_.wsd(codes_govtbond{i},...
         'open,high,low,close','2000-01-01',today-1,'TradingCalendar=NIB');
@@ -18,9 +22,7 @@ for i = 1:length(codes_govtbond)
     wmat = wmat(widx,:);
     cDataFileIO.saveDataToTxtFile([dir_data_,fn_govtbond{i}],wmat,coldefs,'w',false);
 end
-codes_govtbondfut = {'T2312';...%10年国债期货
-    'TL2312';...%30年国债期货
-    };
+
 %%
 clc;close all;
 set(0,'defaultfigurewindowstyle','docked');
@@ -36,34 +38,31 @@ tools_technicalplot2(mat_gzhy_30y(end-nshift:end,:),4,'活跃30年国债收益率',true)
 tools_technicalplot2(mat_gkhy_10y(end-nshift:end,:),5,'活跃10年国开收益率',true);
 %%
 [tblb_headers,tblb_data,tbls_headers,tbls_data,data,tradesb,tradess,validtradesb,validtradess,kellyb,kellys] = fractal_gettradesummary('gzhy','usefractalupdate',0);
-
 %%
-
+output_gzhy = fractal_kelly_summary('codes',{'gzhy'},'frequency','daily','usefractalupdate',0,'usefibonacci',1,'direction','both');
+[tc_gzhy,tb_gzhy,tbl_gzhy,k_l_gzhy,k_s_gzhy,tblbyasset_l_gzhy,tblbyasset_s_gzhy] = kellydistrubitionsummary(output_gzhy);
+%%
+output_gzhy30y = fractal_kelly_summary('codes',{'gzhy_30y'},'frequency','daily','usefractalupdate',0,'usefibonacci',1,'direction','both');
+[tc_gzhy30y,tb_gzhy30y,tbl_gzhy30y,k_l_gzhy30y,k_s_gzhy30y,tblbyasset_l_gzhy30y,tblbyasset_s_gzhy30y] = kellydistrubitionsummary(output_gzhy30y);
+%%
+[signal_10y_yield,op_yield] = fractal_signal_unconditional(data,0.0025,2);
+[signal_cond_10y_yield,op_cond_yield] = fractal_signal_conditional(data,0.0025,2);
+%% 10年国债期货
+code_10y = 'T2312';
+hd_govtbond_10y = cDataFileIO.loadDataFromTxtFile([code_10y,'_daily.txt']);
+idx = ~isnan(hd_govtbond_10y(:,2)) & ~isnan(hd_govtbond_10y(:,3)) & ~isnan(hd_govtbond_10y(:,4)) & ~isnan(hd_govtbond_10y(:,5));
+hd_govtbond_10y = hd_govtbond_10y(idx,:);
+[op_govtbond_10y,opstruct_govtbond_10y] = tools_technicalplot1(hd_govtbond_10y(:,1:5),2,0,'volatilityperiod',0,'tolerance',0);
+op_govtbond_10y(:,1) = x2mdate(op_govtbond_10y(:,1));
+shift = 62;
+tools_technicalplot2(op_govtbond_10y(end-shift:end,:),6,'govtbond-10y-daily',true);
+[signal_10y,op] = fractal_signal_unconditional(opstruct_govtbond_10y,0.005,2);
 [~,op_10y] = fractal_signal_unconditional(opstruct_govtbond_10y,0.005,2);
 if op_10y.use
     if op_10y.direction == 1
-        fprintf('%6s:bullish:%s\n',code_10y,op_10y.comment);
+        fprintf('%6s:%12s:bullish:%s\n',code_10y,datestr(hd_govtbond_10y(end,1),'yyyy-mm-dd'),op_10y.comment);
     elseif op_10y.direction == -1
-        fprintf('%6s:bearish:%s\n',code_10y,op_10y.comment);
-    end
-end
-
-%
-% 5年国债期货
-hd_govtbond_05y = cDataFileIO.loadDataFromTxtFile([code_05y,'_daily.txt']);
-idx = ~isnan(hd_govtbond_05y(:,2)) & ~isnan(hd_govtbond_05y(:,3)) & ~isnan(hd_govtbond_05y(:,4)) & ~isnan(hd_govtbond_05y(:,5));
-hd_govtbond_05y = hd_govtbond_05y(idx,:);
-[op_govtbond_05y,opstruct_govtbond_05y] = tools_technicalplot1(hd_govtbond_05y(:,1:5),2,0,'volatilityperiod',0,'tolerance',0);
-op_govtbond_05y(:,1) = x2mdate(op_govtbond_05y(:,1));
-shift = 62;
-tools_technicalplot2(op_govtbond_05y(end-shift:end,:),3,'govtbond-05y-daily',true);
-[signal_10y,op] = fractal_signal_unconditional(opstruct_govtbond_10y,0.005,2);
-[~,op_05y] = fractal_signal_unconditional(opstruct_govtbond_05y,0.005,2);
-if op_05y.use
-    if op_05y.direction == 1
-        fprintf('%6s:bullish:%s\n',code_05y,op_05y.comment);
-    elseif op_05y.direction == -1
-        fprintf('%6s:bearish:%s\n',code_05y,op_05y.comment);
+        fprintf('%6s:%12s:bearish:%s\n',code_10y,datestr(hd_govtbond_10y(end,1),'yyyy-mm-dd'),op_10y.comment);
     end
 end
 
