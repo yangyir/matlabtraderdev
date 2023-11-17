3195eed77add8a0e0acc13afc992cc9901386973
yangyir <31230104+yangyir@users.noreply.github.com>
2023-11-17 13:52:17 +0800

    Update regressiontest_fractal_y.m

--- regressiontest/cstrat/fractal/regressiontest_fractal_y.m
+++ regressiontest/cstrat/fractal/regressiontest_fractal_y.m
@@ -9,7 +9,7 @@ codes = {'y2401'};
 for i = 1:length(codes)
     addpath([getenv('DATAPATH'),'ticks\',codes{i}]);
     addpath([getenv('DATAPATH'),'intradaybar\',codes{i}]);
-end
+end 
 path_ = [getenv('HOME'),'\regressiontest\cstrat\fractal\'];
 cd(path_);
 bookname = 'soybeanoil';
@@ -28,7 +28,7 @@ combo = rtt_setup('countername','ccb_ly_fut',...
     'riskconfigfilename',riskconfigfilename,...
     'initialfundlevel',1e6,...
     'mode','replay',...
-    'replayfromdate','2023-11-14','replaytodate','2023-11-14');
+    'replayfromdate','2023-11-16','replaytodate','2023-11-16');
 combo.strategy.displaysignalonly_ = false;
 combo.mdefut.printflag_ = true;combo.mdefut.print_timeinterval_ = 30*60;
 combo.ops.printflag_ = true;
