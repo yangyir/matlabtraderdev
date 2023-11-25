839df3c1438fad6257227af26e649794291a107e
yangyir <31230104+yangyir@users.noreply.github.com>
2023-11-25 09:43:24 +0800

    updates of regression tests

--- regressiontest/cstrat/fractal/regressiontest_fractal_a.m
+++ regressiontest/cstrat/fractal/regressiontest_fractal_a.m
@@ -5,7 +5,7 @@ try
 catch
 end
 %
-codes = {'a2401'};
+codes = {'a2211'};
 for i = 1:length(codes)
     addpath([getenv('DATAPATH'),'ticks\',codes{i}]);
     addpath([getenv('DATAPATH'),'intradaybar\',codes{i}]);
@@ -28,7 +28,7 @@ combo = rtt_setup('countername','ccb_ly_fut',...
     'riskconfigfilename',riskconfigfilename,...
     'initialfundlevel',1e6,...
     'mode','replay',...
-    'replayfromdate','2023-11-21','replaytodate','2023-11-21');
+    'replayfromdate','2022-09-15','replaytodate','2022-09-15');
 combo.strategy.displaysignalonly_ = false;
 combo.mdefut.printflag_ = true;combo.mdefut.print_timeinterval_ = 30*60;
 combo.ops.printflag_ = true;
