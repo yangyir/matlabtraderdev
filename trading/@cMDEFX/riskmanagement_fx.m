8e46cdb69a09817663cd64c66f1800a9d047e53d
yangyir <31230104+yangyir@users.noreply.github.com>
2023-12-04 16:06:56 +0800

    Update riskmanagement_fx.m

--- trading/@cMDEFX/riskmanagement_fx.m
+++ trading/@cMDEFX/riskmanagement_fx.m
@@ -1,8 +1,10 @@
 function [] = riskmanagement_fx(mdefx,varargin)
 %cMDEFX method
+    if isempty(mdefx.trades_fx_),return;end
     ntrades = mdefx.trades_fx_.latest_;
     for i = 1:ntrades
         trade_i = mdefx.trades_fx_.node_(i);
+        
     end
     
 end
