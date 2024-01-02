c3134e6c2585853290db62d9dd6cae6e1bcc9523
yangyir <31230104+yangyir@users.noreply.github.com>
2024-01-02 20:50:54 +0800

    update regression tests

--- regressiontest/cstrat/fractal/regressiontest_fractal_c.m
+++ regressiontest/cstrat/fractal/regressiontest_fractal_c.m
@@ -5,7 +5,7 @@ try
 catch
 end
 %
-codes = {'c2307'};
+codes = {'c2405'};
 for i = 1:length(codes)
     addpath([getenv('DATAPATH'),'ticks\',codes{i}]);
     addpath([getenv('DATAPATH'),'intradaybar\',codes{i}]);
@@ -19,7 +19,7 @@ genconfigfile(strategyname,[path_,riskconfigfilename],'instruments',codes);
 for i = 1:length(codes)
 modconfigfile([path_,riskconfigfilename],'code',codes{i},...
     'propnames',{'nfractals';'samplefreq';'baseunits';'maxunits';'riskmanagername';'autotrade'},...
-    'propvalues',{4;'30m';1;1;'spiderman';1});
+    'propvalues',{4;'30m';2;2;'spiderman';1});
 end
 %
 combo = rtt_setup('countername','ccb_ly_fut',...
@@ -28,14 +28,14 @@ combo = rtt_setup('countername','ccb_ly_fut',...
     'riskconfigfilename',riskconfigfilename,...
     'initialfundlevel',1e6,...
     'mode','replay',...
-    'replayfromdate','2023-06-02','replaytodate','2023-06-02');
+    'replayfromdate','2023-12-25','replaytodate','2023-12-29');
 combo.strategy.displaysignalonly_ = false;
 combo.mdefut.printflag_ = true;combo.mdefut.print_timeinterval_ = 30*60;
 combo.ops.printflag_ = true;
 combo.ops.print_timeinterval_ = 30*60;
 combo.strategy.printflag_ = false;
+combo.strategy.load_kelly_intraday('directory',[getenv('onedrive'),'\fractal backtest\kelly distribution\matlab\agriculture\'],'filename','strat_intraday_agriculture.mat');
 set(0,'DefaultFigureWindowStyle','docked');
-mde_fin_plot(combo.mdefut);
 %%
 combo.mdefut.start;
 combo.ops.start;
