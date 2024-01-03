5b085f301f8f764e447124726e8a1112087d514d
yangyir <31230104+yangyir@users.noreply.github.com>
2024-01-03 17:21:38 +0800

    Update kellydistribution_comdty_daily.m

--- backtest/kellydistribution/kellydistribution_comdty_daily.m
+++ backtest/kellydistribution/kellydistribution_comdty_daily.m
@@ -10,9 +10,9 @@ for i = 3:nfiles
     this_code = this_name(1:end-10);
     this_instr = code2instrument(this_code);
     if isempty(this_instr.asset_name), continue;end
-    if strcmpi(this_instr,'eqindex_300') || strcmpi(this_instr,'eqindex_50')  || ...
-            strcmpi(this_instr,'eqindex_50') || strcmpi(this_instr,'eqindex_500') || ...
-            strcmpi(this_instr,'thermal coal')
+    if strcmpi(this_instr.asset_name,'eqindex_300') || strcmpi(this_instr.asset_name,'eqindex_50')  || strcmpi(this_instr.asset_name,'eqindex_1000') || strcmpi(this_instr.asset_name,'eqindex_500') || ...
+            strcmpi(this_instr.asset_name,'govtbond_10y') || strcmpi(this_instr.asset_name,'govtbond_30y') || strcmpi(this_instr.asset_name,'govtbond_5y') || ...
+            strcmpi(this_instr.asset_name,'thermal coal')
         continue;
     end
     ncode = ncode + 1;
@@ -24,26 +24,16 @@ listassets = listassets(1:ncode,:);
 tbl = table(listcodes,listassets);
 listassetsunique = unique(listassets);
 %%
-% for i = 1:length(listassetsunique)
-%     try
-        idx_i = strcmpi(listassets,'soybean oil');
-        codes_i = listcodes(idx_i);
-        output_comdty_daily = fractal_kelly_summary('codes',codes_i,'frequency','daily','usefractalupdate',0,'usefibonacci',1,'direction','both');
-%     catch
-%         fprintf('error in %s\n',listassetsunique{i});
-%     end
-% end
+output_comdty_daily = fractal_kelly_summary('codes',listcodes,'frequency','daily','usefractalupdate',0,'usefibonacci',1,'direction','both');
 %%
-idx_s = strcmpi(listassets,'fuel oil');
-codes_s = listcodes(idx_s);
-for j = 1:length(codes_s)
-    try
-    data_j = fractal_gettradesummary(codes_s{j},...
-                'frequency','daily',...
-                'usefractalupdate',0,...
-                'usefibonacci',1,...
-                'direction','both');
-    catch
-        fprintf('error in %s\n',codes_s{j});
-    end
-end
+[~,~,tbl_comdty_daily,~,~,~,~,strat_daily_comdty] = kellydistributionsummary(output_comdty_daily);
+%%
+[tblreport_comdty_daily,statsreport_comdty_daily] = kellydistributionreport(tbl_comdty_daily,strat_daily_comdty);
+%%
+idx_i = strcmpi(listassets,'soybean oil');
+codes_i = listcodes(idx_i);
+output_comdty_i = fractal_kelly_summary('codes',codes_i,'frequency','daily','usefractalupdate',0,'usefibonacci',1,'direction','both');
+%%
+dir_ = [getenv('onedrive'),'\fractal backtest\kelly distribution\matlab\comdty\'];
+save([dir_,'strat_daily_comdty.mat'],'strat_daily_comdty');
+fprintf('file saved...\n');
