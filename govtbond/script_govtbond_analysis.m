b5d5b3bc43f1c3702eb6e1fb7a5fc16857d04531
yangyir <31230104+yangyir@users.noreply.github.com>
2024-01-09 15:12:25 +0800

    Update script_govtbond_analysis.m

--- govtbond/script_govtbond_analysis.m
+++ govtbond/script_govtbond_analysis.m
@@ -1,5 +1,5 @@
 %%
-w = cWind;
+% w = cWind;
 fprintf('wind instance initiated...\n');
 dir_ = getenv('DATAPATH');
 dir_data_ = [dir_,'dailybar\'];
@@ -13,6 +13,7 @@ fn_govtbond = {'gzhy_daily.txt';'gzhy_30y_daily.txt';'gkhy_daily.txt'};
 codes_govtbondfut = {'T2403';...%10年国债期货
     'TL2403';...%30年国债期货
     };
+dir_ = [getenv('onedrive'),'\fractal backtest\kelly distribution\matlab\govtbond\'];
 %%
 for i = 1:length(codes_govtbond)
     [wdata,~,~,wtime] = w.ds_.wsd(codes_govtbond{i},...
@@ -38,14 +39,22 @@ tools_technicalplot2(mat_gzhy_30y(end-nshift:end,:),4,'活跃30年国债收益率',true)
 % tools_technicalplot2(mat_gkhy_10y(end-nshift:end,:),5,'活跃10年国开收益率',true);
 
 %%
-output_gzhy = fractal_kelly_summary('codes',{'gzhy'},'frequency','daily','usefractalupdate',0,'usefibonacci',1,'direction','both');
+output_gzhy10y = fractal_kelly_summary('codes',{'gzhy'},'frequency','daily','usefractalupdate',0,'usefibonacci',1,'direction','both');
 close all;
-[~,~,tbl_gzhy,~,~,~,~,strat_gzhy_daily] = kellydistributionsummary(output_gzhy,true);
-[tblreport_gzhy_daily,statsreport_gzhy_daily] = kellydistributionreport(tbl_gzhy,strat_gzhy_daily);
+[~,~,tbl_gzhy10y,~,~,~,~,strat_gzhy10y] = kellydistributionsummary(output_gzhy10y,true);
+[tblreport_gzhy10y,statsreport_gzhy10y] = kellydistributionreport(tbl_gzhy10y,strat_gzhy10y);
+save([dir_,'strat_gzhy10y.mat'],'strat_gzhy10y');
+save([dir_,'tblreport_gzhy10y.mat'],'tblreport_gzhy10y');
+fprintf('file of 10y saved....\n');
+
 %%
 output_gzhy30y = fractal_kelly_summary('codes',{'gzhy_30y'},'frequency','daily','usefractalupdate',0,'usefibonacci',1,'direction','both');
 close all;
-[~,~,tbl_gzhy30y,~,~,~,~,strat_gzhy30y_daily] = kellydistrubitionsummary(output_gzhy30y,true);
+[~,~,tbl_gzhy30y,~,~,~,~,strat_gzhy30y] = kellydistributionsummary(output_gzhy30y,true);
+[tblreport_gzhy30y,statsreport_gzhy30y] = kellydistributionreport(tbl_gzhy10y,strat_gzhy30y);
+save([dir_,'strat_gzhy30y.mat'],'strat_gzhy30y');
+save([dir_,'tblreport_gzhy30y.mat'],'tblreport_gzhy30y');
+fprintf('file of 30y saved....\n');
 
 %%
 [signal_10y_yield,op_yield] = fractal_signal_unconditional(data,0.0025,2);
