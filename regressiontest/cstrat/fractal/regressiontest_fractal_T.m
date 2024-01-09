91eee410bac0f8c9ceaef67d0d3a2e4e65fc0ef9
yangyir <31230104+yangyir@users.noreply.github.com>
2024-01-10 07:37:43 +0800

    Update regressiontest_fractal_T.m

--- regressiontest/cstrat/fractal/regressiontest_fractal_T.m
+++ regressiontest/cstrat/fractal/regressiontest_fractal_T.m
@@ -5,21 +5,21 @@ try
 catch
 end
 %
-codes = {'T2309'};
+codes = {'T2403'};
 for i = 1:length(codes)
     addpath([getenv('DATAPATH'),'ticks\',codes{i}]);
     addpath([getenv('DATAPATH'),'intradaybar\',codes{i}]);
 end
 path_ = [getenv('HOME'),'\regressiontest\cstrat\fractal\'];
 cd(path_);
-bookname = 'govtbond_t';
+bookname = 'govtbond_t_30m';
 strategyname = 'fractal';
-riskconfigfilename = 'config_govtbond_t.txt';
+riskconfigfilename = 'config_govtbond_t_30m.txt';
 genconfigfile(strategyname,[path_,riskconfigfilename],'instruments',codes);
 for i = 1:length(codes)
 modconfigfile([path_,riskconfigfilename],'code',codes{i},...
-    'propnames',{'nfractals';'samplefreq';'baseunits';'maxunits';'riskmanagername';'autotrade'},...
-    'propvalues',{4;'30m';1;1;'spiderman';1});
+    'propnames',{'nfractals';'samplefreq';'baseunits';'maxunits';'riskmanagername';'autotrade';'usefractalupdate'},...
+    'propvalues',{4;'30m';1;1;'spiderman';1;0});
 end
 %
 combo = rtt_setup('countername','ccb_ly_fut',...
@@ -28,12 +28,13 @@ combo = rtt_setup('countername','ccb_ly_fut',...
     'riskconfigfilename',riskconfigfilename,...
     'initialfundlevel',1e6,...
     'mode','replay',...
-    'replayfromdate','2023-06-07','replaytodate','2023-06-07');
+    'replayfromdate','2023-11-30','replaytodate','2023-11-30');
 combo.strategy.displaysignalonly_ = false;
-combo.mdefut.printflag_ = true;combo.mdefut.print_timeinterval_ =  10*60;
-combo.ops.printflag_ = true;
-combo.ops.print_timeinterval_ = 30*60;
+combo.mdefut.printflag_ = true;combo.mdefut.print_timeinterval_ =  30*60;
+combo.ops.printflag_ = true;combo.ops.print_timeinterval_ = 30*60;
 combo.strategy.printflag_ = false;
+combo.strategy.load_kelly_intraday('directory',[getenv('onedrive'),'\fractal backtest\kelly distribution\matlab\govtbondfut\'],...
+    'filename','strat_govtbondfut_30m.mat');
 %%
 combo.mdefut.start;
 combo.ops.start;
