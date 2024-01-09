f9fe05d07098da9730d8f59edeebb982134c3025
yangyir <31230104+yangyir@users.noreply.github.com>
2024-01-09 17:27:54 +0800

    Update regressiontest_fractal_T_15m.m

--- regressiontest/cstrat/fractal/regressiontest_fractal_T_15m.m
+++ regressiontest/cstrat/fractal/regressiontest_fractal_T_15m.m
@@ -5,7 +5,7 @@ try
 catch
 end
 %
-codes = {'T2403'};
+codes = {'T2312'};
 for i = 1:length(codes)
     addpath([getenv('DATAPATH'),'ticks\',codes{i}]);
     addpath([getenv('DATAPATH'),'intradaybar\',codes{i}]);
@@ -19,7 +19,7 @@ genconfigfile(strategyname,[path_,riskconfigfilename],'instruments',codes);
 for i = 1:length(codes)
 modconfigfile([path_,riskconfigfilename],'code',codes{i},...
     'propnames',{'nfractals';'samplefreq';'baseunits';'maxunits';'riskmanagername';'autotrade'},...
-    'propvalues',{8;'15m';1;1;'spiderman';1});
+    'propvalues',{4;'15m';1;1;'spiderman';1});
 end
 %
 combo = rtt_setup('countername','ccb_ly_fut',...
@@ -28,13 +28,14 @@ combo = rtt_setup('countername','ccb_ly_fut',...
     'riskconfigfilename',riskconfigfilename,...
     'initialfundlevel',1e6,...
     'mode','replay',...
-    'replayfromdate','2023-12-05','replaytodate','2023-12-05');
+    'replayfromdate','2023-09-01','replaytodate','2023-09-01');
 combo.strategy.displaysignalonly_ = false;
 combo.mdefut.printflag_ = true;combo.mdefut.print_timeinterval_ =  15*60;
 combo.ops.printflag_ = true;
 combo.ops.print_timeinterval_ = 15*60;
 combo.strategy.printflag_ = false;
-combo.strategy.load_kelly_intraday('directory',[getenv('onedrive'),'\fractal backtest\kelly distribution\matlab\govtbondfut\'],'filename','strat_intraday_govtbondfut_5m.mat');
+combo.strategy.load_kelly_intraday('directory',[getenv('onedrive'),'\fractal backtest\kelly distribution\matlab\govtbondfut\'],...
+    'filename','strat_govtbondfut_15m.mat');
 set(0,'DefaultFigureWindowStyle','docked');
 %%
 combo.mdefut.start;
