6bc34784ec98756737c9eba3845f378b51920e20
yangyir <31230104+yangyir@users.noreply.github.com>
2024-01-09 17:28:20 +0800

    Update kellydistribution_argriculture_intraday.m

--- backtest/kellydistribution/kellydistribution_argriculture_intraday.m
+++ backtest/kellydistribution/kellydistribution_argriculture_intraday.m
@@ -1,6 +1,6 @@
-foldername = [getenv('onedrive'),'\matlabdev\agriculture\'];
-shortcodes = {'oi';'p';'y';'m';'rm';'a';'ap';'c';'CF';'jd';'lh';'ru';'SR'};
-codes_agriculture = cell(10000,1);
+foldername = [getenv('onedrive'),'\matlabdev\industrial\'];
+shortcodes = {'fg';'hc';'i';'j';'jm';'rb';'v'};
+codes_industry = cell(10000,1);
 ncodes = 0;
 for i = 1:length(shortcodes)
     foldername_i = [foldername,shortcodes{i}];
@@ -8,19 +8,24 @@ for i = 1:length(shortcodes)
     for j = 3:size(listing_i,1)
         ncodes = ncodes + 1;
         fn_j = listing_i(j).name;
-        codes_agriculture{ncodes,1} = fn_j(1:end-4);
+        codes_industry{ncodes,1} = fn_j(1:end-4);
     end
 end
-codes_agriculture = codes_agriculture(1:ncodes,:);
+codes_industry = codes_industry(1:ncodes,:);
 %
-output_agriculture = fractal_kelly_summary('codes',codes_agriculture,'frequency','intraday','usefractalupdate',0,'usefibonacci',1,'direction','both');
+output_industry = fractal_kelly_summary('codes',codes_industry,'frequency','intraday','usefractalupdate',0,'usefibonacci',1,'direction','both');
 %
-[~,~,tbl_agriculture_i,~,~,~,~,strat_intraday_agriculture] = kellydistributionsummary(output_agriculture);
+[~,~,tbl_industry_i,~,~,~,~,strat_intraday_industry] = kellydistributionsummary(output_industry);
 %
-[tbl_report_agriculture_i,stats_report_agriculture_i] = kellydistributionreport(tbl_agriculture_i,strat_intraday_agriculture);
+[tbl_report_industry_i,stats_report_industry_i] = kellydistributionreport(tbl_industry_i,strat_intraday_industry);
 %
-dir_ = [getenv('onedrive'),'\fractal backtest\kelly distribution\matlab\agriculture\'];
-save([dir_,'strat_intraday_agriculture.mat'],'strat_intraday_agriculture');
+dir_ = [getenv('onedrive'),'\fractal backtest\kelly distribution\matlab\industry\'];
+try
+    cd(dir_);
+catch
+    mkdir(dir_);
+end
+save([dir_,'strat_intraday_industry.mat'],'strat_intraday_industry');
 fprintf('file saved...\n');
 
 %%
