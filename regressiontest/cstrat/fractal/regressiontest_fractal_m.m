adf8b41c3a46bc982ab62c6c62da788fbbc51eb0
yangyir <31230104+yangyir@users.noreply.github.com>
2024-01-28 21:33:03 +0800

    Update regressiontest_fractal_m.m

--- regressiontest/cstrat/fractal/regressiontest_fractal_m.m
+++ regressiontest/cstrat/fractal/regressiontest_fractal_m.m
@@ -5,7 +5,7 @@ try
 catch
 end
 %
-codes = {'m2401'};
+codes = {'m2405'};
 for i = 1:length(codes)
     addpath([getenv('DATAPATH'),'ticks\',codes{i}]);
     addpath([getenv('DATAPATH'),'intradaybar\',codes{i}]);
@@ -28,13 +28,13 @@ combo = rtt_setup('countername','ccb_ly_fut',...
     'riskconfigfilename',riskconfigfilename,...
     'initialfundlevel',1e6,...
     'mode','replay',...
-    'replayfromdate','2023-12-08','replaytodate','2023-12-08');
+    'replayfromdate','2024-01-25','replaytodate','2024-01-26');
 combo.strategy.displaysignalonly_ = false;
 combo.mdefut.printflag_ = true;combo.mdefut.print_timeinterval_ = 30*60;
 combo.ops.printflag_ = true;
 combo.ops.print_timeinterval_ = 30*60;
 combo.strategy.printflag_ = false;
-combo.strategy.load_kelly_intraday('directory',[getenv('onedrive'),'\fractal backtest\kelly distribution\matlab\grease\'],'filename','strat_intraday_grease.mat');
+combo.strategy.load_kelly_intraday('directory',[getenv('onedrive'),'\fractal backtest\kelly distribution\matlab\comdty\'],'filename','strat_comdty_i.mat');
 set(0,'DefaultFigureWindowStyle','docked');
 %%
 combo.mdefut.start;
