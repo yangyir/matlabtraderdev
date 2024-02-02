c1d26cd311485fb3de755e0f5151e8b64642290c
yangyir <31230104+yangyir@users.noreply.github.com>
2024-02-02 08:30:29 +0800

    Update fractal_signal_conditional.m

--- trading/williams/fractal_signal_conditional.m
+++ trading/williams/fractal_signal_conditional.m
@@ -378,7 +378,7 @@ function [signal,op,flags] = fractal_signal_conditional(ei,ticksize,nfractal,var
             idx_bs_start = idx_bs_last-bs_last+1;
             if size(ei.bc,1)-idx_bc13_last <= 12 && ...
                     bs_last >= 22 &&...
-                    idx_bs_start < idx_bc13_last && idx_bs_last >= idx_bc13_last
+                    idx_bs_start < idx_bc13_last && idx_bs_last <= idx_bc13_last
                 shorttrend = false;
             end
         end
