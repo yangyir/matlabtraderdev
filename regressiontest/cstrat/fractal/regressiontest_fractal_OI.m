09a1f0244d7a761edaf49f89ac817395badb1018
yangyir <31230104+yangyir@users.noreply.github.com>
2024-02-04 09:14:17 +0800

    update regression tests for grease products

--- regressiontest/cstrat/fractal/regressiontest_fractal_OI.m
+++ regressiontest/cstrat/fractal/regressiontest_fractal_OI.m
@@ -5,7 +5,7 @@ try
 catch
 end
 %
-codes = {'OI401'};
+codes = {'OI405'};
 for i = 1:length(codes)
     addpath([getenv('DATAPATH'),'ticks\',codes{i}]);
     addpath([getenv('DATAPATH'),'intradaybar\',codes{i}]);
@@ -28,7 +28,7 @@ combo = rtt_setup('countername','ccb_ly_fut',...
     'riskconfigfilename',riskconfigfilename,...
     'initialfundlevel',1e6,...
     'mode','replay',...
-    'replayfromdate','2023-11-08','replaytodate','2023-11-08');
+    'replayfromdate','2024-01-31','replaytodate','2024-02-02');
 combo.strategy.displaysignalonly_ = false;
 combo.mdefut.printflag_ = true;combo.mdefut.print_timeinterval_ = 30*60;
 combo.ops.printflag_ = true;
--- regressiontest/cstrat/fractal/regressiontest_fractal_rm.m
+++ regressiontest/cstrat/fractal/regressiontest_fractal_rm.m
@@ -5,7 +5,7 @@ try
 catch
 end
 %
-codes = {'RM401'};
+codes = {'RM405'};
 for i = 1:length(codes)
     addpath([getenv('DATAPATH'),'ticks\',codes{i}]);
     addpath([getenv('DATAPATH'),'intradaybar\',codes{i}]);
@@ -28,13 +28,13 @@ combo = rtt_setup('countername','ccb_ly_fut',...
     'riskconfigfilename',riskconfigfilename,...
     'initialfundlevel',1e6,...
     'mode','replay',...
-    'replayfromdate','2023-10-24','replaytodate','2023-10-24');
+    'replayfromdate','2024-01-29','replaytodate','2024-02-02');
 combo.strategy.displaysignalonly_ = false;
 combo.mdefut.printflag_ = true;combo.mdefut.print_timeinterval_ = 30*60;
 combo.ops.printflag_ = true;
 combo.ops.print_timeinterval_ = 30*60;
 combo.strategy.printflag_ = false;
-combo.strategy.load_kelly_intraday('directory',[getenv('onedrive'),'\fractal backtest\kelly distribution\matlab\grease\'],'filename','strat_intraday_grease.mat');
+combo.strategy.load_kelly_intraday('directory',[getenv('onedrive'),'\fractal backtest\kelly distribution\matlab\comdty\'],'filename','strat_comdty_i.mat');
 set(0,'DefaultFigureWindowStyle','docked');
 %%
 combo.mdefut.start;
