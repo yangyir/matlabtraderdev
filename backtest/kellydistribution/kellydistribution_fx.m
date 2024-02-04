114f10b5db8decaa8758bb73bc48e5fb58548b93
yangyir <31230104+yangyir@users.noreply.github.com>
2024-02-04 09:47:31 +0800

    Update kellydistribution_fx.m

--- backtest/kellydistribution/kellydistribution_fx.m
+++ backtest/kellydistribution/kellydistribution_fx.m
@@ -2,83 +2,31 @@ names_fx = {'usdx';'eurusd';'usdjpy';'gbpusd';'audusd';'usdcad';'usdchf';...
     'eurjpy';'eurchf';'gbpeur';'gbpjpy';'audjpy';...
     'usdcnh'};
 
-output_daily_fx = fractal_kelly_summary('codes',names_fx,...
+output_fx_daily = fractal_kelly_summary('codes',names_fx,...
     'frequency','daily','usefractalupdate',0,'usefibonacci',1,'direction','both');
-%%
-[rp_tc_fx,rp_tb_fx,tbl_fx,k_l_fx,k_s_fx,tblbyasset_l_fx,tblbyasset_s_fx] = kellydistributionsummary(output_daily_fx);
-%%
-signal_l_valid_fx = k_l_fx.opensignal_unique_l(logical(k_l_fx.use_unique_l));
-signal_s_valid_fx = k_s_fx.opensignal_unique_s(logical(k_s_fx.use_unique_s));
-assetlist_fx = unique([tblbyasset_l_fx.assetlist;tblbyasset_s_fx.assetlist]);
-nasset = size(assetlist_fx,1);
-%
-WMat_L = zeros(length(signal_l_valid_fx),nasset);
-RMat_L = WMat_L;
-KMat_L = WMat_L;
-for i = 1:length(signal_l_valid_fx)
-    for j = 1:nasset
-        ret = kellyempirical('distribution',output_daily_fx,'assetname',assetlist_fx{j},'direction','l','signalname',signal_l_valid_fx{i});
-        WMat_L(i,j) = ret.W;
-        RMat_L(i,j) = ret.R;
-        KMat_L(i,j) = ret.K;
-    end
-end
-%
-WMat_S = zeros(length(signal_s_valid_fx),nasset);
-RMat_S = WMat_S;
-KMat_S = WMat_S;
-for i = 1:length(signal_s_valid_fx)
-    for j = 1:nasset
-        ret = kellyempirical('distribution',output_daily_fx,'assetname',assetlist_fx{j},'direction','s','signalname',signal_s_valid_fx{i});
-        WMat_S(i,j) = ret.W;
-        RMat_S(i,j) = ret.R;
-        KMat_S(i,j) = ret.K;
-    end
-end
-%%
-strat_daily_fx = struct('tblbyasset_l',tblbyasset_l_fx,...
-    'tblbyasset_s',tblbyasset_s_fx,...
-    'kelly_table_l',k_l_fx,...
-    'kelly_table_s',k_s_fx,...
-    rp_tc_fx{1}.name,rp_tc_fx{1}.table,...
-    rp_tc_fx{2}.name,rp_tc_fx{2}.table,...
-    rp_tc_fx{3}.name,rp_tc_fx{3}.table,...
-    rp_tc_fx{4}.name,rp_tc_fx{4}.table,...
-    'breachuplvlup_tb',rp_tb_fx{1}.table,...
-    'breachdnlvldn_tb',rp_tb_fx{2}.table,...
-    'breachupsshighvalue_tb',rp_tb_fx{3}.table,...
-    'breachdnbshighvalue_tb',rp_tb_fx{4}.table,...
-    'breachuplvlup_tc',rp_tc_fx{5}.table,...
-    'breachdnlvldn_tc',rp_tc_fx{6}.table,...
-    'breachupsshighvalue_tc',rp_tc_fx{7}.table,...
-    'breachdnbshighvalue_tc',rp_tc_fx{8}.table,...
-    'breachuphighsc13',rp_tc_fx{9}.table,...
-    'breachdnlowbc13',rp_tc_fx{10}.table,...
-    'kelly_matrix_l',KMat_L,...
-    'kelly_matrix_s',KMat_S,...
-    'winprob_matrix_l',WMat_L,...
-    'winprob_matrix_s',WMat_S,...
-    'signal_l',{signal_l_valid_fx},...
-    'signal_s',{signal_s_valid_fx},...
-    'asset_list',{assetlist_fx'});
-%%
-dir_ = [getenv('onedrive'),'\fractal backtest\kelly distribution\matlab\fx\'];
-save([dir_,'strat_daily_fx.mat'],'strat_daily_fx');
-fprintf('file saved...\n');
 
 %%
-[tbl_report_fx,stats_report_fx] = kellydistributionreport(tbl_fx,strat_daily_fx);
+[~,~,tbl_fx_daily,~,~,~,~,strat_fx_daily] = kellydistributionsummary(output_fx_daily);
+%
+[tbl_report_fx_daily,stats_report_fx_daily] = kellydistributionreport(tbl_fx_daily,strat_fx_daily);
+
 %%
-output2_daily_fx = fractal_kelly_summary('codes',names_fx,...
-    'frequency','daily','usefractalupdate',0,'usefibonacci',1,'direction','both',...
-    'fromdate','2023-10-30');
-%%
-[~,~,tbl2_fx,~,~,~,~] = kellydistributionsummary(output2_daily_fx,true);
-tbl_report2_fx = kellydistributionreport(tbl2_fx,strat_daily_fx);
-close all;
-%%
-code = 'gbpusd';
-dailybar_ = cDataFileIO.loadDataFromTxtFile([getenv('datapath'),'globalmacro\',code,'_daily.txt']);
-[dailybarmat_,dailybarstruct_] = tools_technicalplot1(dailybar_,2,false);dailybarmat_(:,1) = x2mdate(dailybarmat_(:,1));
-trade_ = fractal_latestposition('code',code,'extrainfo',dailybarstruct_,'usefractalupdate',0);
-tools_technicalplot2(dailybarmat_(end-61:end,:),2,code,true,0.0001);
+dir_ = [getenv('onedrive'),'\fractal backtest\kelly distribution\matlab\fx\'];
+try
+    cd(dir_);
+catch
+    mkdir(dir_);
+    cd(dir_);
+end
+save([dir_,'strat_fx_daily.mat'],'strat_fx_daily');
+fprintf('strat M-file saved...\n');
+
+save([dir_,'tbl_report_fx_daily.mat'],'tbl_report_fx_daily');
+fprintf('tbl report M-file saved...\n');
+
+save([dir_,'output_fx_daily.mat'],'output_fx_daily');
+fprintf('output M-file saved...\n');
+%
+filename = [getenv('onedrive'),'\fractal backtest\kelly distribution\tbl_report_fx_daily.xlsx'];
+writetable(tbl_report_fx_daily,filename,'Sheet',1,'Range','A1');
+fprintf('excel file saved...\n');
