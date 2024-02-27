50fb3693dcd36676f25252eac07ea20c66ef96da
yangyir <31230104+yangyir@users.noreply.github.com>
2024-02-27 15:09:34 +0800

    base metal regression tests updates

--- regressiontest/cstrat/fractal/regressiontest_fractal_ni.m
+++ regressiontest/cstrat/fractal/regressiontest_fractal_ni.m
@@ -5,7 +5,7 @@ try
 catch
 end
 %
-codes = {'ni2403'};
+codes = {'ni2405'};
 for i = 1:length(codes)
     addpath([getenv('DATAPATH'),'ticks\',codes{i}]);
     addpath([getenv('DATAPATH'),'intradaybar\',codes{i}]);
@@ -28,7 +28,7 @@ combo = rtt_setup('countername','ccb_ly_fut',...
     'riskconfigfilename',riskconfigfilename,...
     'initialfundlevel',1e6,...
     'mode','replay',...
-    'replayfromdate','2024-02-23','replaytodate','2024-02-23');
+    'replayfromdate','2024-02-26','replaytodate','2024-02-26');
 combo.strategy.displaysignalonly_ = false;
 combo.mdefut.printflag_ = true;combo.mdefut.print_timeinterval_ = 30*60;
 combo.ops.printflag_ = true;
