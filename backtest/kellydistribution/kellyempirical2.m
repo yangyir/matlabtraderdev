6e6220edff71026333ea712c5e749a91f18fee56
yangyir <31230104+yangyir@users.noreply.github.com>
2024-04-10 15:22:04 +0800

    Update kellyempirical2.m
    bug fixed

--- backtest/kellydistribution/kellyempirical2.m
+++ backtest/kellydistribution/kellyempirical2.m
@@ -31,10 +31,16 @@ function [ret] = kellyempirical2(varargin)
     tblOut = tblInput(idx2check,:);
     
     [winp_running,R_running,kelly_running] = calcrunningkelly(tblOut.pnlrel);
-    N = size(winp_running,1);
-    W = winp_running(end);
-    R = R_running(end);
-    K = kelly_running(end);
+    N = size(tblOut.pnlrel,1);
+    if N == 0
+        W = NaN;
+        R = NaN;
+        K = NaN;
+    else
+        W = winp_running(end);
+        R = R_running(end);
+        K = kelly_running(end);
+    end
     
     ret = struct('N',N,'W',W,'R',R,'K',K);
 end
