987094a62090d4ac471e5db0056bf36a47818f35
yangyir <31230104+yangyir@users.noreply.github.com>
2024-07-12 10:18:57 +0800

    Update charlotte_script.m

--- charlotte/charlotte_script.m
+++ charlotte/charlotte_script.m
@@ -1,5 +1,5 @@
-asset = 'aluminum';
-dtfrom = '2024-05-27';
+asset = 'soybean oil';
+dtfrom = '2024-06-11';
 [tblout,kellyout,tblout_notused] = charlotte_kellycheck('assetname',asset,...
     'datefrom',dtfrom,...
     'frequency','intraday',...
@@ -7,22 +7,23 @@ dtfrom = '2024-05-27';
 open tblout;
 open kellyout;
 open tblout_notused;
-%
+%%
 [tblpnl,tblout2,statsout] = charlotte_gensingleassetprofile('assetname',asset);
 open tblout2;
 open statsout;
+open tblpnl;
 set(0,'defaultfigurewindowstyle','docked');
 timeseries_plot([tblpnl.dts,tblpnl.runningnotional],'figureindex',2,'dateformat','yy-mmm-dd','title',asset);
 timeseries_plot([tblpnl.dts,tblpnl.runningrets],'figureindex',3,'dateformat','yy-mmm-dd','title',asset);
 %%
-code = tblout2.code{end};
+code = 'ag2408';
 [tblb_headers,tblb_data,~,tbls_data,data] = fractal_gettradesummary(code,...
     'frequency','intraday',...
     'usefractalupdate',0,...
     'usefibonacci',1,...
     'direction','both');
 %%
-charlotte_plot('futcode','TL2409','figureindex',3,'datefrom','2024-05-24');
+charlotte_plot('futcode','CF409','figureindex',3,'datefrom','2024-06-14');
 %%
 assetlist = {'copper';'aluminum';'zinc';'lead';'nickel';'tin'};
 nasset = length(assetlist);
@@ -30,3 +31,109 @@ tblpnlcell = cell(nasset,1);
 for i = 1:nasset
     [tblpnlcell{i},~,~] = charlotte_gensingleassetprofile('assetname',assetlist{i});
 end
+%%
+code = 'au2408';
+resstruct = charlotte_plot('futcode',code,'figureindex',4,'datefrom','2024-07-05');
+fut = code2instrument(code);
+if strcmpi(fut.asset_name,'govtbond_10y') || strcmpi(fut.asset_name,'govtbond_30y')
+    data = load('C:\Users\yy\OneDrive\fractal backtest\kelly distribution\matlab\govtbondfut\strat_govtbondfut_30m.mat');
+    kellytables = data.strat_govtbondfut_30m;
+else
+    data = load('C:\Users\yy\OneDrive\fractal backtest\kelly distribution\matlab\comdty\strat_comdty_i.mat');
+    kellytables = data.strat_comdty_i;
+end
+
+idxstart = find(resstruct.px(:,1) >= datenum('2024-07-05 14:30','yyyy-mm-dd HH:MM'),1,'first');
+clc;
+for i = idxstart:size(resstruct.px,1)
+    ei_ = fractal_truncate(resstruct,i);
+    output = fractal_signal_conditional2('extrainfo',ei_,...
+        'ticksize',fut.tick_size,...
+        'kellytables',kellytables,...
+        'assetname',fut.asset_name);
+    if isempty(output)
+        [~,op] = fractal_signal_unconditional(ei_,fut.tick_size,4);
+        if isempty(op)
+            fprintf('%6s:\t%s:%2d\t%s\n',code,datestr(ei_.px(end,1),'yyyy-mm-dd HH:MM'),0,'no conditional signal');
+        else
+            try
+                fprintf('%6s:\t%s:%2d\t%s\n',code,datestr(ei_.px(end,1),'yyyy-mm-dd HH:MM'),0,op.comment);
+            catch
+                fprintf('%6s:\t%s:%2d\t%s\n',code,datestr(ei_.px(end,1),'yyyy-mm-dd HH:MM'),0,'no conditional signal');
+            end
+        end
+    else
+        fprintf('%6s:\t%s:%2d\t%s\n',code,datestr(ei_.px(end,1),'yyyy-mm-dd HH:MM'),output.directionkellied,output.opkellied);
+    end
+    
+end
+%%
+%%
+asset_list={'gold';'silver';...
+            'copper';'aluminum';'zinc';'lead';'nickel';'tin';...
+            'pta';'lldpe';'pp';'methanol';'crude oil';'fuel oil';'lpg';'soda ash';'carbamide';...
+            'sugar';'cotton';'corn';'egg';...
+            'soybean';'soymeal';'soybean oil';'palm oil';...
+            'rapeseed oil';'rapeseed meal';...
+            'apple';...
+            'rubber';...
+            'live hog';...
+            'coke';'coking coal';'deformed bar';'iron ore';'hotroiled coil';'glass';'pvc'};
+nasset = length(asset_list);
+tblpnls = cell(nasset,1);
+tblout2s = cell(nasset,1);
+statsouts = cell(nasset,1);
+for i = 1:nasset
+    [tblpnls{i},tblout2s{i},statsouts{i}] = charlotte_gensingleassetprofile('assetname',asset_list{i});
+end
+%
+maxpnldrawdown = zeros(nasset,1);
+varpnldrawdown = zeros(nasset,1);
+varpnl = zeros(nasset,1);
+avgpnl = zeros(nasset,1);
+stdpnl = zeros(nasset,1);
+sharpratio = zeros(nasset,1);
+maxtradepnldrawdown = zeros(nasset,1);
+vartradepnldrawdown = zeros(nasset,1);
+avgtradepnl = zeros(nasset,1);
+stdtradepnl = zeros(nasset,1);
+Ns = zeros(nasset,1);
+Ws = zeros(nasset,1);
+Rs = zeros(nasset,1);
+Ks = zeros(nasset,1);
+maxretdrawdown = zeros(nasset,1);
+varretsdrawdown = zeros(nasset,1);
+varret = zeros(nasset,1);
+avgret = zeros(nasset,1);
+stdret = zeros(nasset,1);
+maxtraderetdrawdown = zeros(nasset,1);
+vartraderetdrawdown = zeros(nasset,1);
+for i = 1:nasset
+    maxpnldrawdown(i) = statsouts{i}.maxpnldrawdown;
+    varpnldrawdown(i) = statsouts{i}.varpnldrawdown;
+    varpnl(i) = statsouts{i}.varpnl;
+    avgpnl(i) = statsouts{i}.avgpnl;
+    stdpnl(i) = statsouts{i}.stdpnl;
+    sharpratio(i) = statsouts{i}.sharpratio;
+    maxtradepnldrawdown(i) = statsouts{i}.maxtradepnldrawdown;
+    vartradepnldrawdown(i) = statsouts{i}.vartradepnldrawdown;
+    avgtradepnl(i) = statsouts{i}.avgtradepnl;
+    stdtradepnl(i) = statsouts{i}.stdtradepnl;
+    Ns(i) = statsouts{i}.N;
+    Ws(i) = statsouts{i}.W;
+    Rs(i) = statsouts{i}.R;
+    Ks(i) = statsouts{i}.K;
+    maxretdrawdown(i) = statsouts{i}.maxretdrawdown;
+    varretsdrawdown(i) = statsouts{i}.varretsdrawdown;
+    varret(i) = statsouts{i}.varret;
+    avgret(i) = statsouts{i}.avgret;
+    stdret(i) = statsouts{i}.stdret;
+    maxtraderetdrawdown(i) = statsouts{i}.maxtraderetdrawdown;
+    vartraderetdrawdown(i) = statsouts{i}.vartraderetdrawdown;
+end
+assetlist = asset_list;
+tblreportbyasset = table(assetlist,maxpnldrawdown,varpnldrawdown,varpnl,avgpnl,stdpnl,...
+    sharpratio,maxtradepnldrawdown,vartradepnldrawdown,avgtradepnl,stdtradepnl,...
+    Ns,Ws,Rs,Ks,maxretdrawdown,varretsdrawdown,varret,avgret,stdret,...
+    maxtraderetdrawdown,vartraderetdrawdown);
+
