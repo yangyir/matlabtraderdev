dc854b433fb1aa1b19a3cbe8c8695864568cfddf
yangyir <31230104+yangyir@users.noreply.github.com>
2024-08-29 09:31:07 +0800

    Update kellydistribution_comdty_intraday.m

--- backtest/kellydistribution/kellydistribution_comdty_intraday.m
+++ backtest/kellydistribution/kellydistribution_comdty_intraday.m
@@ -3,6 +3,7 @@ codes_comdty = cell(10000,1);
 ncodes = 0;
 categories = {'agriculture';'basemetal';'energy';'industrial';'preciousmetal'};
 useflags = [1;1;1;1;1];
+passcheck = true;
 for i = 1:length(categories)
     if ~useflags(i),continue;end
     folder_i = [getenv('onedrive'),'\matlabdev\',categories{i},'\'];
@@ -15,11 +16,26 @@ for i = 1:length(categories)
             ncodes = ncodes + 1;
             fn_ij = listing_ij(k).name;
             codes_comdty{ncodes,1} = fn_ij(1:end-4);
+            try
+                data = load([subfolder_i,fn_ij]);
+                data = data.data;
+                if size(data,1) < 13
+                    fprintf('file %s returns insufficient data...\n',fn_ij);
+                    passcheck = false;
+                end
+            catch
+                fprintf('file %s not loaded...\n',fn_ij);
+                passcheck = false;
+            end
         end
     end
 end
+if passcheck
+    fprintf('all code pass pre-check\n');
+end
 codes_comdty = codes_comdty(1:ncodes,:);
-%
+
+%%
 output_comdty_i = fractal_kelly_summary('codes',codes_comdty,'frequency','intraday','usefractalupdate',0,'usefibonacci',1,'direction','both');
 %
 [~,~,tbl_comdty_i,~,~,~,~,strat_comdty_i] = kellydistributionsummary(output_comdty_i,'useactiveonly',true);
@@ -31,7 +47,7 @@ try
     cd(dir_);
 catch
     mkdir(dir_);
-    cd(dir_);
+    cd(dir_);£»
 end
 save([dir_,'strat_comdty_i.mat'],'strat_comdty_i');
 fprintf('strat M-file saved...\n');
@@ -43,6 +59,7 @@ save([dir_,'output_comdty_i.mat'],'output_comdty_i');
 fprintf('output M-file saved...\n');
 %
 filename = [getenv('onedrive'),'\fractal backtest\kelly distribution\tblreport_comdty_i.xlsx'];
+delete(filename);
 writetable(tbl_report_comdty_i,filename,'Sheet',1,'Range','A1');
 fprintf('excel file saved...\n');
 
