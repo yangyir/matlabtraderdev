cd4a3c9bbcd9f81a8d8dbda3ce4321cd8efd5031
yangyir <31230104+yangyir@users.noreply.github.com>
2024-10-17 01:33:46 +0800

    inline

--- trading/strat/@cStratFutMultiFractal/gensignals_futmultifractal1.m
+++ trading/strat/@cStratFutMultiFractal/gensignals_futmultifractal1.m
@@ -428,7 +428,8 @@ function signals = gensignals_futmultifractal1(stratfractal)
                                     kelly = -9.99;
                                     wprob = 0;
                                 end
-                                if ~(kelly > 0.1 && wprob >= 0.40)
+%                                 if ~(kelly > 0.1 && wprob >= 0.40)
+                                if ~(kelly > 0.1)
                                     signal_i(1) = 0;
                                     signal_i(4) = 0;
                                     %unwind position as the kelly or
