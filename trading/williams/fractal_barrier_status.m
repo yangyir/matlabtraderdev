1198082c995bff1cafec443c855f44f10bb2f4f0
yangyir <31230104+yangyir@users.noreply.github.com>
2024-11-12 20:49:38 +0800

    Update fractal_barrier_status.m

--- trading/williams/fractal_barrier_status.m
+++ trading/williams/fractal_barrier_status.m
@@ -11,19 +11,23 @@ function [hhstatus,llstatus] = fractal_barrier_status(extrainfo,ticksize)
             hhstatus = 'flat';
         elseif size(last2hhidx,1) == 2
             last2hh = extrainfo.hh(last2hhidx);
-            if abs(last2hh(end) - last2hh(1)) < 2*ticksize
-                last3hhidx = find(extrainfo.idxhh == 1,3,'last');
-                try
-                    if min(last2hh) - extrainfo.hh(last3hhidx(1)) >= 2*ticksize
-                        hhstatus = 'upward';
-                    else
-                        hhstatus = 'dnward';
-                    end
-                catch
+            if abs(last2hh(end) - last2hh(1)) <= 2*ticksize
+                if abs(last2hh(end) - last2hh(1)) <= 1e-6
                     hhstatus = 'flat';
+                else 
+                    last3hhidx = find(extrainfo.idxhh == 1,3,'last');
+                    try
+                        if min(last2hh) - extrainfo.hh(last3hhidx(1)) >= 2*ticksize
+                            hhstatus = 'upward';
+                        else
+                            hhstatus = 'dnward';
+                        end
+                    catch
+                        hhstatus = 'flat';
+                    end
                 end
             else
-                if last2hh(end) - last2hh(1) >= 2*ticksize
+                if last2hh(end) - last2hh(1) > 2*ticksize
                     hhstatus = 'upward';
                 else
                     hhstatus = 'dnward';
@@ -41,19 +45,23 @@ function [hhstatus,llstatus] = fractal_barrier_status(extrainfo,ticksize)
             llstatus = 'flat';
         elseif size(last2llidx,1) == 2
             last2ll = extrainfo.ll(last2llidx);
-            if abs(last2ll(end) - last2ll(1)) < 2*ticksize
-                last3llidx = find(extrainfo.idxll == -1,3,'last');
-                try
-                    if max(last2ll) - extrainfo.ll(last3llidx(1)) <= -2*ticksize
-                        llstatus = 'dnward';
-                    else
-                        llstatus = 'upward';
-                    end
-                catch
+            if abs(last2ll(end) - last2ll(1)) <= 2*ticksize
+                if abs(last2ll(end) - last2ll(1)) <= 1e-6
                     llstatus = 'flat';
+                else
+                    last3llidx = find(extrainfo.idxll == -1,3,'last');
+                    try
+                        if max(last2ll) - extrainfo.ll(last3llidx(1)) <= -2*ticksize
+                            llstatus = 'dnward';
+                        else
+                            llstatus = 'upward';
+                        end
+                    catch
+                        llstatus = 'flat';
+                    end
                 end
             else
-                if last2ll(end) - last2ll(1) <= -2*ticksize
+                if last2ll(end) - last2ll(1) < -2*ticksize
                     llstatus = 'dnward';
                 else
                     llstatus = 'upward';
