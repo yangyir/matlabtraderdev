d93caf763ba328e08f0ce52adc46b074232fe1f6
yangyir <31230104+yangyir@users.noreply.github.com>
2024-12-04 09:04:11 +0800

    Update charlotte_script.m

--- charlotte/charlotte_script.m
+++ charlotte/charlotte_script.m
@@ -9,7 +9,7 @@ else
 end
 %%
 asset = 'govtbond_30y';
-dtfrom = '2024-09-03';
+dtfrom = '2024-11-13';
 [tblout,kellyout,tblout_notused,kellytables] = charlotte_kellycheck('assetname',asset,'datefrom',dtfrom,'frequency',freq,'reportunused',true);
 open tblout;open kellyout;open tblout_notused;
 %%
@@ -19,19 +19,21 @@ set(0,'defaultfigurewindowstyle','docked');
 timeseries_plot([tblpnl.dts,tblpnl.runningnotional],'figureindex',2,'dateformat','yy-mmm-dd','title',asset);
 timeseries_plot([tblpnl.dts,tblpnl.runningrets],'figureindex',3,'dateformat','yy-mmm-dd','title',asset);
 %%
-charlotte_backtest_all;
+[tblroll,tblcheck_] = charlotte_backtest_all('assetname',asset,'frequency',freq);
+[tblpnl_,tblout_,statsout_] = irene_trades2dailypnl('tradestable',tblcheck_);
+timeseries_plot([tblpnl_.dts,tblpnl_.runningnotional],'figureindex',1,'dateformat','yy-mmm-dd','title',asset);
 %%
-figure(5);plot(cumsum(tbl2check_.closepnl),'b');
 %%
-code = 'TL2412';
-dt1 = '2024-09-10';
-dt2 = '2024-09-10';
+code = 'TL2503';
+dt1 = '2024-12-02';
+dt2 = '2024-12-03';
+% dt2 = dt1;
 [unwindedtrades,carriedtrades,tbl2check] = charlotte_backtest_period('code',code,'fromdate',dt1,'todate',dt2,'kellytables',kellytables,'showlogs',true,'figureidx',4,'frequency',freq);
 % open tbl2check;
 %%
-code_ = 'T2306';
-dt1_ = '2023-03-10';
-dt2_ = '2023-03-13';
+code_ = 'T2406';
+dt1_ = '2023-04-08';
+dt2_ = '2023-04-08';
 [~,~,tbl2check2] = charlotte_backtest_period('code',code_,'fromdate',dt1_,'todate',dt2_,'kellytables',kellytables,'showlogs',true,'figureidx',5);
 %%
 condup = strcmpi(tbl2check_.opensignal,'conditional-uptrendconfirmed') | ...
@@ -74,71 +76,71 @@ for i = 1:n
 end
 
 %%
-% asset_list={'gold';'silver';...
-%             'copper';'aluminum';'zinc';'lead';'nickel';'tin';...
-%             'pta';'lldpe';'pp';'methanol';'crude oil';'fuel oil';'lpg';'soda ash';'carbamide';...
-%             'sugar';'cotton';'corn';'egg';...
-%             'soybean';'soymeal';'soybean oil';'palm oil';...
-%             'rapeseed oil';'rapeseed meal';...
-%             'apple';...
-%             'rubber';...
-%             'live hog';...
-%             'coke';'coking coal';'deformed bar';'iron ore';'hotroiled coil';'glass';'pvc'};
-% nasset = length(asset_list);
-% tblpnls = cell(nasset,1);
-% tblout2s = cell(nasset,1);
-% statsouts = cell(nasset,1);
-% for i = 1:nasset
-%     [tblpnls{i},tblout2s{i},statsouts{i}] = charlotte_gensingleassetprofile('assetname',asset_list{i});
-% end
-% %
-% maxpnldrawdown = zeros(nasset,1);
-% varpnldrawdown = zeros(nasset,1);
-% varpnl = zeros(nasset,1);
-% avgpnl = zeros(nasset,1);
-% stdpnl = zeros(nasset,1);
-% sharpratio = zeros(nasset,1);
-% maxtradepnldrawdown = zeros(nasset,1);
-% vartradepnldrawdown = zeros(nasset,1);
-% avgtradepnl = zeros(nasset,1);
-% stdtradepnl = zeros(nasset,1);
-% Ns = zeros(nasset,1);
-% Ws = zeros(nasset,1);
-% Rs = zeros(nasset,1);
-% Ks = zeros(nasset,1);
-% maxretdrawdown = zeros(nasset,1);
-% varretsdrawdown = zeros(nasset,1);
-% varret = zeros(nasset,1);
-% avgret = zeros(nasset,1);
-% stdret = zeros(nasset,1);
-% maxtraderetdrawdown = zeros(nasset,1);
-% vartraderetdrawdown = zeros(nasset,1);
-% for i = 1:nasset
-%     maxpnldrawdown(i) = statsouts{i}.maxpnldrawdown;
-%     varpnldrawdown(i) = statsouts{i}.varpnldrawdown;
-%     varpnl(i) = statsouts{i}.varpnl;
-%     avgpnl(i) = statsouts{i}.avgpnl;
-%     stdpnl(i) = statsouts{i}.stdpnl;
-%     sharpratio(i) = statsouts{i}.sharpratio;
-%     maxtradepnldrawdown(i) = statsouts{i}.maxtradepnldrawdown;
-%     vartradepnldrawdown(i) = statsouts{i}.vartradepnldrawdown;
-%     avgtradepnl(i) = statsouts{i}.avgtradepnl;
-%     stdtradepnl(i) = statsouts{i}.stdtradepnl;
-%     Ns(i) = statsouts{i}.N;
-%     Ws(i) = statsouts{i}.W;
-%     Rs(i) = statsouts{i}.R;
-%     Ks(i) = statsouts{i}.K;
-%     maxretdrawdown(i) = statsouts{i}.maxretdrawdown;
-%     varretsdrawdown(i) = statsouts{i}.varretsdrawdown;
-%     varret(i) = statsouts{i}.varret;
-%     avgret(i) = statsouts{i}.avgret;
-%     stdret(i) = statsouts{i}.stdret;
-%     maxtraderetdrawdown(i) = statsouts{i}.maxtraderetdrawdown;
-%     vartraderetdrawdown(i) = statsouts{i}.vartraderetdrawdown;
-% end
-% assetlist = asset_list;
-% tblreportbyasset = table(assetlist,maxpnldrawdown,varpnldrawdown,varpnl,avgpnl,stdpnl,...
-%     sharpratio,maxtradepnldrawdown,vartradepnldrawdown,avgtradepnl,stdtradepnl,...
-%     Ns,Ws,Rs,Ks,maxretdrawdown,varretsdrawdown,varret,avgret,stdret,...
-%     maxtraderetdrawdown,vartraderetdrawdown);
+asset_list={'gold';'silver';...
+            'copper';'aluminum';'zinc';'lead';'nickel';'tin';...
+            'pta';'lldpe';'pp';'methanol';'crude oil';'fuel oil';'lpg';'soda ash';'carbamide';...
+            'sugar';'cotton';'corn';'egg';...
+            'soybean';'soymeal';'soybean oil';'palm oil';...
+            'rapeseed oil';'rapeseed meal';...
+            'apple';...
+            'rubber';...
+            'live hog';...
+            'coke';'coking coal';'deformed bar';'iron ore';'hotroiled coil';'glass';'pvc'};
+nasset = length(asset_list);
+tblpnls = cell(nasset,1);
+tblout2s = cell(nasset,1);
+statsouts = cell(nasset,1);
+for i = 1:nasset
+    [tblpnls{i},tblout2s{i},statsouts{i}] = charlotte_gensingleassetprofile('assetname',asset_list{i});
+end
+%
+maxpnldrawdown = zeros(nasset,1);
+varpnldrawdown = zeros(nasset,1);
+varpnl = zeros(nasset,1);
+avgpnl = zeros(nasset,1);
+stdpnl = zeros(nasset,1);
+sharpratio = zeros(nasset,1);
+maxtradepnldrawdown = zeros(nasset,1);
+vartradepnldrawdown = zeros(nasset,1);
+avgtradepnl = zeros(nasset,1);
+stdtradepnl = zeros(nasset,1);
+Ns = zeros(nasset,1);
+Ws = zeros(nasset,1);
+Rs = zeros(nasset,1);
+Ks = zeros(nasset,1);
+maxretdrawdown = zeros(nasset,1);
+varretsdrawdown = zeros(nasset,1);
+varret = zeros(nasset,1);
+avgret = zeros(nasset,1);
+stdret = zeros(nasset,1);
+maxtraderetdrawdown = zeros(nasset,1);
+vartraderetdrawdown = zeros(nasset,1);
+for i = 1:nasset
+    maxpnldrawdown(i) = statsouts{i}.maxpnldrawdown;
+    varpnldrawdown(i) = statsouts{i}.varpnldrawdown;
+    varpnl(i) = statsouts{i}.varpnl;
+    avgpnl(i) = statsouts{i}.avgpnl;
+    stdpnl(i) = statsouts{i}.stdpnl;
+    sharpratio(i) = statsouts{i}.sharpratio;
+    maxtradepnldrawdown(i) = statsouts{i}.maxtradepnldrawdown;
+    vartradepnldrawdown(i) = statsouts{i}.vartradepnldrawdown;
+    avgtradepnl(i) = statsouts{i}.avgtradepnl;
+    stdtradepnl(i) = statsouts{i}.stdtradepnl;
+    Ns(i) = statsouts{i}.N;
+    Ws(i) = statsouts{i}.W;
+    Rs(i) = statsouts{i}.R;
+    Ks(i) = statsouts{i}.K;
+    maxretdrawdown(i) = statsouts{i}.maxretdrawdown;
+    varretsdrawdown(i) = statsouts{i}.varretsdrawdown;
+    varret(i) = statsouts{i}.varret;
+    avgret(i) = statsouts{i}.avgret;
+    stdret(i) = statsouts{i}.stdret;
+    maxtraderetdrawdown(i) = statsouts{i}.maxtraderetdrawdown;
+    vartraderetdrawdown(i) = statsouts{i}.vartraderetdrawdown;
+end
+assetlist = asset_list;
+tblreportbyasset = table(assetlist,maxpnldrawdown,varpnldrawdown,varpnl,avgpnl,stdpnl,...
+    sharpratio,maxtradepnldrawdown,vartradepnldrawdown,avgtradepnl,stdtradepnl,...
+    Ns,Ws,Rs,Ks,maxretdrawdown,varretsdrawdown,varret,avgret,stdret,...
+    maxtraderetdrawdown,vartraderetdrawdown);
 
