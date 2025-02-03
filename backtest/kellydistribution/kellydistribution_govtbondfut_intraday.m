cc81c16631c3f091be21a1910f62e8af6a578629
yangyir <31230104+yangyir@users.noreply.github.com>
2025-02-03 23:18:23 +0800

    Update kellydistribution_govtbondfut_intraday.m

--- backtest/kellydistribution/kellydistribution_govtbondfut_intraday.m
+++ backtest/kellydistribution/kellydistribution_govtbondfut_intraday.m
@@ -42,6 +42,7 @@ for j = 3:size(listing_tl,1)
 end
 codes_govtbondfut_tl = codes_govtbondfut_tl(1:ncodes,:);
 %
+ntl = size(codes_govtbondfut_tl,1);
 %%
 output_govtbondfut_30m = fractal_kelly_summary('codes',codes_govtbondfut_tl,...
     'frequency','intraday','usefractalupdate',0,'usefibonacci',1,'direction','both');
@@ -49,9 +50,9 @@ output_govtbondfut_30m = fractal_kelly_summary('codes',codes_govtbondfut_tl,...
 [~,~,tbl_govtbondfut_30m,~,~,~,~,strat_govtbondfut_30m] = kellydistributionsummary(output_govtbondfut_30m,'useactiveonly',true);
 %
 [tblreport_govtbondfut_30m,statsreport_govtbondfut_30m] = kellydistributionreport(tbl_govtbondfut_30m,strat_govtbondfut_30m);
-% save([dir_,'strat_govtbondfut_30m.mat'],'strat_govtbondfut_30m');
-% save([dir_,'tblreport_govtbondfut_30m.mat'],'tblreport_govtbondfut_30m');
-% fprintf('file of 30m-govtbond saved...\n');
+save([dir_,'strat_govtbondfut_30m.mat'],'strat_govtbondfut_30m');
+save([dir_,'tblreport_govtbondfut_30m.mat'],'tblreport_govtbondfut_30m');
+fprintf('file of 30m-govtbond saved...\n');
 %%
 % codes_govtbondfut_t_latest = {'T2312';'T2403';'T2406';'T2409';'T2412';'T2503'};
 output_govtbondfut_5m = fractal_kelly_summary('codes',codes_govtbondfut_tl,...
@@ -61,9 +62,39 @@ output_govtbondfut_5m = fractal_kelly_summary('codes',codes_govtbondfut_tl,...
 %
 [tblreport_govtbondfut_5m,statsreport_govtbondfut_5m] = kellydistributionreport(tbl_govtbondfut_5m,strat_govtbondfut_5m);
 %
-save([dir_,'strat_govtbondfut_5m.mat'],'strat_govtbondfut_5m');
-save([dir_,'tblreport_govtbondfut_5m.mat'],'tblreport_govtbondfut_5m');
-fprintf('file of 5m-govtbond saved...\n');
+%compare with existing ones
+strat_govtbondfut_5m_existing = load([dir_,'strat_govtbondfut_5m.mat']);
+strat_govtbondfut_5m_existing = strat_govtbondfut_5m_existing.strat_govtbondfut_5m;
+charlotte_strat_compare('strat1',strat_govtbondfut_5m_existing,'strat2',strat_govtbondfut_5m,'assetname','govtbond_30y');
+%compare pnl profiles
+tbl2check_5m = cell(ntl,1);
+tbl2check_5m_existing = cell(ntl,1);
+for i = 1:ntl
+    [dt1,dt2] = irene_findactiveperiod('code',codes_govtbondfut_tl{i});
+    dt1 = datestr(dt1,'yyyy-mm-dd');
+    dt2 = datestr(dt2,'yyyy-mm-dd');
+    [~,~,tbl2check_5m{i}] = charlotte_backtest_period('code',codes_govtbondfut_tl{i},'fromdate',dt1,'todate',dt2,'kellytables',strat_govtbondfut_5m,'showlogs',false,'figureidx',4,'frequency','5m');
+    [~,~,tbl2check_5m_existing{i}] = charlotte_backtest_period('code',codes_govtbondfut_tl{i},'fromdate',dt1,'todate',dt2,'kellytables',strat_govtbondfut_5m_existing,'showlogs',false,'figureidx',4,'frequency','5m');
+end
+tbl2check_5m_all = tbl2check_5m{1};
+tbl2check_5m_existing_all = tbl2check_5m_existing{1};
+for i = 2:ntl
+    temp_5m = [tbl2check_5m_all;tbl2check_5m{i}];
+    tbl2check_5m_all = temp_5m;
+    temp_5m_existing = [tbl2check_5m_existing_all;tbl2check_5m_existing{i}];
+    tbl2check_5m_existing_all = temp_5m_existing;
+end
+[tblpnl_5m,~,statsout_5m] = irene_trades2dailypnl('tradestable',tbl2check_5m_all,'frequency','5m');
+[tblpnl_5m_existing,~,statsout_5m_existing] = irene_trades2dailypnl('tradestable',tbl2check_5m_existing_all,'frequency','5m');
+figure(10);
+plot(tblpnl_5m_existing.runningnotional,'b-');
+hold on;
+plot(tblpnl_5m.runningnotional,'r-');
+legend('exiting-5m','new-5m');hold off;
+
+% save([dir_,'strat_govtbondfut_5m.mat'],'strat_govtbondfut_5m');
+% save([dir_,'tblreport_govtbondfut_5m.mat'],'tblreport_govtbondfut_5m');
+% fprintf('file of 5m-govtbond saved...\n');
 %%
 output_govtbondfut_15m = fractal_kelly_summary('codes',codes_govtbondfut_tl,...
     'frequency','intraday-15m','usefractalupdate',0,'usefibonacci',1,'direction','both');
@@ -72,29 +103,57 @@ output_govtbondfut_15m = fractal_kelly_summary('codes',codes_govtbondfut_tl,...
 %
 [tblreport_govtbondfut_15m,statsreport_govtbondfut_15m] = kellydistributionreport(tbl_govtbondfut_15m,strat_govtbondfut_15m);
 %
-save([dir_,'strat_govtbondfut_15m.mat'],'strat_govtbondfut_15m');
-save([dir_,'tblreport_govtbondfut_15m.mat'],'tblreport_govtbondfut_15m');
-fprintf('file 15m-govtbond saved...\n');
+%compare with existing ones
+strat_govtbondfut_15m_existing = load([dir_,'strat_govtbondfut_15m.mat']);
+strat_govtbondfut_15m_existing = strat_govtbondfut_15m_existing.strat_govtbondfut_15m;
+charlotte_strat_compare('strat1',strat_govtbondfut_15m_existing,'strat2',strat_govtbondfut_15m,'assetname','govtbond_30y');
+%compare pnl profiles
+tbl2check_15m = cell(ntl,1);
+tbl2check_15m_existing = cell(ntl,1);
+for i = 1:ntl
+    [dt1,dt2] = irene_findactiveperiod('code',codes_govtbondfut_tl{i});
+    dt1 = datestr(dt1,'yyyy-mm-dd');
+    dt2 = datestr(dt2,'yyyy-mm-dd');
+    [~,~,tbl2check_15m{i}] = charlotte_backtest_period('code',codes_govtbondfut_tl{i},'fromdate',dt1,'todate',dt2,'kellytables',strat_govtbondfut_15m,'showlogs',false,'figureidx',4,'frequency','15m');
+    [~,~,tbl2check_15m_existing{i}] = charlotte_backtest_period('code',codes_govtbondfut_tl{i},'fromdate',dt1,'todate',dt2,'kellytables',strat_govtbondfut_15m_existing,'showlogs',false,'figureidx',4,'frequency','15m');
+end
+tbl2check_15m_all = tbl2check_15m{1};
+tbl2check_15m_existing_all = tbl2check_15m_existing{1};
+for i = 2:ntl
+    temp_15m = [tbl2check_15m_all;tbl2check_15m{i}];
+    tbl2check_15m_all = temp_15m;
+    temp_15m_existing = [tbl2check_15m_existing_all;tbl2check_15m_existing{i}];
+    tbl2check_15m_existing_all = temp_15m_existing;
+end
+[tblpnl_15m,~,statsout_15m] = irene_trades2dailypnl('tradestable',tbl2check_15m_all,'frequency','15m');
+[tblpnl_15m_existing,~,statsout_15m_existing] = irene_trades2dailypnl('tradestable',tbl2check_15m_existing_all,'frequency','15m');
+figure(11);
+plot(tblpnl_15m_existing.runningnotional,'b-');
+hold on;
+plot(tblpnl_15m.runningnotional,'r-');
+legend('exiting-15m','new-15m');hold off;
+% save([dir_,'strat_govtbondfut_15m.mat'],'strat_govtbondfut_15m');
+% save([dir_,'tblreport_govtbondfut_15m.mat'],'tblreport_govtbondfut_15m');
+% fprintf('file 15m-govtbond saved...\n');
 %%
-ntl = size(codes_govtbondfut_tl,1);
-tbl2check_5m = cell(ntl,1);
+
 tbl2check_15m = cell(ntl,1);
 tbl2check_30m = cell(ntl,1);
 for i = 1:ntl
     [dt1,dt2] = irene_findactiveperiod('code',codes_govtbondfut_tl{i});
     dt1 = datestr(dt1,'yyyy-mm-dd');
     dt2 = datestr(dt2,'yyyy-mm-dd');
-    [~,~,tbl2check_5m{i}] = charlotte_backtest_period('code',codes_govtbondfut_tl{i},'fromdate',dt1,'todate',dt2,'kellytables',strat_govtbondfut_5m,'showlogs',false,'figureidx',4,'frequency','5m');
+    
     [~,~,tbl2check_15m{i}] = charlotte_backtest_period('code',codes_govtbondfut_tl{i},'fromdate',dt1,'todate',dt2,'kellytables',strat_govtbondfut_15m,'showlogs',false,'figureidx',5,'frequency','15m');
     [~,~,tbl2check_30m{i}] = charlotte_backtest_period('code',codes_govtbondfut_tl{i},'fromdate',dt1,'todate',dt2,'kellytables',strat_govtbondfut_30m,'showlogs',false,'figureidx',6,'frequency','30m');
 end
 %%
-tbl2check_5m_all = tbl2check_5m{1};
+% tbl2check_5m_all = tbl2check_5m{1};
 tbl2check_15m_all = tbl2check_15m{1};
 tbl2check_30m_all = tbl2check_30m{1};
 for i = 2:ntl
-    temp_5m = [tbl2check_5m_all;tbl2check_5m{i}];
-    tbl2check_5m_all = temp_5m;
+%     temp_5m = [tbl2check_5m_all;tbl2check_5m{i}];
+%     tbl2check_5m_all = temp_5m;
     temp_15m = [tbl2check_15m_all;tbl2check_15m{i}];
     tbl2check_15m_all = temp_15m;
     temp_30m = [tbl2check_30m_all;tbl2check_30m{i}];
