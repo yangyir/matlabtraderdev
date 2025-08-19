277c763f83ab67e02fe1999c9c66ec0f61a1b97e
yangyir <31230104+yangyir@users.noreply.github.com>
2025-08-19 20:48:06 +0800

    Update onNewSignal.m
    in line with backtest, i.e. we shall not open a position in case the conditional has negative kelly and even the unconditional is a breachdn-lvldn

--- charlotte/@charlotteTraderFX/onNewSignal.m
+++ charlotte/@charlotteTraderFX/onNewSignal.m
@@ -84,29 +84,32 @@ function [] = onNewSignal(obj,~,eventData)
                     end
                 else
                     if signal_i.status.islvldnbreach
-                        trade = fractal_gentrade(ei_i,code_i,size(ei_i.px,1),signal_i.opkellied,-1,freq_i,nfractal,0);
-                        trade.riskmanager_.setusefractalupdateflag(0);
-                        trade.riskmanager_.setusefibonacciflag(1);
-                        unwindtrade = trade.riskmanager_.riskmanagementwithcandle([],...
-                            'usecandlelastonly',true,...
-                            'debug',false,...
-                            'updatepnlforclosedtrade',true,...
-                            'extrainfo',ei_i,...
-                            'RunRiskManagementBeforeMktClose',false,...
-                            'KellyTables',data.kellytables_{i},...
-                            'CompulsoryCheckForConditional',true);
-                        if ~isempty(unwindtrade)
-                            trade.status_ = 'closed';
-                        end
-                        obj.book_.push(trade);
-                        if strcmpi(mode_i,'realtime')
-                            exporttrade2mt4(trade,ei_i);
-                        else
-                            freq = trade.opensignal_.frequency_;
-                            freqappendix = freq2mt4freq(freq);
-                            opendtstr = datestr(ei_i.px(end,1),'yyyymmdd');
-                            fn = [getenv('OneDrive'),'\mt4\replay\',trade.code_,'.lmx_',freqappendix,'_trades_',opendtstr,'.txt'];
-                            exporttrade2mt4(trade,ei_i,fn);
+%                         trade = fractal_gentrade(ei_i,code_i,size(ei_i.px,1),signal_i.opkellied,-1,freq_i,nfractal,0);
+                        trade = fractal_gentrade2(ei_i,code_i,size(ei_i.px,1),freq_i,nfractal,data.kellytables_{i});
+                        if ~isempty(trade)
+                            trade.riskmanager_.setusefractalupdateflag(0);
+                            trade.riskmanager_.setusefibonacciflag(1);
+                            unwindtrade = trade.riskmanager_.riskmanagementwithcandle([],...
+                                'usecandlelastonly',true,...
+                                'debug',false,...
+                                'updatepnlforclosedtrade',true,...
+                                'extrainfo',ei_i,...
+                                'RunRiskManagementBeforeMktClose',false,...
+                                'KellyTables',data.kellytables_{i},...
+                                'CompulsoryCheckForConditional',true);
+                            if ~isempty(unwindtrade)
+                                trade.status_ = 'closed';
+                            end
+                            obj.book_.push(trade);
+                            if strcmpi(mode_i,'realtime')
+                                exporttrade2mt4(trade,ei_i);
+                            else
+                                freq = trade.opensignal_.frequency_;
+                                freqappendix = freq2mt4freq(freq);
+                                opendtstr = datestr(ei_i.px(end,1),'yyyymmdd');
+                                fn = [getenv('OneDrive'),'\mt4\replay\',trade.code_,'.lmx_',freqappendix,'_trades_',opendtstr,'.txt'];
+                                exporttrade2mt4(trade,ei_i,fn);
+                            end
                         end
                     end
                     %
