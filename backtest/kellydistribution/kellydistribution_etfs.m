95d41301df93e367ef754a3ae84429144d62ee15
yangyir <31230104+yangyir@users.noreply.github.com>
2025-08-26 10:03:54 +0800

    Update kellydistribution_etfs.m

--- backtest/kellydistribution/kellydistribution_etfs.m
+++ backtest/kellydistribution/kellydistribution_etfs.m
@@ -6,6 +6,11 @@ output_etfs = fractal_kelly_summary('codes',codes_etfs,'frequency','daily','usef
 [~,~,tbl_etfs,~,~,~,~,strat_daily_etfs] = kellydistributionsummary(output_etfs);
 [tblreport_etfs,stats_etfs,tbl_byasset_etfs] = kellydistributionreport(tbl_etfs,strat_daily_etfs);
 %%
+output_etfs = fractal_kelly_summary('codes',{'512100'},'frequency','intraday','usefractalupdate',0,'usefibonacci',1,'direction','both');
+[~,~,tbl_etfs_m30,~,~,~,~,strat_etfs_m30] = kellydistributionsummary(output_etfs);
+%%
+[unwindedtrades,carriedtrades,tbl2check] = charlotte_backtest_period('code','512100','fromdate','2023-08-25','todate','2023-08-28','kellytables',strat_etfs_m30,'showlogs',false,'figureidx',4,'frequency','30m','nfractal',4);
+%%
 dir_ = [getenv('onedrive'),'\fractal backtest\kelly distribution\matlab\etfs\'];
 try
     cd(dir_);
