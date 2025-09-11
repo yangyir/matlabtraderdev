03ba5fbe19d7c6bbd0d59f2917f24370fdc32f8b
yangyir <31230104+yangyir@users.noreply.github.com>
2025-09-11 15:11:20 +0800

    Create cStratConfigFractalOpt.m

---
+++ trading/strat/configurations/@cStratConfigFractalOpt/cStratConfigFractalOpt.m
@@ -0,0 +1,35 @@
+classdef cStratConfigFractalOpt < cStratConfig
+    %class cStratConfigFractalOpt
+    %used in trading with cStratOptMultiFractal
+    
+    properties (GetAccess = public, SetAccess = public)
+        %default values
+        tdsqlag_@double = 4
+        tdsqconsecutive_@double = 9
+        nfractals_@double = 2
+        includelastcandle_@double = 0
+        usefractalupdate_@double = 1
+        usefibonacci_@double = 1
+        %
+        underliercodectp_@char
+        
+    end
+    
+    methods
+        function obj = cStratConfigFractalOpt(varargin)
+            obj = obj@cStratConfig(varargin{:});
+            obj.setname('cStratConfigFractal');
+        end
+        
+        function [] = loadfromfile(obj,varargin)
+            loadfromfile@cStratConfig(obj,varargin{:});
+            
+            [optflag,~,~,underlierstr,~] = isoptchar(obj.codectp_);
+            if ~optflag
+                error('%s:%s:option input is required...',class(cStratConfigFractalOpt),'loadfromfile')
+            end
+            
+            obj.underliercodectp_ = underlierstr;
+        end
+    end
+end
