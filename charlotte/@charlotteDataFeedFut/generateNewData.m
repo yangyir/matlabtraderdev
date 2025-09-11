47dbcdb83f7a8d04e20137344e0348e26612124e
yangyir <31230104+yangyir@users.noreply.github.com>
2025-09-11 15:13:08 +0800

    update charlotteDataFeedFut

--- charlotte/@charlotteDataFeedFut/generateNewData.m
+++ charlotte/@charlotteDataFeedFut/generateNewData.m
@@ -5,68 +5,100 @@ function [] = generateNewData(obj)
     
     if strcmpi(obj.mode_,'realtime')
         t = now;
-    elseif strcmpi(obj.mode_,'replay')
-        obj.replaycounts_(1) = obj.replaycounts_(1) + 1;
-        try
-            t = obj.replaydata_{1}(obj.replaycounts_(1),1);
-        catch
-            
+        
+        if obj.istime2sleep(t), return;end
+        
+        mm = minute(t) + hour(t)*60; 
+        if ((mm >= obj.mm_02_30_ + 1 && mm <  obj.mm_08_50_) || ...
+                (mm >= obj.mm_15_15_ + 1 && mm < obj.mm_20_50_))
+            % 1 minute after market closes and 10 minutes before market opens
+            data.time = t;
+            notify(obj, 'MarketClose', charlotteDataFeedEventData(data));
         end
         
+        if ((mm >= obj.mm_08_50_ && mm < obj.mm_09_00_) || ...
+                (mm >= obj.mm_20_50_ && mm < obj.mm_21_00_))
+            % 10 minutes before market opens
+            data.time = t;
+            notify(obj, 'MarketOpen', charlotteDataFeedEventData(data));
+        end
+        %
+        data = cell(ncodes,1);
+        nupdate = 0;
+        try
+            obj.qms_.refresh;
+        catch ME
+            % error event triggered
+            notify(obj, 'ErrorOccurred', charlotteErrorEventData(ME.message));
+        end
+        
+        for i = 1:ncodes
+            quote = obj.qms_.getquote(obj.codes_{i});
+            lasttrade = quote.last_trade;
+            
+            if lasttrade <= 0, continue;end
+            if isnan(lasttrade), continue;end
+            if isempty(lasttrade),continue;end
+            
+            currentticktime = quote.update_time1;
+            if (currentticktime > obj.lastticktime_(i)) || ...
+                    (currentticktime - obj.lastticktime_(i) < 5e-6 && lasttrade ~= obj.lasttrade_(i))
+                nupdate = nupdate + 1;
+                obj.lastticktime_(i) = currentticktime;
+                obj.lasttrade_(i) = lasttrade;
+                data_i = struct('time',currentticktime,...
+                    'lasttrade',lasttrade);
+                data{i} = data_i;
+            end
+        end
+        
+        if nupdate > 0
+            % event triggered
+            notify(obj, 'NewDataArrived', charlotteDataFeedEventData(data));
+        end
+        
+        return
+        
+    end
+    %
+    %
+    if strcmpi(obj.mode_,'replay')
+        totalCount = size(obj.replaydata_{1},1);
+        thisCount = obj.replaycounts_(1);
+        if thisCount < totalCount
+            t = obj.replaydata_{1}(thisCount,1);
+            lasttrade = obj.replaydata_{1}(thisCount,2);
+            obj.lastticktime_(1) = t;
+            obj.lasttrade_(1) = lasttrade;
+            data = cell(ncodes,1);
+            data{1} = struct('time',t,'lasttrade',lasttrade);
+            % event trigger
+            notify(obj, 'NewDataArrived', charlotteDataFeedEventData(data));
+            newCount = thisCount + 1;
+            obj.replaycounts_(1) = newCount;
+        else
+            data.time = obj.replaydata_{1}(end,1)+1/86400;
+            % event trigger
+            notify(obj, 'MarketClose', charlotteDataFeedEventData(data));
+        end
+        
+        return
+        
         
     end
     
-    if obj.istime2sleep(t), return;end
     
     
-    mm = minute(t) + hour(t)*60;
     
-    if ((mm >= obj.mm_02_30_ + 1 && mm <  obj.mm_08_50_) || ...
-           (mm >= obj.mm_15_15_ + 1 && mm < obj.mm_20_50_))
-       % 1 minute after market closes and 10 minutes before market opens
-       data.time = now;
-       notify(obj, 'MarketClose', charlotteDataFeedEventData(data));
-    end
     
-    if ((mm >= obj.mm_08_50_ && mm < obj.mm_09_00_) || ...
-           (mm >= obj.mm_20_50_ && mm < obj.mm_21_00_))
-       % 10 minutes before market opens
-        data.time = now;
-        notify(obj, 'MarketOpen', charlotteDataFeedEventData(data));
-    end
     
-    data = cell(ncodes,1);
-    nupdate = 0;
-    try
-        obj.qms_.refresh;
-    catch ME
-        % error event triggered
-        notify(obj, 'ErrorOccurred', ...
-            charlotteErrorEventData(ME.message));
-    end
+    
+    
+    
     
      
-    for i = 1:ncodes
-        quote = obj.qms_.getquote(obj.codes_{i});
-        lasttrade = quote.last_trade;
-        if lasttrade <= 0, continue;end
-        if isnan(lasttrade), continue;end
-        if isempty(lasttrade),continue;end
-        currentticktime = quote.update_time1;
-        if (currentticktime > obj.lastticktime_(i)) || ...
-                (currentticktime - obj.lastticktime_(i) < 5e-6 && lasttrade ~= obj.lasttrade_(i))
-            nupdate = nupdate + 1;
-            obj.lastticktime_(i) = currentticktime;
-            obj.lasttrade_(i) = lasttrade;
-            data_i = struct('time',currentticktime,...
-                    'lasttrade',lasttrade);
-            data{i} = data_i;
-        end
-    end
+    
         
-    if nupdate > 0
-        % event triggered
-        notify(obj, 'NewDataArrived', charlotteDataFeedEventData(data));
-    end
+    
 
 end
--- charlotte/@charlotteDataFeedFut/onMarketClose.m
+++ charlotte/@charlotteDataFeedFut/onMarketClose.m
@@ -2,10 +2,20 @@ function [] = onMarketClose(obj,~,eventData)
 % a charlotteDataFeedFut function
     data = eventData.MarketData;
     t = data.time;
-    if obj.qmsconnected_
-        fprintf('charlotteDataFeedFut:onMarketClose called: CTP logoff at %s\n',...
-            datestr(t,'yyyy-mm-dd HH:MM:SS'))
-        obj.qms_.ctplogoff;
-        obj.qmsconnected_ = false;
+    
+    if strcmpi(obj.mode_,'realtime')
+        if obj.qmsconnected_
+            fprintf('charlotteDataFeedFut:onMarketClose called: CTP logoff at %s\n',...
+                datestr(t,'yyyy-mm-dd HH:MM:SS'))
+            try
+                obj.qms_.ctplogoff;
+                obj.qmsconnected_ = false;
+            catch
+                obj.qmsconnected_ = false;
+                notify(obj, 'ErrorOccurred', ...
+                        charlotteErrorEventData('Failed to log off CTP server'));
+                obj.stop;    
+            end
+        end
     end
 end
